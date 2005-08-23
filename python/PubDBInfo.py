cbd43cba33dd72f057d17b8e4e28e66bc69a995a
slacapra <>
2005-08-23 10:57:51 +0000

    add files for pre2

---
+++ python/PubDBInfo.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+import sys, os, string, re
+import urllib, urllister
+import urllib2
+from UnserializePHP import *
+from orcarcBuilderNew import *
+
+class PubDBInfoError:
+  def __init__(self, Collections):
+    print '\nERROR accessing PubDB for Collections: '+Collections+'\n'
+    pass
+class PubDBInfoNoCollectionError:
+  def __init__(self, Collections):
+    print '\nERROR No Collections found in PubDB : '+Collections+'\n'
+    pass
+class NoPHPError:
+  def __init__(self, url):
+    #print '\nERROR accessing PHP at '+url+' \n'
+    print 'ERROR accessing PHP: ',url,'isn\'t updated version \n'
+    pass
+class PubDBInfoResult:
+  def __init__(self,
+               contents):
+    self.contents=contents
+
+################################################################################
+# Class to connect to PubDB interface for the analysis and download the data in one shot using the serialized PHP data.
+################################################################################
+class PubDBInfo:
+  def __init__(self, pubdburl, Collections):
+         self.Collections= Collections
+         self.PubDBurl_ = pubdburl
+         #self.PubDBInfophp_ = 'pubdb-get-analisys-info.php'
+         self.PubDBInfophp_ = 'pubdb-get-analysisinfo.php'
+         self.protocolPrio_ = ['http', 'rfio', 'mysql' , 'gridftp']
+         #self.protocolPrio_ = ['http', 'mysql' , 'rfio' , 'gridftp']
+
+  ##########################################################################   
+  def GetPubDBInfo(self):
+    """
+    Get all the catalogues-related info from PubDB and select the best ones if multiple choices are possible 
+    """
+    ### extract catalogues related info from pubDB
+    cataloguecoll_map = self.ExtractPubDBInfo()
+    ### select the Best catalogues
+    cataloguesinfos=self.SelectBestPubDBInfo(cataloguecoll_map)
+    return cataloguesinfos
+
+  #########################################################################
+  def ExtractPubDBInfo(self):
+    """
+    Extract all the information from the PubDB analysis interface
+    """
+    try:
+             #print " contacting PubDb... "+self.PubDBurl_+self.PubDBInfophp_+'?collid='+self.Collections+"\n"
+        f = urllib.urlopen(self.PubDBurl_+self.PubDBInfophp_+'?collid='+self.Collections)
+    except IOError:
+        raise PubDBInfoError(self.Collections)
+
+    data = f.read()
+    #print data
+    if len(data)>0:
+       if data[0]=='<':
+              raise PubDBInfoNoCollectionError(self.Collections)
+    try:
+        catalogues = PHPUnserialize().unserialize(data)
+    except IOError:
+        raise PHPUnserializeError(data)
+    try:
+        catinfos=[]
+        collmap={}
+        for k in catalogues.keys():
+           CollId=catalogues[k]['CollectionId']
+           colllist=[]
+           #print ">>> Catalogues for Collection: "+CollId+"\n"
+          
+           cat=catalogues[k]['Catalogue']
+           for kcat in cat.keys():
+                ##print ("key %s, val %s" %(kcat,cat[kcat]))
+                ContactString=cat[kcat]['ContactString']
+                ContactProtocol=cat[kcat]['ContactProtocol']
+                CatalogueType=cat[kcat]['CatalogueType']
+                ValidationStatus=cat[kcat]['ValidationStatus']
+                #print "CS: "+ContactString
+                #print "CP: "+ContactProtocol
+                #print "CT: "+CatalogueType
+                #print "VS: "+ValidationStatus
+                ce=cat[kcat]['CEs']
+                CElist=[]
+                for kce in ce.keys():
+                   ##print ("key %s, val %s" %(kce,ce[kce]))
+                   CE=ce[kce]
+                   CElist.append(ce[kce])
+                #print " CE list :"
+                #for aCE in CElist:
+                #  print " CE : "+aCE
+                cc=cat[kcat]['CatalogueContents']
+                for kcc in cc.keys():
+                   ##print ("key %s, val %s" %(kcc,cc[kcc]))
+                   FileType=cc[kcc]['FileType']
+                   SE=cc[kcc]['SE']
+                   #print "FT: "+FileType
+                   #print "SE: "+SE 
+                   if cc[kcc]['Variables']==None:
+                     Variables=''
+                   else:
+                    for kvar in cc[kcc]['Variables'].keys():
+                     Variables=kvar+"="+cc[kcc]['Variables'][kvar]
+                     #print "Variables: "+Variables
+                   run=cc[kcc]['RunRange']
+                   for krun in run.keys():
+                     ##print ("key %s, val %s" %(krun,run[krun]))
+                     reTot = re.compile(r'TotalEvents=(\d*)')
+                     TotalEvents=reTot.search(run[krun]).group(1)
+                     reFirst= re.compile(r'FirstRun=(\d*)')
+                     FirstRun = reFirst.search(run[krun]).group(1)
+                     reLast= re.compile(r'LastRun=(\d*)')
+                     LastRun = reLast.search(run[krun]).group(1)
+                     #print "Nevents: "+TotalEvents
+                     #print "First: "+FirstRun
+                     #print "Last: "+LastRun
+                     #print "----------------------------------"
+
+                     ## fill a catlogue entry
+                     acatalogue=catalogEntryNew(FileType,ValidationStatus,ContactString,ContactProtocol,CatalogueType,SE,CElist,TotalEvents,FirstRun+'-'+LastRun,Variables)          
+                     ## list the catalogues belonging to a given collection
+                     colllist.append(acatalogue) 
+                     
+           ## dictionary grouping catalogues by CollectionID
+           collmap[CollId]=colllist
+           
+    except IOError:
+       raise PHPUnserializeError(data)
+    
+    return collmap
+
+  ########################################################################
+  def SelectBestPubDBInfo(self,cataloglist):
+    """
+    Select the lists of needed catalogues (from a set of catalogues refering to the same collection and FileType selects the best on based on protocol)
+    """
+
+    selectcatalogues=[]
+ 
+    ### for each collection   
+    for collid in cataloglist.keys():
+      #print ("key %s, val %s" %(collid,cataloglist[collid]))
+      ### get all the possible FileTypes 
+      filetypes=[]
+      for catalog in cataloglist[collid]:
+        if catalog.FileType not in filetypes :
+          filetypes.append(catalog.FileType) 
+      ### dictionary grouping catalogues by FileType 
+      ftmap={}
+      for afiletype in filetypes:
+        #print ' filetype is '+afiletype+' for collid='+collid 
+        sameFileType=[]
+        for catalog in cataloglist[collid]:
+          if catalog.FileType==afiletype :
+            sameFileType.append(catalog)
+        ftmap[afiletype]=sameFileType        
+      ### select only one catalogue among the catalouges with the same FileType
+      for ft in ftmap.keys():
+       #print ("key %s, val %s" %(ft,ftmap[ft])) 
+       bestcatalog=self.SelectBestCatalog(ftmap[ft])
+       selectcatalogues.append(bestcatalog)
+
+    ### return the minimal list of needed catalogues
+    return selectcatalogues 
+
+  ####################################################
+  def SelectBestCatalog(self,ftcat):
+    """
+    From a set of catalogues with the same FileTypeand different access protocol, select the one according to access protocols
+    """
+    sortedProtocols = self.protocolPrio_
+
+    ### if just one catalog, just return it!
+    if (len(ftcat)==1):
+      #print '----- Just one catalogue, no selection based on protocol needed'
+      #ftcat[0].dump()
+      #print '---------------------'
+       return ftcat[0]
+    ### oterwise select the best catalogue based on protocol   
+    for prot in sortedProtocols:
+      for cat in ftcat:
+        if cat.ContactProtocol==prot:
+          #print '----- Catalogue selected based on protocol : '+prot
+          #cat.dump()
+          #print '---------------------'
+          return cat
+        
---
+++ python/Scram.py
@@ -0,0 +1,158 @@
+import common
+from crab_logger import Logger
+from crab_exceptions import *
+from crab_util import *
+
+class Scram:
+    def __init__(self, cfg_params):
+        self.build = 'tgz'
+        self.tgz_name = 'default.tgz'
+        self.tgzNameWithPath = None
+
+        self.scramVersion = 0
+        scramArea = ''
+
+        if os.environ.has_key("SCRAMRT_LOCALRT"):
+            # try scram v1
+            self.scramArea = os.environ["SCRAMRT_LOCALRT"]
+            self.scramVersion = 1
+        elif os.environ.has_key("LOCALRT"):
+            # try scram v0
+            self.scramArea = os.environ["LOCALRT"]
+            self.scramVersion = 0
+        else:
+            msg = 'Did you do eval `scram(v1) runtime ...` from your ORCA area ?\n'
+            raise CrabException(msg)
+        common.logger.debug(5, "Scram::Scram() version is "+str(self.scramVersion))
+        common.logger.debug(6, "Scram::Scram() area is "+self.scramArea)
+        pass
+
+    def commandName(self):
+        """ return scram or scramv1 """
+        if self.scramVersion == 1: return 'scramv1'
+        else: return 'scram'
+
+    def getSWArea_(self):
+        """
+        Get from SCRAM the local working area location
+        """
+        return string.strip(self.scramArea)
+
+    def getSWVersion(self):
+        """
+        Get the version of the sw
+        """
+
+        sw  = ''
+        ver = ''
+        if self.scramVersion == 1 :
+            try:
+                ver = string.split(os.environ["SCRAMRT_SET"],":")[-1]
+            except IOError, e:
+                msg = 'Cannot find sw project and version:\n'
+                msg += str(e)
+                raise CrabException(msg)
+        else:
+            ver = string.split(self.scramArea,'/')[-1]
+        return string.strip(ver)
+        
+    def getTarBall(self, exe):
+        """
+        Return the TarBall with lib and exe
+        """
+        
+        # if it exist, just return it
+        self.tgzNameWithPath = common.work_space.shareDir()+self.tgz_name
+        if os.path.exists(self.tgzNameWithPath):
+            return self.tgzNameWithPath
+
+        # Prepare a tar gzipped file with user binaries.
+        self.prepareTgz_(exe)
+
+        return string.strip(self.tgzNameWithPath)
+
+    def getReleaseTop_(self):
+        """ get release top """
+        result = ''
+        if self.scramVersion == 1 :
+            try:
+                result = os.environ["SCRAMRT_RELEASETOP"]
+            except KeyError:
+                result = self.scramArea
+        else:
+            envFileName = self.scramArea+"/.SCRAM/Environment"
+            try:
+                envFile = open(envFileName, 'r')
+                for line in envFile:
+                    line = string.strip(line)
+                    (k, v) = string.split(line, '=')
+                    if k == 'RELEASETOP':
+                        result=v
+                        break
+                    pass
+                pass
+            except IOError:
+                msg = 'Cannot open scram environment file '+envFileName
+                raise CrabException(msg)
+                pass
+            pass
+        return string.strip(result)
+            
+    def prepareTgz_(self, executable):
+
+        # First of all declare the user Scram area
+        swArea = self.getSWArea_()
+        swVersion = self.getSWVersion()
+        swReleaseTop = self.getReleaseTop_()
+
+        ## check if working area is release top
+        if swReleaseTop == '' or swArea == swReleaseTop:
+            return
+
+        ## First find the executable
+        cmd = 'which ' + executable
+        exeWithPath = string.strip(runCommand(cmd))
+        if int(exeWithPath.find("no "+executable+" in"))!=-1:
+            raise CrabException('User executable '+executable+' not found')
+
+        filesToBeTarred = []
+        ## then check if it's private or not
+        if exeWithPath.find(swReleaseTop) == -1:
+            # the exe is private, so we must ship
+            common.logger.debug(5,"Exe "+exeWithPath+" to be tarred")
+            filesToBeTarred.append(exeWithPath)
+            pass
+        else:
+            # the exe is from release, we'll find it on WN
+            pass
+
+        ## Now get the libraries: only those in local working area
+        cmd = 'ldd ' + exeWithPath + ' | grep ' + swArea
+        myCmd = os.popen(cmd)
+        for line in (myCmd):
+            libWithFullPath = string.split(string.strip(line))[2]
+            path = swArea+'/'
+            lib = string.replace(libWithFullPath, path,'')
+            common.logger.debug(5,"lib "+lib+" to be tarred")
+            filesToBeTarred.append(lib)
+        status = myCmd.close()
+
+        ## Now check if the Data dir is present
+        dataDir = 'src/Data/'
+        if os.path.isdir(swArea+'/'+dataDir):
+            filesToBeTarred.append(dataDir)
+
+        ## Create the tar-ball
+        if len(filesToBeTarred)>0:
+            os.chdir(swArea)
+            tarcmd = 'tar zcvf ' + self.tgzNameWithPath + ' ' 
+            for line in filesToBeTarred:
+                tarcmd = tarcmd + line + ' '
+            cout = runCommand(tarcmd)
+            if not cout:
+                raise CrabException('Could not create tar-ball')
+            os.chdir(common.work_space.topDir())
+        else:
+            common.logger.debug(5,"No files to be to be tarred")
+        
+        return
