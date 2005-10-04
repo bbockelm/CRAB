7c5abfc29489857d99737c365e29c59959d4949c
corvo <>
2005-10-04 16:47:53 +0000

    Bug fix

--- python/Submitter.py
+++ python/Submitter.py
@@ -36,64 +36,68 @@ class Submitter(Actor):
         # Loop over jobs
         njs = 0
         try:
-          for nj in self.nj_list:
-            st = common.jobDB.status(nj)
-            if st != 'C' and st != 'K' and st != 'A' and st != 'RC':
-                long_st = crabJobStatusToString(st)
-                #msg = "Job # %d is not submitted: status %s"%(nj+1, long_st)
-                #common.logger.message(msg)
-                continue
+            for nj in self.nj_list:
+                st = common.jobDB.status(nj)
+                #print "nj = ", nj 
+                #print "st = ", st
+                if st != 'C' and st != 'K' and st != 'A' and st != 'RC':
+                    long_st = crabJobStatusToString(st)
+                    #msg = "Job # %d is not submitted: status %s"%(nj+1, long_st)
+                    #common.logger.message(msg)
+                    continue
 
-            common.logger.message("Submitting job # "+`(nj+1)`)
-            jid = common.scheduler.submit(nj)
+                common.logger.message("Submitting job # "+`(nj+1)`)
+                print "submitting job!"
+                jid = common.scheduler.submit(nj)
 
-            common.jobDB.setStatus(nj, 'S')
-            common.jobDB.setJobId(nj, jid)
-            njs += 1
+                common.jobDB.setStatus(nj, 'S')
+                common.jobDB.setJobId(nj, jid)
+                njs += 1
 
-            destination = common.scheduler.queryDest(jid).split(":")[0]
-            print "Destinazione: ", destination
-            ID3 =  jid.split("/")[3]
-            broker = jid.split("/")[2].split(":")[0]
-            if st == 'C':
-                resFlag = 0
-            elif st == 'RC':
-                resFlag = 2
-            else:            
-                resFlag = 0
-                pass
+                ############################################   
 
-            Statistic.notify('submit',resFlag,'-----',self.dataset,self.owner,destination,broker,ID3,self.ID1,self.NJC)
-            
-            # List of parameters to be sent to ML monitor system
-            user = os.getlogin()
-            taskId = os.getlogin()+'_'+string.split(common.work_space.topDir(),'/')[-2] 
-            jobId = str(nj)
-            sid = jid
-            try:
-                application = os.path.basename(os.environ['SCRAMRT_LOCALRT'])
-            except KeyError:
-                application = os.path.basename(os.environ['LOCALRT'])
+                destination = common.scheduler.queryDest(jid).split(":")[0]
+                ID3 =  jid.split("/")[3]
+                broker = jid.split("/")[2].split(":")[0]
+                if st == 'C':
+                    resFlag = 0
+                elif st == 'RC':
+                    resFlag = 2
+                else:            
+                    resFlag = 0
+                    pass
 
-            nevtJob = common.jobDB.maxEvents(nj)
-            exe = self.cfg_params['USER.executable']
-            tool = common.prog_name
-            scheduler = self.cfg_params['CRAB.scheduler']
-            taskType = 'analysis'
-            vo = 'cms'
-            dataset = self.cfg_params['USER.dataset']
-            owner = self.cfg_params['USER.owner']
-            rb = sid.split(':')[1]
-            rb = rb.replace('//', '')
-            params = {'taskId': taskId, 'jobId': jobId, 'sid': sid, 'application': application, \
-                      'exe': exe, 'nevtJob': nevtJob, 'tool': tool, 'scheduler': scheduler, \
-                      'user': user, 'taskType': taskType, 'vo': vo, 'dataset': dataset, 'owner': owner, 'broker': rb}
-            mon.fillDict(params)
-            mon.sendToML()
-            pass
+                Statistic.notify('submit',resFlag,'-----',self.dataset,self.owner,destination,broker,ID3,self.ID1,self.NJC)
+                
+                # List of parameters to be sent to ML monitor system
+                user = os.getlogin()
+                taskId = os.getlogin()+'_'+string.split(common.work_space.topDir(),'/')[-2] 
+                jobId = str(nj)
+                sid = jid
+                try:
+                    application = os.path.basename(os.environ['SCRAMRT_LOCALRT'])
+                except KeyError:
+                    application = os.path.basename(os.environ['LOCALRT'])
+
+                nevtJob = common.jobDB.maxEvents(nj)
+                exe = self.cfg_params['USER.executable']
+                tool = common.prog_name
+                scheduler = self.cfg_params['CRAB.scheduler']
+                taskType = 'analysis'
+                vo = 'cms'
+                dataset = self.cfg_params['USER.dataset']
+                owner = self.cfg_params['USER.owner']
+                rb = sid.split(':')[1]
+                rb = rb.replace('//', '')
+                params = {'taskId': taskId, 'jobId': jobId, 'sid': sid, 'application': application, \
+                          'exe': exe, 'nevtJob': nevtJob, 'tool': tool, 'scheduler': scheduler, \
+                          'user': user, 'taskType': taskType, 'vo': vo, 'dataset': dataset, 'owner': owner, 'broker': rb}
+                mon.fillDict(params)
+                mon.sendToML()
         except:
-          common.jobDB.save()
-
+            common.jobDB.save()
+        common.jobDB.save()
+            
         msg = '\nTotal of %d jobs submitted'%njs
         if njs != len(self.nj_list) :
             msg += ' (from %d requested).'%(len(self.nj_list))
