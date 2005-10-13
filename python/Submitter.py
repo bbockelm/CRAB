990b7e0f1d4aa06eb5bb3ced79976fca4bc16749
slacapra <>
2005-10-13 17:24:12 +0000

    cosmetic

--- python/Submitter.py
+++ python/Submitter.py
@@ -44,7 +44,7 @@ class Submitter(Actor):
                 #print "st = ", st
                 if st != 'C' and st != 'K' and st != 'A' and st != 'RC':
                     long_st = crabJobStatusToString(st)
-                    msg = "Job # %d is not submitted: status %s"%(nj+1, long_st)
+                    msg = "Job # %d not submitted: status %s"%(nj+1, long_st)
                     common.logger.message(msg)
                     continue
 
