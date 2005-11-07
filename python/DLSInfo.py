0d5fe062fae230d83703fce1c80a1a8d80ee832b
afanfani <>
2005-11-07 23:20:21 +0000

    extract info from DLS using dls prototype command-line

---
+++ python/DLSInfo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import sys, os, commands,string, re
+from crab_util import *
+import common
+
+class DLSError:
+  def __init__(self, fileblocks):
+    print '\nERROR accessing DLS for fileblock '+fileblocks+'\n'
+    pass
+
+##############################################################################
+# Class to extract info from DLS 
+##############################################################################
+
+class DLSInfo:
+     def __init__(self, fileblocks):
+          self.fileblocks = fileblocks
+          self.DLSclient_ = 'DLSAPI/dls-get-se '
+          self.DLSServer_ = 'cmsbogw.bo.infn.it'
+
+# ####################################
+     def getReplicas(self):
+         """
+          query DLS to get replicas
+         """
+         ## 
+         cmd = self.DLSclient_+" --host "+self.DLSServer_+" --datablock "+self.fileblocks 
+         #print cmd
+         sites = runCommand(cmd)
+         ListSites=string.split(sites,'\n')
+         return ListSites         
