3f4c395159432a9363ec37cfa13435a34957fb17
afanfani <>
2005-12-11 18:51:17 +0000

    use the CERN DLS server lxgate10.cern.ch

--- python/DLSInfo.py
+++ python/DLSInfo.py
@@ -16,16 +16,17 @@ class DLSInfo:
      def __init__(self, fileblocks):
           self.fileblocks = fileblocks
           self.DLSclient_ = 'DLSAPI/dls-get-se '
-          self.DLSServer_ = 'cmsbogw.bo.infn.it'
+          self.DLSServer_ = 'lxgate10.cern.ch'
+          self.DLSServerPort_ = '18081'
+          #self.DLSServerPort_ = '18080'
 
 # ####################################
      def getReplicas(self):
          """
           query DLS to get replicas
          """
-         ## 
-         cmd = self.DLSclient_+" --host "+self.DLSServer_+" --datablock "+self.fileblocks 
-         #print cmd
+         ##
+         cmd = self.DLSclient_+" --port "+self.DLSServerPort_+" --host "+self.DLSServer_+" --datablock "+self.fileblocks 
          sites = runCommand(cmd)
          ListSites=string.split(string.strip(sites),'\n')
          return ListSites         
