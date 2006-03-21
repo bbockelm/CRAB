e05fbff496300021752e0dec89a6db92d3384f42
spiga <>
2006-03-21 15:50:09 +0000

    patch for LB slow query

--- python/Statistic.py
+++ python/Statistic.py
@@ -3,6 +3,7 @@
 from socket import *
 import os
 import common
+import time
 
 def Monitor(operation,Resubmit,jid,exitCode):
        fileCODE1 = open(common.work_space.shareDir()+"/.code","r")
@@ -18,13 +19,13 @@ def Monitor(operation,Resubmit,jid,exitCode):
            executable = array[4]
            pass
        fileCODE1.close()
-          
-       try:
-           dest = common.scheduler.queryDest(jid).split(":")[0]
-       except:
-           dest =  " "
-    
-
+       if operation != 'submit' :
+          try:
+              dest = common.scheduler.queryDest(jid).split(":")[0]
+          except:
+              dest =  " "
+       else:
+          dest =  " "       
        SID =  jid.split("/")[3]
        brok = jid.split("/")[2].split(":")[0]
 
@@ -35,14 +36,13 @@ def Monitor(operation,Resubmit,jid,exitCode):
        # for name in os.popen('whoami').readlines():
            name = name.strip()
            UIname = name.split(" ")[0]
-                                                                                                                             
-                                                                                                                             
+
        sockobj = socket(AF_INET,SOCK_DGRAM)
        sockobj.connect((address,port))
        if ( jobtype == 'ORCA' ) or ( jobtype == 'ORCA_DBSDLS'):
            sockobj.send(str(UIname)+'::'+str(operation)+'::'+str(jobtype)+'::'+str(Resubmit)+'::'+str(exitCode)+'::'+str(dataset)+'::'+str(owner)+'::'+str(dest)+'::'+str(brok)+'::'+str(SID)+'::'+str(time)+'::'+str(NjobCre))
        elif jobtype == 'FAMOS':
-           #sockobj.send(str(UIname)+'::'+str(operation)+'::'+str(jobtype)+'::'+str(Resubmit)+'::'+str(exitCode)+'::'+str(inputData)+'::'+str(executable)+'::'+str(dest)+'::'+str(brok)+'::'+str(SID)+'::'+str(time)+'::'+str(NjobCre))
+          # sockobj.send(str(UIname)+'::'+str(operation)+'::'+str(jobtype)+'::'+str(Resubmit)+'::'+str(exitCode)+'::'+str(inputData)+'::'+str(executable)+'::'+str(dest)+'::'+str(brok)+'::'+str(SID)+'::'+str(time)+'::'+str(NjobCre))
            pass 
        sockobj.close()
  
