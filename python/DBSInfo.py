9b8afed0f2635a50cc8d8446d5d5dd2977e14c7e
slacapra <>
2006-03-28 11:25:39 +0000

    fix indenting - 4 spaces, no tabcvs diff DBSInfo.py!

--- python/DBSInfo.py
+++ python/DBSInfo.py
@@ -4,115 +4,115 @@ import exceptions
 import common
 from crab_exceptions import *
 try:
- import dbsCgiApi
- import dbsApi
+    import dbsCgiApi
+    import dbsApi
 except:
-  try:
-   Crabpydir=commands.getoutput('which crab')
-   Topdir=string.replace(Crabpydir,'/python/crab','')
-   sys.path.append(Topdir+'/DBSAPI')
-   import dbsCgiApi
-   import dbsApi
-  except:
-   msg="ERROR no DBS API available"
-   raise CrabException(msg)
+    try:
+        Crabpydir=commands.getoutput('which crab')
+        Topdir=string.replace(Crabpydir,'/python/crab','')
+        sys.path.append(Topdir+'/DBSAPI')
+        import dbsCgiApi
+        import dbsApi
+    except:
+        msg="ERROR no DBS API available"
+        raise CrabException(msg)
 
 # #######################################
 class DBSError(exceptions.Exception):
-  def __init__(self, errorName, errorMessage):
-   args='\nERROR DBS %s : %s \n'%(errorName,errorMessage)
-   exceptions.Exception.__init__(self, args)
-   pass
+    def __init__(self, errorName, errorMessage):
+        args='\nERROR DBS %s : %s \n'%(errorName,errorMessage)
+        exceptions.Exception.__init__(self, args)
+        pass
 
-  def getErrorMessage(self):
-   """ Return exception error """
-   return "%s" % (self.args)
+    def getErrorMessage(self):
+        """ Return exception error """
+        return "%s" % (self.args)
 
 # #######################################
 class DBSInvalidDataTierError(exceptions.Exception):
-  def __init__(self, errorName, errorMessage):
-   args='\nERROR DBS %s : %s \n'%(errorName,errorMessage)
-   exceptions.Exception.__init__(self, args)
-   pass
+    def __init__(self, errorName, errorMessage):
+        args='\nERROR DBS %s : %s \n'%(errorName,errorMessage)
+        exceptions.Exception.__init__(self, args)
+        pass
                                                                                       
-  def getErrorMessage(self):
-   """ Return exception error """
-   return "%s" % (self.args)
+    def getErrorMessage(self):
+        """ Return exception error """
+        return "%s" % (self.args)
 
 # ####################################
 class DBSInfoError:
-  def __init__(self, url):
-    print '\nERROR accessing DBS url : '+url+'\n'
-    pass
+    def __init__(self, url):
+        print '\nERROR accessing DBS url : '+url+'\n'
+        pass
 
 ##################################################################################
 # Class to extract info from DBS 
 ###############################################################################
 
 class DBSInfo:
-     def __init__(self):
-          # Construct api object
-          self.api = dbsCgiApi.DbsCgiApi()
-          # Configure api logging level
-          self.api.setLogLevel(dbsApi.DBS_LOG_LEVEL_QUIET_)
-          #if common.logger.debugLevel() >= 4:
-          # self.api.setLogLevel(dbsApi.DBS_LOG_LEVEL_INFO_)
-          #if common.logger.debugLevel() >= 10:          
-          # self.api.setLogLevel(dbsApi.DBS_LOG_LEVEL_ALL_)
+    def __init__(self):
+        # Construct api object
+        self.api = dbsCgiApi.DbsCgiApi()
+        # Configure api logging level
+        self.api.setLogLevel(dbsApi.DBS_LOG_LEVEL_QUIET_)
+        #if common.logger.debugLevel() >= 4:
+        # self.api.setLogLevel(dbsApi.DBS_LOG_LEVEL_INFO_)
+        #if common.logger.debugLevel() >= 10:          
+        # self.api.setLogLevel(dbsApi.DBS_LOG_LEVEL_ALL_)
 
 # ####################################
-     def getMatchingDatasets (self, owner, dataset):
-         """ Query DBS to get provenance """
-         try:
-           list = self.api.listDatasets("/%s/*/%s" % (dataset, owner))
-         except dbsApi.InvalidDataTier, ex:
-           raise DBSInvalidDataTierError(ex.getClassName(),ex.getErrorMessage())
-         except dbsApi.DbsApiException, ex:
-           raise DBSError(ex.getClassName(),ex.getErrorMessage())
-         return list
+    def getMatchingDatasets (self, owner, dataset):
+        """ Query DBS to get provenance """
+        try:
+            list = self.api.listDatasets("/%s/*/%s" % (dataset, owner))
+        except dbsApi.InvalidDataTier, ex:
+            raise DBSInvalidDataTierError(ex.getClassName(),ex.getErrorMessage())
+        except dbsApi.DbsApiException, ex:
+            raise DBSError(ex.getClassName(),ex.getErrorMessage())
+        return list
 
 # ####################################
-     def getDatasetProvenance(self, path, dataTiers):
-         """
-          query DBS to get provenance
-         """
-         try:
-           datasetParentList = self.api.getDatasetProvenance(path,dataTiers)
-         except dbsApi.InvalidDataTier, ex:
-           raise DBSInvalidDataTierError(ex.getClassName(),ex.getErrorMessage())  
-         except dbsApi.DbsApiException, ex:
-           raise DBSError(ex.getClassName(),ex.getErrorMessage())
-         return datasetParentList                                                                                                            
-         #parent = {}
-         #for aparent in datasetParentList:
-         #  common.logger.debug(6, "DBSInfo: parent path is "+aparent.getDatasetPath()+" datatier is "+aparent.getDataTier())
-         #  parent[aparent.getDatasetPath()]=aparent.getDataTier()
-         #
-         #return parent
+    def getDatasetProvenance(self, path, dataTiers):
+        """
+        query DBS to get provenance
+        """
+        try:
+            datasetParentList = self.api.getDatasetProvenance(path,dataTiers)
+        except dbsApi.InvalidDataTier, ex:
+            raise DBSInvalidDataTierError(ex.getClassName(),ex.getErrorMessage())  
+        except dbsApi.DbsApiException, ex:
+            raise DBSError(ex.getClassName(),ex.getErrorMessage())
+        return datasetParentList                                                                                                            
+        #parent = {}
+        #for aparent in datasetParentList:
+        #  common.logger.debug(6, "DBSInfo: parent path is "+aparent.getDatasetPath()+" datatier is "+aparent.getDataTier())
+        #  parent[aparent.getDatasetPath()]=aparent.getDataTier()
+        #
+        #return parent
 
 # ####################################
-     def getDatasetContents(self, path):
-         """
-          query DBS to get event collections
-         """
-         try:
-           fileBlockList = self.api.getDatasetContents(path)
-         except dbsApi.DbsApiException, ex:
-           raise DBSError(ex.getClassName(),ex.getErrorMessage())
-         ## get the fileblock and event collections
-         nevtsbyblock= {}
-         for fileBlock in fileBlockList:
+    def getDatasetContents(self, path):
+        """
+        query DBS to get event collections
+        """
+        try:
+            fileBlockList = self.api.getDatasetContents(path)
+        except dbsApi.DbsApiException, ex:
+            raise DBSError(ex.getClassName(),ex.getErrorMessage())
+        ## get the fileblock and event collections
+        nevtsbyblock= {}
+        for fileBlock in fileBlockList:
             ## get the event collections for each block
             eventCollectionList = fileBlock.getEventCollectionList()
             nevts=0
             for eventCollection in eventCollectionList:
-              common.logger.debug(20,"DBSInfo:  evc: "+eventCollection.getCollectionName()+" nevts:%i"%eventCollection.getNumberOfEvents()) 
-              nevts=nevts+eventCollection.getNumberOfEvents()
+                common.logger.debug(20,"DBSInfo:  evc: "+eventCollection.getCollectionName()+" nevts:%i"%eventCollection.getNumberOfEvents()) 
+                nevts=nevts+eventCollection.getNumberOfEvents()
             common.logger.debug(6,"DBSInfo: total nevts %i in block %s "%(nevts,fileBlock.getBlockName()))
             nevtsbyblock[fileBlock.getBlockName()]=nevts
 
-         # returning a map of fileblock-nevts  will be enough for now
-         # TODO: in future the EvC collections grouped by fileblock should be returned
-         
-         return nevtsbyblock
+        # returning a map of fileblock-nevts  will be enough for now
+        # TODO: in future the EvC collections grouped by fileblock should be returned
+        
+        return nevtsbyblock
 
--- python/DLSInfo.py
+++ python/DLSInfo.py
@@ -6,54 +6,54 @@ from crab_util import *
 import common
 
 class DLSError:
-  def __init__(self, fileblocks):
-    print '\nERROR accessing DLS for fileblock '+fileblocks+'\n'
-    pass
+    def __init__(self, fileblocks):
+        print '\nERROR accessing DLS for fileblock '+fileblocks+'\n'
+        pass
 
 
 class DLSNoReplicas(exceptions.Exception):
-  def __init__(self, FileBlock):
-    args ="No replicas exists for fileblock: "+FileBlock+"\n"
-    exceptions.Exception.__init__(self, args)
-    pass
+    def __init__(self, FileBlock):
+        args ="No replicas exists for fileblock: "+FileBlock+"\n"
+        exceptions.Exception.__init__(self, args)
+        pass
 
-  def getClassName(self):
-    """ Return class name. """
-    return "%s" % (self.__class__.__name__)
+    def getClassName(self):
+        """ Return class name. """
+        return "%s" % (self.__class__.__name__)
 
-  def getErrorMessage(self):
-    """ Return exception error. """
-    return "%s" % (self.args)
+    def getErrorMessage(self):
+        """ Return exception error. """
+        return "%s" % (self.args)
 
 ##############################################################################
 # Class to extract info from DLS 
 ##############################################################################
 
 class DLSInfo:
-     def __init__(self, fileblocks):
-          self.fileblocks = fileblocks
-          self.DLSclient_ = 'dls-get-se '
-          self.DLSServer_ = 'lxgate10.cern.ch'
-          self.DLSServerPort_ = '18081'
-          #self.DLSServerPort_ = '18080'
-           
-          out=commands.getstatusoutput('which '+self.DLSclient_)
-          if out[0]>0:
-             msg="ERROR no DLS CLI available in $PATH : %s"%self.DLSclient_
-             raise CrabException(msg)
+    def __init__(self, fileblocks):
+        self.fileblocks = fileblocks
+        self.DLSclient_ = 'dls-get-se '
+        self.DLSServer_ = 'lxgate10.cern.ch'
+        self.DLSServerPort_ = '18081'
+        #self.DLSServerPort_ = '18080
+
+        out=commands.getstatusoutput('which '+self.DLSclient_)
+        if out[0]>0:
+            msg="ERROR no DLS CLI available in $PATH : %s"%self.DLSclient_
+            raise CrabException(msg)
 
 # ####################################
-     def getReplicas(self):
-         """
-          query DLS to get replicas
-         """
-         ##
-         cmd = self.DLSclient_+" --port "+self.DLSServerPort_+" --host "+self.DLSServer_+" --datablock "+self.fileblocks 
-         #print cmd
-         sites = runCommand(cmd)
-         sites=string.strip(sites)
-         if len(sites)<=0:
-           raise DLSNoReplicas(self.fileblocks)
+    def getReplicas(self):
+        """
+        query DLS to get replicas
+        """
+        ##
+        cmd = self.DLSclient_+" --port "+self.DLSServerPort_+" --host "+self.DLSServer_+" --datablock "+self.fileblocks 
+        #print cmd
+        sites = runCommand(cmd)
+        sites=string.strip(sites)
+        if len(sites)<=0:
+            raise DLSNoReplicas(self.fileblocks)
 
-         ListSites=string.split(string.strip(sites),'\n')
-         return ListSites         
+        ListSites=string.split(string.strip(sites),'\n')
+        return ListSites         
