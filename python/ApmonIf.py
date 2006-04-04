20a38b515ab40225378910304959a2a6c1d572f0
corvo <>
2006-04-04 16:13:47 +0000

    New version of ApmonIf

--- python/ApmonIf.py
+++ python/ApmonIf.py
@@ -1,39 +1,52 @@
-import apmon, sys, common
+from DashboardAPI import apmonSend, apmonFree    
 
 class ApmonIf:
     """
     Provides an interface to the Monalisa Apmon python module
     """
-    def __init__(self, address='http://lxgate35.cern.ch:40808/ApMonConf'):
-        self._params = {}
+    def __init__(self, taskid=None, jobid=None) :
+        self.taskId = taskid
+        self.jobId = jobid
         self.fName = 'mlCommonInfo'
-        self._MLaddress = address
-        self.apm = None
-        self.apm = self.getApmonInstance()
 
-    def fillDict(self, parr):
-        """
-        Used to fill the dictionary of key/value pair and instantiate the ML client
-        """
-        self._params = parr
+    #def fillDict(self, parr):
+    #    """
+    #    Obsolete
+    #    """
+    #    pass
     
-    def sendToML(self):
-        self.apm.sendParameters(self._params['taskId'], self._params['jobId'], self._params)
-        
-    def getApmonInstance(self):
-        if self.apm is None :
-            try:
-                apmonUrls = ["http://lxgate35.cern.ch:40808/ApMonConf", "http://monalisa.cacr.caltech.edu:40808/ApMonConf"]
-                apmInstance = apmon.ApMon(apmonUrls, apmon.Logger.FATAL);
-                if not apmInstance.initializedOK():
-                    print "It seems that ApMon cannot read its configuration. Setting the default destination"
-                apmInstance.setDestinations({'lxgate35.cern.ch:58884': {'sys_monitoring':0, 'general_info':0, 'job_monitoring':1, 'job_interval':300}});
-            except:
-                exctype, value = sys.exc_info()[:2]
-                print "ApmonIf::getApmonInstance Exception raised %s Value: %s"%(exctype, value)
-                common.logger.message("ApmonIf::getApmonInstance Exception raised: %s %s"%(exctype, value))
-                return
-        return apmInstance 
-        
+    def sendToML(self, params, jobid=None, taskid=None):
+        # Figure out taskId and jobId
+        taskId = 'unknown'
+        jobId = 'unknown'
+        # taskId
+        if self.taskId is not None :
+            taskId = self.taskId
+        if params.has_key('taskId') :
+            taskId = params['taskId']
+        if taskid is not None :
+            taskId = taskid
+        # jobId
+        if self.jobId is not None :
+            jobId = self.jobId
+        if params.has_key('jobId') :
+            jobId = params['jobId']
+        if jobid is not None :
+            jobId = jobid
+        # Send to Monalisa
+        apmonSend(taskId, jobId, params)
+            
     def free(self):
-        self.apm.free()
+        apmonFree()
+
+if __name__ == '__main__' :
+    apmonIf = ApmonIf()
+    apmonIf.sendToML({'taskId' : 'Test-ApmonIf', 'jobId': 'jobid', 'param-to-send':'value-to-send'})
+    apmonIf.free()
+    apmonIf = ApmonIf('Test-ApmonIf-Constuctor-Argument')
+    apmonIf.sendToML({'jobId': 'jobid', 'param-to-send':'value-to-send'})
+    apmonIf.sendToML({'param-to-send':'value-to-send'}, 'jobid-method-argument')
+    apmonIf.sendToML({'param-to-send':'value-to-send'}, 'jobid-method-argument', 'Test-ApmonIf-Override-Argument')
+    apmonIf.sendToML({'taskId': 'Test-ApmonIf-Override-Params','param-to-send':'value-to-send'}, 'jobid-method-argument')
+    apmonIf.free()
+    
