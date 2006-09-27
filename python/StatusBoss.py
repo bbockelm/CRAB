d5894adc5cef793c01f55c9b78e09a9e3526c330
gutsche <>
2006-09-27 16:18:11 +0000

    cosmetics, nice output of status check, better status messages during output retrieval, comma separated lists of jobs in summary of status check

--- python/StatusBoss.py
+++ python/StatusBoss.py
@@ -8,14 +8,15 @@ class StatusBoss(Actor):
     def __init__(self, *args):
         self.cfg_params = args[0]
         self.countToTjob = 0
-        self.countCreated = 0
-        self.countDone = 0
-        self.countRun = 0
-        self.countSched = 0
-        self.countReady = 0
-        self.countCancel = 0
-        self.countAbort = 0
-        self.countCleared = 0
+        self.countCreated = []
+        self.countDone = []
+        self.countRun = []
+        self.countSched = []
+        self.countReady = []
+        self.countCancel = []
+        self.countAbort = []
+        self.countCorrupt = []
+        self.countCleared = []
         return
 
     def run(self):
@@ -88,21 +89,39 @@ class StatusBoss(Actor):
                     CoupJobsID[int(js[0])]=int(js[0])
             nline = nline+1
 
+        # format output
+        # header: field length 8
+        # status: field length 18
+        # E_HOST: field length 40
+        # EXE_EXIT_CODE: field lenght 13
+        # JOB_EXIT_CODE: field lenght 15
+
         printline = ''
-        printline+=header[0]
-        printline+='   STATUS          E_HOST            EXE_EXIT_CODE        JOB_EXIT_STATUS'
+        printline+= "%-8s " % header[0]
+        printline+= "%-18s %-40s %-13s %-15s" % ('STATUS','E_HOST','EXE_EXIT_CODE','JOB_EXIT_STATUS')
         print printline
-        for_summary = []
+        print '---------------------------------------------------------------------------------------------------'
+        for_summary = {}
         orderdBossID = CoupJobsID.values()
+        counter = 0
         for bossid in orderdBossID:
+            # every 10 jobs, print a line for orientation
+            if counter != 0 and counter%10 == 0 :
+                print '---------------------------------------------------------------------------------------------------'
+            counter += 1
             printline=''
             jobStatus=''
-            jobStatus = common.scheduler.queryStatus(bossTaskId, bossid)
+
+            # if JobDB status is 'Z', corrupted output tarball, don't check status
+            if common.jobDB.status(int(jobAttributes[bossid][0].strip())-1) == 'Z' :
+                jobStatus = 'Cleared (Corrupt)'
+            else :
+                jobStatus = common.scheduler.queryStatus(bossTaskId, bossid)
             # debug
             msg = 'jobStatus' + jobStatus
             common.logger.debug(4,msg)
             ###
-            for_summary.append(jobStatus)
+            for_summary[int(jobAttributes[bossid][0].strip())] = jobStatus
             exe_code =jobAttributes[bossid][2]   ##BOSS4 EXE_EXIT_CODE
    
         ###########------> This info must be come from BOSS4      DS.
@@ -123,16 +142,16 @@ class StatusBoss(Actor):
  
             job_exit_status = jobAttributes[bossid][4]   ##BOSS4 JOB_EXIT_STATUS
             
-            if jobStatus == 'Done (Success)' or jobStatus == 'Cleared(BOSS)':
-                printline+=jobAttributes[bossid][0]+' '+jobStatus+'   '+dest+'      '+exe_code+'       '+job_exit_status
-            elif jobStatus == 'Created(BOSS)':
+            if jobStatus == 'Done (Success)' or jobStatus == 'Cleared':
+                printline+="%-8s %-18s %-40s %-13s %-15s" % (jobAttributes[bossid][0],jobStatus,dest,exe_code,job_exit_status)
+            elif jobStatus == 'Created':
                 pass
                 #self.countCreated = self.countCreated + 1
                 #printline+=' '+jobStatus+'   '+dest+'      '+exe_code+'       '+job_exit_status
             else:
-                printline+=jobAttributes[bossid][0]+' '+jobStatus+'   '+dest
+                printline+="%-8s %-18s %-40s %-13s %-15s" % (jobAttributes[bossid][0],jobStatus,dest,'','')
             resFlag = 0
-            if jobStatus != 'Created(BOSS)'  and jobStatus != 'Unknown(BOSS)':
+            if jobStatus != 'Created'  and jobStatus != 'Unknown':
                 jid1 = string.strip(jobAttributes[bossid][2])
 
         ##########--------> for the moment this is out, when BOSS will know also the ce we reimplement it  DS. 
@@ -161,63 +180,89 @@ class StatusBoss(Actor):
 
         # moved loading of jobDB before boss status check to enable condor_g scheduler to query jobdb for efficient access to destination
         # common.jobDB.load()
-        nj = 0
-        for status in statusList:
-            if status == 'Created(BOSS)':
-                self.countCreated = self.countCreated + 1
-            if status == 'Done (Success)' or status == 'Done (Aborted)':
-                self.countDone = self.countDone + 1
-	        common.jobDB.setStatus(nj, 'D')
-            elif status == 'Running' :
-                self.countRun = self.countRun + 1
-            elif status == 'Scheduled' :
-                self.countSched = self.countSched + 1
-            elif status == 'Ready' :
-                self.countReady =  self.countReady + 1
-            elif status == 'Cancelled' or status == 'Killed(BOSS)':
-                self.countCancel =  self.countCancel + 1
-                common.jobDB.setStatus(nj, 'K')
-            elif status == 'Aborted':
-                self.countAbort =  self.countAbort + 1
-                common.jobDB.setStatus(nj, 'A')
-            elif status == 'Cleared(BOSS)':
-                self.countCleared = self.countCleared + 1
+        for id in statusList.keys():
+            if statusList[id] == 'Created':
+                self.countCreated.append(id)
+            elif statusList[id] == 'Done (Success)' or statusList[id] == 'Done (Aborted)':
+                self.countDone.append(id)
+	        common.jobDB.setStatus(int(id)-1, 'D')
+            elif statusList[id] == 'Running' :
+                self.countRun.append(id)
+            elif statusList[id] == 'Scheduled' :
+                self.countSched.append(id)
+            elif statusList[id] == 'Ready' :
+                self.countReady.append(id)
+            elif statusList[id] == 'Cancelled' or statusList[id] == 'Killed':
+                self.countCancel.append(id)
+                common.jobDB.setStatus(int(id)-1, 'K')
+            elif statusList[id] == 'Aborted':
+                self.countAbort.append(id)
+                common.jobDB.setStatus(int(id)-1, 'A')
+            elif statusList[id] == 'Cleared (Corrupt)':
+                self.countCorrupt.append(id)
+                common.jobDB.setStatus(int(id)-1, 'Z')
+            elif statusList[id] == 'Cleared':
+                self.countCleared.append(id)
                 pass 
-            nj = nj + 1   
 
         common.jobDB.save()
         common.logger.debug(5,'done loop StatusBoss::report')
  
-        self.countToTjob = (len(statusList)) 
+        self.countToTjob = (len(statusList.keys())) 
         return
 
     def PrintReport_(self):
         print ''
         print ">>>>>>>>> %i Total Jobs " % (self.countToTjob)
 
-        if (self.countCreated != 0):
+        if (len(self.countCreated) != 0):
             print ''
-            print ">>>>>>>>> %i Jobs Created" % (self.countCreated)
-        if (self.countReady != 0):
+            print ">>>>>>>>> %i Jobs Created" % len(self.countCreated)
+        if (len(self.countReady) != 0):
             print ''
-            print ">>>>>>>>> %i Jobs Ready" % (self.countReady)
-        if (self.countSched != 0):
+            print ">>>>>>>>> %i Jobs Ready" % len(self.countReady)
+        if (len(self.countSched) != 0):
             print ''
-            print ">>>>>>>>> %i Jobs Scheduled" % (self.countSched)
-        if (self.countRun != 0):
+            print ">>>>>>>>> %i Jobs Scheduled" % len(self.countSched)
+        if (len(self.countRun) != 0):
             print ''
-            print ">>>>>>>>> %i Jobs Running" % (self.countRun)
-        if (self.countCancel != 0) or (self.countAbort != 0) or (self.countCleared != 0):
+            print ">>>>>>>>> %i Jobs Running" % len(self.countRun)
+        if (len(self.countCancel) != 0):
             print ''
-            tot = int(self.countAbort) + int(self.countCancel) + int(self.countCleared)
-            print ">>>>>>>>> %i Jobs killed or Aborted or Cleared" % (tot)
+            print ">>>>>>>>> %i Jobs canceled/killed" % len(self.countCancel)
+            print "          List of jobs: %s" % self.joinIntArray_(self.countCancel)
             print "          You can resubmit them specifying JOB numbers: crab -resubmit JOB_number (or range of JOB)" 
             print "          (i.e -resubmit 1-3 => 1 and 2 and 3 or -resubmit 1,3 => 1 and 3)"       
-        if (self.countDone != 0):
-            print ">>>>>>>>> %i Jobs Done" % (self.countDone)
+        if (len(self.countAbort) != 0):
+            self.countAbort.sort()
+            print ''
+            print ">>>>>>>>> %i Jobs aborted" % len(self.countAbort)
+            print "          List of jobs: %s" % self.joinIntArray_(self.countAbort)
+            print "          You can resubmit them specifying JOB numbers: crab -resubmit JOB_number (or range of JOB)" 
+            print "          (i.e -resubmit 1-3 => 1 and 2 and 3 or -resubmit 1,3 => 1 and 3)"       
+        if (len(self.countDone) != 0):
+            self.countDone.sort()
+            print ''
+            print ">>>>>>>>> %i Jobs Done" % len(self.countDone)
+            print "          List of jobs: %s" % self.joinIntArray_(self.countDone)
             print "          Retrieve them with: crab -getoutput to retrieve all" 
             print "          or specifying JOB numbers (i.e -getoutput 1-3 => 1 and 2 and 3 or -getoutput 1,3 => 1 and 3)"
-            print('\n')  
-        pass
+        if (len(self.countCorrupt) != 0):
+            self.countCorrupt.sort()
+            print ''
+            print ">>>>>>>>> %i Jobs cleared with corrupt output" % len(self.countCorrupt)
+            print "          List of jobs: %s" % self.joinIntArray_(self.countCorrupt)
+            print "          You can resubmit them specifying JOB numbers: crab -resubmit JOB_number (or range of JOB)" 
+            print "          (i.e -resubmit 1-3 => 1 and 2 and 3 or -resubmit 1,3 => 1 and 3)"       
+        if (len(self.countCleared) != 0):
+            print ''
+            print ">>>>>>>>> %i Jobs cleared" % len(self.countCleared)
          
 
+    def joinIntArray_(self,array) :
+        output = ''
+        for item in array :
+            output += str(item)+','
+        if output[-1] == ',' :
+            output = output[:-1]
+        return output
