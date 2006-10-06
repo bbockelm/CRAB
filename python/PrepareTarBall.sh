ce3108f8617c31a3599e63797d5cd05e65d08d80
slacapra <>
2006-10-06 10:19:08 +0000

    update DLS version

--- python/PrepareTarBall.sh
+++ python/PrepareTarBall.sh
@@ -2,7 +2,7 @@
 
 ### $1 is the tag value CRAB_X_X_X ###
 if [ $# -lt 1 ]; then
-  echo "Usage: `basename $0` <CRAB_X_Y_Z> <BOSS version: X_Y_Z>"
+  echo "Usage: `basename $0` <CRAB_X_Y_Z> <BOSS X_Y_Z>"
   exit 1
 fi
 tag=$1
@@ -14,21 +14,19 @@ CRABdir="$tag"
 echo "CRABDIR = $CRABDIR"
 CRABtag=$tag
 DBSAPItag="DBS_0_0_3"
-DLSAPItag="DLS_0_1_0"
+DLSAPItag="DLS_0_1_1"
 
 CVSrepo=":pserver:anonymous@cmscvs.cern.ch:/cvs_server/repositories"
 export CVSROOT=${CVSrepo}"/CMSSW"
 
 #configure_dbsdls=configure_dbsdls
 ## download CRAB from CVS and cleanup the code a bit
-#echo ">> downloading CRAB tag $CRABtag from CVS CRAB"
-echo ">> downloading CRAB HEAD from CVS CRAB"
+echo ">> downloading CRAB tag $CRABtag from CVS CRAB"
+#echo ">> downloading CRAB HEAD from CVS CRAB"
 #cvs co -r $CRABtag -d $CRABdir CRAB
 cvs co -d $CRABdir CRAB
-#cvs up -P BossScript 
 cd $CRABdir
-#rm -r CVS
-#rm -r python/CVS
+cvs up -P python/BossScript 
 chmod -x python/crab.py
 rm python/crab.*sh
 rm python/tar*
@@ -36,14 +34,8 @@ rm python/zero
 mv python/configure .
 mv python/configureBoss .
 
-#rm configure 
-#mv $configure_dbsdls configure
-#less python/crab.cfg | sed -e "s?jobtype = orca?jobtype = orca_dbsdls?" >  crab.cfg.tmp
-#mv crab.cfg.tmp python/crab.cfg
-#echo "--> configure crab.cfg to set by default the jobtype to orca_dbsdls"
-
 ## download Boss
-echo ">> downloading BOSS from http://boss.bo.infn.it/boss-v$2-bin.tar.gz "
+echo ">> downloading BOSS version ${boss_version} from http://boss.bo.infn.it/boss-v$2-bin.tar.gz "
 mkdir Boss
 cd Boss
 wget http://boss.bo.infn.it/BOSS_${boss_version}-bin.tar.gz
@@ -51,7 +43,7 @@ wget http://boss.bo.infn.it/BOSS_${boss_version}-bin.tar.gz
 # mv BOSS-bin.tgz boss-v${boss_version}-bin.tar.gz
 
 ## download DBS API
-echo ">> downloading DBS API tag $DBSAPItag from CVS DBS/Clients/PythonAPI"
+echo ">> downloading DBS API tag ${DBSAPItag} from CVS DBS/Clients/PythonAPI"
 cd ..
 cvs co -r ${DBSAPItag} -d DBSAPI COMP/DBS/Clients/PythonAPI
 #rm -r DBSAPI/CVS
