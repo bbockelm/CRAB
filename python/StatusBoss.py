cdcd4cadc6e1f0eb480cd0dd0f6730e56ca77b2b
slacapra <>
2006-10-09 15:27:55 +0000

    correct list index to get sid 2->1

--- python/StatusBoss.py
+++ python/StatusBoss.py
@@ -166,9 +166,9 @@ class StatusBoss(Actor):
    ##                 Statistic.Monitor('checkstatus',resFlag,jid1,exe_code)   
 
                 if int(self.cfg_params['USER.activate_monalisa']) == 1:
-                    params = {'taskId': self.cfg_params['taskId'], 'jobId': str(bossid) + '_' + string.strip(jobAttributes[bossid][2]), \
-                    'sid': string.strip(jobAttributes[bossid][2]), 'StatusValueReason': common.scheduler.getAttribute(string.strip(jobAttributes[bossid][2]), 'reason'), \
-                    'StatusValue': jobStatus, 'StatusEnterTime': common.scheduler.getAttribute(string.strip(jobAttributes[bossid][2]), 'stateEnterTime'), 'StatusDestination': dest}
+                    params = {'taskId': self.cfg_params['taskId'], 'jobId': str(bossid) + '_' + string.strip(jobAttributes[bossid][1]), \
+                    'sid': string.strip(jobAttributes[bossid][2]), 'StatusValueReason': common.scheduler.getAttribute(string.strip(jobAttributes[bossid][1]), 'reason'), \
+                    'StatusValue': jobStatus, 'StatusEnterTime': common.scheduler.getAttribute(string.strip(jobAttributes[bossid][1]), 'stateEnterTime'), 'StatusDestination': dest}
                     self.cfg_params['apmon'].sendToML(params)
             if printline != '': 
                 print printline
