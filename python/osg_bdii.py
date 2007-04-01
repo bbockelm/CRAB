dda980e8ad6b059b65e3d6b59667d176edfdc5b3
gutsche <>
2007-04-01 22:41:27 +0000

    improvements in OSG usage of BDII

--- python/osg_bdii.py
+++ python/osg_bdii.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
 import re, popen2, sys
-
-def runldapquery(filter, attribute):
-    bdii = 'exp-bdii.cern.ch'
+def unwraplines(wrapped_list):
+    r = re.compile('^ (.*)$')
+    unwrapped_list = []
+    for l in wrapped_list:
+        m = r.match(l)
+        if m:
+            unwrapped_list[-1] += m.groups()[0] 
+        else:
+            unwrapped_list.append(l.rstrip())
+        
+    return unwrapped_list        
+        
+    
+def runldapquery(filter, attribute, bdii):
+#    bdii = 'exp-bdii.cern.ch'
     command = 'ldapsearch -xLLL -p 2170 -h '+bdii+' -b o=grid '
     command += filter+' '+attribute
 
@@ -11,16 +23,17 @@ def runldapquery(filter, attribute):
     pout = pout.readlines()
     p = perr.readlines()
 
+    pout = unwraplines(pout)
     if (p):
         for l in p: print l
         raise RuntimeError('ldapsearch call failed')
 
     return pout
 
-def jm_from_se_bdii(se):
+def jm_from_se_bdii(se, bdii='exp-bdii.cern.ch'):
     se = '\''+se+'\''
     pout = runldapquery(''' '(GlueCESEBindGroupSEUniqueID='''+se+''')' ''',
-                        'GlueCESEBindGroupCEUniqueID')
+                        'GlueCESEBindGroupCEUniqueID', bdii)
 
     r = re.compile('^GlueCESEBindGroupCEUniqueID: (.*:.*/jobmanager-.*)-cms')
     jm = []
@@ -34,7 +47,7 @@ def jm_from_se_bdii(se):
     return jm
 
     
-def cestate_from_se_bdii(se):
+def cestate_from_se_bdii(se, bdii='exp-bdii.cern.ch' ):
     status = []
     jmlist = jm_from_se_bdii(se)
 
@@ -42,7 +55,7 @@ def cestate_from_se_bdii(se):
         jm += "-cms"
 
         pout = runldapquery(''' '(&(objectClass=GlueCEState)(GlueCEUniqueID='''+
-        jm+'''))' ''', 'GlueCEStateStatus')
+        jm+'''))' ''', 'GlueCEStateStatus', bdii)
 
         r = re.compile('^GlueCEStateStatus: (.*)')
         for l in pout:
@@ -52,9 +65,9 @@ def cestate_from_se_bdii(se):
 
     return status            
 
-def cestate_from_ce_bdii(ce):
+def cestate_from_ce_bdii(ce, bdii='exp-bdii.cern.ch'):
     pout = runldapquery(''' '(&(objectClass=GlueCEState)(GlueCEInfoHostName='''+
-    ce+''')(GlueCEAccessControlBaseRule=VO:cms))' ''', 'GlueCEStateStatus')
+    ce+''')(GlueCEAccessControlBaseRule=VO:cms))' ''', 'GlueCEStateStatus', bdii)
 
     status = ''
     r = re.compile('^GlueCEStateStatus: (.*)')
