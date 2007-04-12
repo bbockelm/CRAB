328319f19b46bdce213fc78c7dc0a65853b45a84
mcinquil <>
2007-04-12 13:25:20 +0000

    Added 'proxy', 'status' and 'uuid' columns

--- CRABSERVER/src/sql/CrabServerDB/CrabServerDB.sql
+++ CRABSERVER/src/sql/CrabServerDB/CrabServerDB.sql
@@ -6,6 +6,9 @@ CREATE TABLE js_taskInstance (
      tresholdLevel int (3) UNSIGNED NOT NULL default '100',
      notificationSent int (1) NOT NULL default '0',
      endedLevel INT(3) UNSIGNED NOT NULL default '0',
+     proxy varchar(255) NOT NULL default '',
+     uuid varchar(255) NOT NULL default '',
+     status varchar(255) NOT NULL default 'not submitted',
      primary key(id),
      unique(taskName),
      key(taskName)
