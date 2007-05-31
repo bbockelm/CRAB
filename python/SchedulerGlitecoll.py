d2ee5177eb2258f167fa61b7e556dfb5e9b549f7
bbockelm <>
2007-05-31 14:47:42 +0000

    This commit was manufactured by cvs2svn to create tag 'CRABSERVER_0_0_1_pre6'.
    Sprout from master 2007-05-31 14:47:41 UTC mcinquil 'Support of kill functionalities'
    Delete:
        PsetCode/.cvsignore
        PsetCode/CfgInterface.py
        PsetCode/InputSource.py
        PsetCode/OutputModule.py
        PsetCode/Utilities.py
        PsetCode/cmsconfig.py
        TestSuite/.cvsignore
        TestSuite/History.py
        TestSuite/LinearTester.py
        TestSuite/Parser.py
        TestSuite/ProxyInit.py
        TestSuite/README
        TestSuite/Runner.py
        TestSuite/Session.py
        TestSuite/TestCreator
        TestSuite/TestSuite
        TestSuite/TestSuite.cfg
        TestSuite/TestSuite.csh
        TestSuite/TestSuite.py
        TestSuite/TestSuite.sh
        TestSuite/Tester.py
        TestSuite/WowTester.py
        TestSuite/crab.cfg
        TestSuite/pythia.cfg
        TestSuite/readPSetTrivial.cfg
        python/.cvsignore
        python/00INSTALL
        python/Actor.py
        python/AnalyzeProfile.py
        python/ApmonIf.py
        python/Checker.py
        python/Cleaner.py
        python/CondorGLoggingInfo.py
        python/Creator.py
        python/DBSInfo.py
        python/DLSInfo.py
        python/DashboardAPI.py
        python/DataDiscovery.py
        python/DataDiscovery_DBS2.py
        python/DataLocation.py
        python/EdgConfig.py
        python/EdgLoggingInfo.py
        python/GetOutputServer.py
        python/GliteConfig.py
        python/JobDB.py
        python/JobList.py
        python/JobType.py
        python/Logger.py
        python/Makefile
        python/NameSpace.py
        python/PostMortem.py
        python/PrepareTarBall.sh
        python/ProcInfo.py
        python/ProgressBar.py
        python/PsetManipulator.py
        python/Scheduler.py
        python/SchedulerBoss.py
        python/SchedulerCondor_g.py
        python/SchedulerEdg.py
        python/SchedulerGlite.py
        python/SchedulerGlitecoll.py
        python/Scram.py
        python/ScriptWriter.py
        python/Statistic.py
        python/Status.py
        python/StatusBoss.py
        python/StatusServer.py
        python/Submitter.py
        python/SubmitterServer.py
        python/TaskDB.py
        python/TerminalController.py
        python/UnserializePHP.py
        python/WorkSpace.py
        python/apmon.py
        python/asap-user-register
        python/checkPyXML.py
        python/cms_cmssw.py
        python/cms_site_catalogue.sh
        python/common.py
        python/configure
        python/configureBoss
        python/crab
        python/crab.cfg
        python/crab.csh
        python/crab.py
        python/crab.sh
        python/crab_exceptions.py
        python/crab_help.py
        python/crab_logger.py
        python/crab_template.sh
        python/crab_util.py
        python/osg_bdii.py
        python/parseCrabFjr.py
        python/pythia.cfg
        python/report.py
        python/runSimHitAnalyzer.cfg
        python/zero
        script/CMSSW.xml
        script/CONDOR_G.xml
        script/CRAB.xml
        script/EDG.xml
        script/GLITE.xml
        script/GLITECOLL.xml
        script/MySQLConfig.sql
        script/MySQLConfig.sql_grid
        script/MySQLRTMon.xml
        script/cmssw.preruntime
        script/cmssw.runtime
        script/cmssw.schema
        script/crab.preruntime
        script/crab.runtime
        script/crab.schema
        script/dbDirectUpdatorMySQL
        script/lb.schema
        script/replaceBOSSDIR.sh

--- python/SchedulerGlitecoll.py
+++
@@ -1,6 +0,0 @@
-#from Scheduler import Scheduler
-from SchedulerGlite import SchedulerGlite
-
-class SchedulerGlitecoll(SchedulerGlite):
-    def __init__(self):
-        SchedulerGlite.__init__(self)
