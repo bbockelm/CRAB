738fb2bc16ceb0bc1c057eb7dd37016774e1a330
spiga <>
2007-06-03 17:52:05 +0000

    first commit

---
+++ python/PostMortemServer.py
@@ -0,0 +1,73 @@
+from Actor import *
+from crab_util import *
+import common
+from ApmonIf import ApmonIf
+import Statistic
+import time
+from ProgressBar import ProgressBar
+from TerminalController import TerminalController
+
+import xml.dom.minidom
+import xml.dom.ext
+
+class PostMortemServer(Actor):
+ 
+    def __init__(self, cfg_params,):
+        self.cfg_params = cfg_params
+        try:  
+            self.server_name = self.cfg_params['CRAB.server_name'] # gsiftp://pcpg01.cern.ch/data/SEDir/
+        except KeyError:
+            msg = 'No server selected ...' 
+            msg = msg + 'Please specify a server in the crab cfg file' 
+            raise CrabException(msg) 
+        return
+    
+    def run(self):
+        """
+        The main method of the class: retrieve the post mortem output from server
+        """
+        common.logger.debug(5, "PostMortem server::run() called")
+
+        start = time.time()
+
+        common.scheduler.checkProxy()
+
+        common.taskDB.load()
+        WorkDirName =os.path.basename(os.path.split(common.work_space.topDir())[0])
+        projectUniqName = 'crab_'+str(WorkDirName)+'_'+common.taskDB.dict("TasKUUID")     
+        #Need to add a check on the treashold level 
+        # and on the task readness  TODO  
+        try:
+            ### retrieving poject from the server
+            common.logger.message("Retrieving the poject from the server...\n")
+
+            copyHere = common.work_space.jobDir() # MATT
+             
+            cmd = 'lcg-cp --vo cms --verbose gsiftp://' + str(self.server_name) + str(projectUniqName)+'/res/failed.tgz file://'+copyHere+'failed.tgz'# MATT
+            common.logger.debug(5, cmd)
+            copyOut = os.system(cmd +' >& /dev/null')
+        except:
+            msg = ("postMortem output not yet available")
+            raise CrabException(msg)
+
+        zipOut = "failed.tgz"
+        if os.path.exists( copyHere + zipOut ): # MATT
+            cwd = os.getcwd()
+            os.chdir( copyHere )# MATT
+            common.logger.debug( 5, 'tar -zxvf ' + zipOut )
+  	    cmd = 'tar -zxvf ' + zipOut 
+            cmd += '; mv .tmpFailed/* .; rm -drf .tmpDone/'
+	    cmd_out = runCommand(cmd)
+	    os.chdir(cwd)
+            common.logger.debug( 5, 'rm -f '+copyHere+zipOut )# MATT 
+	    cmd = 'rm -f '+copyHere+zipOut# MATT
+	    cmd_out = runCommand(cmd)
+
+	    msg='Logging info for project '+str(WorkDirName)+': \n'      
+	    msg+='written to '+copyHere+' \n'      # MATT
+	    common.logger.message(msg)
+        else:
+            common.logger.message("Logging info is not yet ready....\n")
+
+        return
+
