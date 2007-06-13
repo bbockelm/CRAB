4105cc8839c9d45c25ff6a245a0a1dd0c3be1a53
slacapra <>
2007-06-13 11:09:26 +0000

    add check if both copy_data and return_data are set to 1: raise exception

--- python/SchedulerEdg.py
+++ python/SchedulerEdg.py
@@ -81,7 +81,12 @@ class SchedulerEdg(Scheduler):
 
         if ( int(self.return_data) == 0 and int(self.copy_data) == 0 ):
            msg = 'Warning: return_data = 0 and copy_data = 0 ==> your exe output will be lost\n' 
-           msg = msg + 'Please modify return_data and copy_data value in your crab.cfg file\n' 
+           msg = msg + 'Please modify return_data or copy_data value in your crab.cfg file\n' 
+           raise CrabException(msg)
+
+        if ( int(self.return_data) == 1 and int(self.copy_data) == 1 ):
+           msg = 'Warning: return_data = 1 and copy_data = 1\n' 
+           msg = msg + 'Please modify return_data or copy_data value in your crab.cfg file\n' 
            raise CrabException(msg)
 
         try:
