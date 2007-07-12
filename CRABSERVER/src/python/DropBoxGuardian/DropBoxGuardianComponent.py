6d889ffed39b609c0d326b1a5cffe7d00ef87a39
farinafa <>
2007-07-12 14:21:47 +0000

    Updated DropBoxGuardian with polling messages

--- CRABSERVER/src/python/DropBoxGuardian/DropBoxGuardianComponent.py
+++ CRABSERVER/src/python/DropBoxGuardian/DropBoxGuardianComponent.py
@@ -4,8 +4,8 @@ _DropBoxGuardianComponent_
 
 """
 
-__version__ = "$Revision: 1.1 $"
-__revision__ = "$Id: DropBoxGuardianComponent.py,v 1.1 2006/11/08 18:12:00 farinafa Exp $"
+__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: DropBoxGuardianComponent.py,v 1.4 2007/05/11 15:17:18 farinafa Exp $"
 
 import os
 import socket
@@ -13,7 +13,6 @@ import pickle
 import logging
 import time
 from logging.handlers import RotatingFileHandler
-
 from MessageService.MessageService import MessageService
 
 class DropBoxGuardianComponent:
@@ -28,8 +27,7 @@ class DropBoxGuardianComponent:
         self.args['ProxiesDir'] = None
         self.args['Logfile'] = None
         self.args.update(args)
-        
-           
+                  
         if self.args['Logfile'] == None:
             self.args['Logfile'] = os.path.join(self.args['ComponentDir'],
                                                 "ComponentLog")
@@ -51,6 +49,8 @@ class DropBoxGuardianComponent:
         
         # Load the drop box set file
         self.sleepTime = int(self.args['sleepTime'])
+        logging.info("DropBox polling time: 00:%.2d:%.2d"%(self.sleepTime/60, self.sleepTime%60) )
+
         self.dropBoxPath = str(self.args['dropBoxPath'])
         self.proxyDir = str(self.args['ProxiesDir'])
 	
@@ -91,15 +91,17 @@ class DropBoxGuardianComponent:
 
         """
         logging.debug("Event: %s %s" % (event, payload))
-        # No specific event handling for these component
-        # It just push tickets
+
+        if event == "DropBoxGuardianComponent:poll":
+            self.poll()
+            return
         if event == "DropBoxGuardianComponent:StartDebug":
             logging.getLogger().setLevel(logging.DEBUG)
             return
         if event == "DropBoxGuardianComponent:EndDebug":
             logging.getLogger().setLevel(logging.INFO)
             return
-        return 
+        return
         
     def startComponent(self):
         """
@@ -115,76 +117,91 @@ class DropBoxGuardianComponent:
         self.ms.registerAs("DropBoxGuardianComponent")
                                                                                 
         # subscribe to messages
+        self.ms.subscribeTo("DropBoxGuardianComponent:poll")
         self.ms.subscribeTo("DropBoxGuardianComponent:StartDebug")
         self.ms.subscribeTo("DropBoxGuardianComponent:EndDebug")
 
+        # generate first polling cycle
+        self.ms.remove("DropBoxGuardianComponent:poll")
+        self.ms.publish("DropBoxGuardianComponent:poll", "")
+        self.ms.commit()
+
+        # dispatch messages
         while True:
-            ## Watch the dropbox and find new arrivals
-            time.sleep(self.sleepTime)
-            dropBoxStatus = os.listdir(self.dropBoxPath)
+             messageType, payload = self.ms.get()
+             self.ms.commit()
+             self.__call__(messageType, payload)
+        pass
+
+    def poll(self):
+        ## Watch the dropbox and find new arrivals
+        dropBoxStatus = os.listdir(self.dropBoxPath)
 	    
-            # Marco. Modified for Kill command
-            newCommand = []
-            newCommand = [s for s in dropBoxStatus if (s not in self.previousDropBoxStatus) and (s.split('.')[-1]=='xml')]
+        # Marco. Modified for Kill command
+        newCommand = []
+        newCommand = [s for s in dropBoxStatus if (s not in self.previousDropBoxStatus) and (s.split('.')[-1]=='xml')]
 
-            # Watch for new projects
-            newFiles = []
-	    preservedTars = []
-            ## newFiles = [s.split('.tgz')[0] for s in dropBoxStatus if (s not in self.previousDropBoxStatus) and (s.split('.')[-1]=='tgz')]
-            # gridFTP tar arrival bugfix -- Fabio
-            for s in dropBoxStatus:
-                 if s.split('.')[-1]=='tgz' and (s not in self.previousDropBoxStatus):
-                     tarSize = 0
-                     try:
-                          tarSize = int(os.stat(os.path.join(self.dropBoxPath ,s))[6])
-                          if s in self.tarSizeRegistry and tarSize == self.tarSizeRegistry[s]:
-                               newFiles.append(s.split('.tgz')[0])
-                               del self.tarSizeRegistry[s]
-                          else:
-                               self.tarSizeRegistry[s] = tarSize
-                               preservedTars.append(s)
-                     except Exception, e:
-                          logging.info("%s has encoutered problems, it wont be processed."%s + '\n'+ str(e))
-                          if s in self.tarSizeRegistry:
-                               del self.tarSizeRegistry[s]
-                          pass
-                 pass
-            pass
+        # Watch for new projects
+        newFiles = []
+	preservedTars = []
+        # gridFTP tar arrival bugfix -- Fabio
+        for s in dropBoxStatus:
+             if s.split('.')[-1]=='tgz' and (s not in self.previousDropBoxStatus):
+                  tarSize = 0
+                  try:
+                       tarSize = int(os.stat(os.path.join(self.dropBoxPath ,s))[6])
+                       if s in self.tarSizeRegistry and tarSize == self.tarSizeRegistry[s]:
+                            newFiles.append(s.split('.tgz')[0])
+                            del self.tarSizeRegistry[s]
+                       else:
+                            self.tarSizeRegistry[s] = tarSize
+                            preservedTars.append(s)
+                  except Exception, e:
+                       logging.info("%s has encoutered problems, it wont be processed."%s + '\n'+ str(e))
+                       if s in self.tarSizeRegistry:
+                            del self.tarSizeRegistry[s]
+                       pass
+             pass
+        pass
 
-            # Watch for new proxies iif you have not recived new project files
-            newProxies = []
-            if newFiles == []: 
-                  proxyList = os.listdir(self.proxyDir)
-                  newProxies = [p for p in proxyList if (p not in self.previousProxyList) ]
-                  self.previousProxyList = proxyList
-            else:
-                  self.previousProxyList=os.listdir(self.proxyDir)
+        # Watch for new proxies iif you have not recived new project files
+        newProxies = []
+        if newFiles == []: 
+             proxyList = os.listdir(self.proxyDir)
+             newProxies = [p for p in proxyList if (p not in self.previousProxyList) ]
+             self.previousProxyList = proxyList
+        else:
+             self.previousProxyList=os.listdir(self.proxyDir)
 
-            # Update persistent data-structures
-            self.previousDropBoxStatus = dropBoxStatus
-	    for t in preservedTars:
-	    	self.previousDropBoxStatus.remove(t)
-		
-            self.materializeDropBoxStatus()
-	    
-	    # Logging
-            if newFiles != []:
-                 logging.info("New Project(s) Arrived:" + str(len(newFiles)) )
-                 logging.info(newFiles) # change me if needed # F&M
-            if newProxies != []:
-                 logging.info("New Proxy Arrived:" + str(len(newProxies)) )
-                 logging.debug(newProxies)
-            if newCommand != []:
-                 logging.info("New Command Arrived:" + str(len(newCommand)) )
-                 logging.debug(newCommand)
+        # Update persistent data-structures
+        self.previousDropBoxStatus = dropBoxStatus
+        for t in preservedTars:
+	     self.previousDropBoxStatus.remove(t)
 
-            # Notify the arrival for the new tarball
-            for i in newFiles:
-               self.ms.publish("DropBoxGuardianComponent:NewFile",i)
-            for i in newCommand:
-               self.ms.publish("DropBoxGuardianComponent:NewCommand",i)
-            if newProxies != [] and newFiles==[]:
-               self.ms.publish("DropBoxGuardianComponent:NewProxy", "")
-            self.ms.commit()
+        self.materializeDropBoxStatus()
+
+	# Logging
+        if newFiles != []:
+             logging.info("New Project(s) Arrived:" + str(len(newFiles)) )
+             logging.debug(newFiles) # change me if needed # F&M
+        if newProxies != []:
+             logging.info("New Proxy Arrived:" + str(len(newProxies)) )
+             logging.debug(newProxies)
+        if newCommand != []:
+             logging.info("New Command Arrived:" + str(len(newCommand)) )
+             logging.debug(newCommand)
+
+        # Notify the arrival for the new tarball
+        for i in newFiles:
+             self.ms.publish("DropBoxGuardianComponent:NewFile",i)
+        for i in newCommand:
+             self.ms.publish("DropBoxGuardianComponent:NewCommand",i)
+        if newProxies != [] and newFiles==[]:
+             self.ms.publish("DropBoxGuardianComponent:NewProxy", "")
+
+        # Renew polling cycle
+        self.ms.publish("DropBoxGuardianComponent:poll","","00:%.2d:%.2d"%(self.sleepTime/60, self.sleepTime%60) )
+        self.ms.commit()
+        return
 
 
