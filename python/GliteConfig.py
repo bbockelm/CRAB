0d5881c1182dae5524d4834f9be222ec2eb87bbd
fanzago <>
2007-10-12 13:59:33 +0000

    Downlading ui config if the existing one is older then 1 day

--- python/GliteConfig.py
+++ python/GliteConfig.py
@@ -17,16 +17,28 @@ class GliteConfig:
     def config(self):
         return self.theConfig
 
+    def downloadFile(self, url, destination):
+        try:
+            f = urllib.urlopen(url)
+            ff = open(self.configFileName, 'w')
+            ff.write(f.read())
+            ff.close()
+        except IOError:
+            # print 'Cannot access URL: '+url
+            raise CrabException('Cannot download config file '+self.configFileName+' from '+self.url)
+
     def getConfig_(self):
         if not os.path.exists(self.configFileName):
             url = self.url+self.configFileName
-            common.logger.message('Downloading config files for RB: '+url)
-            try:
-                f = urllib.urlopen(url)
-                ff = open(self.configFileName, 'w')
-                ff.write(f.read())
-                ff.close()
-            except IOError:
-                # print 'Cannot access URL: '+url
-                raise CrabException('Cannot download config file '+self.configFileName+' from '+self.url)
+            common.logger.message('Downloading config files for WMS: '+url)
+            self.downloadFile( url, self.configFileName)
+        else:
+            statinfo = os.stat(self.configFileName)
+            ## if the file is older then 1 day it is re-downloaded to update the configuration
+            oldness = 3600*24
+            if (time.time() - statinfo.st_ctime) > oldness:
+                url = self.url+self.configFileName
+                common.logger.message('Downloading config files for WMS: '+url)
+                self.downloadFile( url, self.configFileName)
+            pass
         return os.getcwd()+'/'+self.configFileName
