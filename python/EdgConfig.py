bd4bdcb0636fffc934e565224ccbae2faf307cb0
mcinquil <>
2007-10-12 13:42:43 +0000

    Downlading ui config if the existing one is older then 1 day

--- python/EdgConfig.py
+++ python/EdgConfig.py
@@ -22,29 +22,44 @@ class EdgConfig:
     def configVO(self):
         return self.theConfigVO
 
+    def downloadFile(self, url, destination):
+        try:
+            f = urllib.urlopen(url)
+            ff = open(self.configFileName, 'w')
+            ff.write(f.read())
+            ff.close()
+        except IOError:
+            # print 'Cannot access URL: '+url
+            raise CrabException('Cannot download config file '+self.configFileName+' from '+self.url)
+
     def getConfig_(self):
         if not os.path.exists(self.configFileName):
             url = self.url+self.configFileName
             common.logger.message('Downloading config files for RB: '+url)
-            try:
-                f = urllib.urlopen(url)
-                ff = open(self.configFileName, 'w')
-                ff.write(f.read())
-                ff.close()
-            except IOError:
-                # print 'Cannot access URL: '+url
-                raise CrabException('Cannot download config file '+self.configFileName+' from '+self.url)
+            self.downloadFile( url, self.configFileName)
+        else:
+            statinfo = os.stat(self.configFileName)
+            ## if the file is older then 1 day it is re-downloaded to update the configuration
+            oldness = 3600*24
+            if (time.time() - statinfo.st_ctime) > oldness:
+                url = self.url+self.configFileName
+                common.logger.message('Downloading config files for RB: '+url)
+                self.downloadFile( url, self.configFileName)
+            pass
         return os.getcwd()+'/'+self.configFileName
 
     def getConfigVO_(self):
         if not os.path.exists(self.configVOFileName):
             url = self.url+self.configVOFileName
             common.logger.message('Downloading config files for RB: '+url)
-            try:
-                f = urllib.urlopen(url)
-                ff = open(self.configVOFileName, 'w')
-                ff.write(f.read())
-                ff.close()
-            except IOError:
-                raise CrabException('Cannot download configVO file '+self.configVOFileName+' from '+self.url)
+            self.downloadFile( url, self.configVOFileName)
+        else:
+            statinfo = os.stat(self.configVOFileName)
+            ## if the file is older then 1 day it is re-downloaded to update the configuration
+            oldness = 3600*24
+            if (time.time() - statinfo.st_ctime) > oldness:
+                url = self.url+self.configVOFileName
+                common.logger.message('Downloading config files for RB: '+url)
+                self.downloadFile( url, self.configVOFileName)
+            pass
         return os.getcwd()+'/'+self.configVOFileName
