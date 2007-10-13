da6bc5a8b33e578c87b9263ddffb3102f0ea7bda
mcinquil <>
2007-10-13 17:16:05 +0000

    Downloading config if older then half an hour; plus bug fix

--- python/EdgConfig.py
+++ python/EdgConfig.py
@@ -25,12 +25,12 @@ class EdgConfig:
     def downloadFile(self, url, destination):
         try:
             f = urllib.urlopen(url)
-            ff = open(self.configFileName, 'w')
+            ff = open(destination, 'w')
             ff.write(f.read())
             ff.close()
         except IOError:
             # print 'Cannot access URL: '+url
-            raise CrabException('Cannot download config file '+self.configFileName+' from '+self.url)
+            raise CrabException('Cannot download config file '+destination+' from '+self.url)
 
     def getConfig_(self):
         if not os.path.exists(self.configFileName):
@@ -39,8 +39,8 @@ class EdgConfig:
             self.downloadFile( url, self.configFileName)
         else:
             statinfo = os.stat(self.configFileName)
-            ## if the file is older then 1 day it is re-downloaded to update the configuration
-            oldness = 3600*24
+            ## if the file is older then half an hour it is re-downloaded to update the configuration
+            oldness = 3600/2
             if (time.time() - statinfo.st_ctime) > oldness:
                 url = self.url+self.configFileName
                 common.logger.message('Downloading config files for RB: '+url)
@@ -55,8 +55,8 @@ class EdgConfig:
             self.downloadFile( url, self.configVOFileName)
         else:
             statinfo = os.stat(self.configVOFileName)
-            ## if the file is older then 1 day it is re-downloaded to update the configuration
-            oldness = 3600*24
+            ## if the file is older then half an hour it is re-downloaded to update the configuration
+            oldness = 3600/2
             if (time.time() - statinfo.st_ctime) > oldness:
                 url = self.url+self.configVOFileName
                 common.logger.message('Downloading config files for RB: '+url)
