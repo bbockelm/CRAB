7966b320410e06877140e6d575b383c70eae8f84
slacapra <>
2008-01-21 18:39:37 +0000

    add support for copy output to user defined location and copy command

--- python/SchedulerLocal.py
+++ python/SchedulerLocal.py
@@ -25,7 +25,31 @@ class SchedulerLocal(Scheduler) :
 
         self._taskId = common.taskDB.dict('taskId')
 
-        self.return_data = 1 
+        self.return_data = int(cfg_params.get('USER.return_data',0))
+
+        self.copy_data = int(cfg_params.get("USER.copy_data",0))
+        if self.copy_data == 1:
+            self._copyCommand = cfg_params.get('USER.copyCommand','rfcp')
+            self.SE_path= cfg_params.get('USER.storage_path',None)
+            self.SE_path+='/'
+            if not self.SE_path:
+                if os.environ.has_key('CASTOR_HOME'):
+                    self.SE_path=os.environ['CASTOR_HOME']
+                else:
+                    msg='No USER.storage_path has been provided: cannot copy_output'
+                    raise CrabException(msg)
+                pass
+            pass
+
+        if ( self.return_data == 0 and self.copy_data == 0 ):
+           msg = 'Error: return_data = 0 and copy_data = 0 ==> your exe output will be lost\n'
+           msg = msg + 'Please modify return_data and copy_data value in your crab.cfg file\n'
+           raise CrabException(msg)
+
+        if ( self.return_data == 1 and self.copy_data == 1 ):
+           msg = 'Error: return_data and copy_data cannot be set both to 1\n'
+           msg = msg + 'Please modify return_data or copy_data value in your crab.cfg file\n'
+           raise CrabException(msg)
 
         ## Get local domain name
         import socket
@@ -79,6 +103,52 @@ class SchedulerLocal(Scheduler) :
         txt += 'dumpStatus $RUNTIME_AREA/$repo \n'
 
         txt += '\n\n'
+
+        return txt
+
+    def wsCopyOutput(self):
+        """
+        Write a CopyResults part of a job script, e.g.
+        to copy produced output into a storage element.
+        """
+        if not self.copy_data: return
+
+        txt = '\n'
+
+        txt += '#\n'
+        txt += '# COPY OUTPUT FILE TO '+self.SE_path
+        txt += '#\n\n'
+
+        txt += 'export SE_PATH='+self.SE_path+'\n'
+
+        txt += 'export CP_CMD='+self._copyCommand+'\n'
+
+        txt += 'echo ">>> Copy output files from WN = `hostname` to PATH = $SE_PATH using $CP_CMD :"\n'
+
+        txt += 'if [ $output_exit_status -eq 60302 ]; then\n'
+        txt += '    echo "--> No output file to copy to $SE"\n'
+        txt += '    copy_exit_status=$output_exit_status\n'
+        txt += '    echo "COPY_EXIT_STATUS = $copy_exit_status"\n'
+        txt += 'else\n'
+        txt += '    for out_file in $file_list ; do\n'
+        txt += '        echo "Trying to copy output file to $SE_PATH"\n'
+        txt += '        $CP_CMD $SOFTWARE_DIR/$out_file ${SE_PATH}/$out_file\n'
+        txt += '        copy_exit_status=$?\n'
+        txt += '        echo "COPY_EXIT_STATUS = $copy_exit_status"\n'
+        txt += '        echo "STAGE_OUT = $copy_exit_status"\n'
+        txt += '        if [ $copy_exit_status -ne 0 ]; then\n'
+        txt += '            echo "Problem copying $out_file to $SE $SE_PATH"\n'
+        txt += '            echo "StageOutExitStatus = $copy_exit_status " | tee -a $RUNTIME_AREA/$repo\n'
+        txt += '            copy_exit_status=60307\n'
+        txt += '        else\n'
+        txt += '            echo "StageOutSE = $SE" | tee -a $RUNTIME_AREA/$repo\n'
+        txt += '            echo "StageOutCatalog = " | tee -a $RUNTIME_AREA/$repo\n'
+        txt += '            echo "output copied into $SE/$SE_PATH directory"\n'
+        txt += '            echo "StageOutExitStatus = 0" | tee -a $RUNTIME_AREA/$repo\n'
+        txt += '        fi\n'
+        txt += '    done\n'
+        txt += 'fi\n'
+        txt += 'exit_status=$copy_exit_status\n'
 
         return txt
 
