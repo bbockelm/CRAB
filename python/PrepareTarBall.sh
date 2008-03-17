5fce0660ad1c10d7c5fb43a998b6365378d14b2e
spiga <>
2008-03-17 16:09:04 +0000

    added stuff to manage the new dependeces (py2-psqlite and sqlite) and removed the boss stuff

--- python/PrepareTarBall.sh
+++ python/PrepareTarBall.sh
@@ -3,11 +3,10 @@
 ### $1 is the tag value CRAB_X_X_X ###
 ### the second argument is the BOSS version: 4_3_4-sl3-sl4 (for DBS2 publication)###
 if [ $# -lt 1 ]; then
-  echo "Usage: `basename $0` <CRAB_X_Y_Z> <X_Y_Z (number of BOSS version)>"
+  echo "Usage: `basename $0` <CRAB_X_Y_Z> "
   exit 1
 fi
 tag=$1
-boss_version=$2
 echo "tag = $tag"
 
 CRABdir=$tag
@@ -15,18 +14,19 @@ echo "CRABDIR = $CRABdir"
 CRABtag=$tag
 DBSAPItag="DBS_1_0_9"
 DLSAPItag="DLS_1_0_0"
-#PAAPItag="PRODAGENT_0_7_0_pre1"
 PRODCOMMONtag="PRODCOMMON_0_7_0_pre1"
 
 CVSrepo=":pserver:anonymous@cmscvs.cern.ch:/cvs_server/repositories"
 export CVSROOT=${CVSrepo}"/CMSSW"
 
-## download CRAB from CVS and cleanup the code a bit
-echo ">> downloading CRAB tag $CRABtag from CVS CRAB"
-cvs co -r $CRABtag -d $CRABdir CRAB
 
-#echo ">> downloading CRAB HEAD from CVS CRAB"
-#cvs co -d $CRABdir CRAB
+## download CRAB from CVS and cleanup the code a bit
+#echo ">> downloading CRAB tag $CRABtag from CVS CRAB"
+#cvs co -r $CRABtag -d $CRABdir CRAB
+
+echo ">> downloading CRAB HEAD from CVS CRAB"
+echo ">> NOTE: Temporary Use of HEAD "
+cvs co -d $CRABdir CRAB
 
 cd $CRABdir
 cvs up -P python/BossScript 
@@ -37,19 +37,17 @@ rm python/zero
 rm -rf CRABSERVER
 rm -rf PsetCode
 mv python/configure .
-mv python/configureBoss .
 
-## download Boss
-echo ">> downloading BOSS version ${boss_version} from http://boss.bo.infn.it/boss-v$2-bin.tar.gz "
-mkdir Boss
-cd Boss
-wget http://boss.bo.infn.it/BOSS_${boss_version}-bin.tar.gz
-# wget http://www.bo.infn.it/~codispot/BOSS/BOSS-bin.tgz
-# mv BOSS-bin.tgz boss-v${boss_version}-bin.tar.gz
+## download sqlite
+echo ">> downloading sqlite CRAB web page"
+wget http://cmsdoc.cern.ch/cms/ccs/wm/www/Crab/sqlite.tgz
+
+## download py2-sqlite
+echo ">> downloading py2-sqlite from CRAB web page"
+wget http://cmsdoc.cern.ch/cms/ccs/wm/www/Crab/py2-pysqlite.tgz
 
 ## download DBS API
 echo ">> downloading DBS API tag ${DBSAPItag} from CVS DBS/Clients/PythonAPI"
-cd ..
 cvs co -r ${DBSAPItag} -d DBSAPI COMP/DBS/Clients/Python
 # add this dirs to the PYTHONPATH
 
@@ -65,29 +63,17 @@ mv DLS/Client/lib DLSAPI
 rm -r DLS
 # add this dir to PATH
 
-## download PA API
-#echo ">> downloading PA API tag ${DBSAPItag} from CVS DBS/Clients/PythonAPI"
-#mkdir -p ProdAgentApi
-#cd ProdAgentApi
-#cvs co -r ${PAAPItag} -d IMProv COMP/PRODAGENT/src/python/IMProv
-#cvs co -r ${PAAPItag} -d FwkJobRep COMP/PRODAGENT/src/python/FwkJobRep
-#cd ..
-
 ## download PRODCOMMON
 echo ">> downloading PRODCOMMON tag ${PRODCOMMONtag} from CVS PRODCOMMON"
+echo ">> downloading PRODCOMMON HEAD (Temporary) from CVS PRODCOMMON"
 mkdir -p ProdCommon
 cd ProdCommon
-cvs co -r ${PRODCOMMONtag} -d ProdCommon COMP/PRODCOMMON/src/python/ProdCommon
-cvs co -r ${PRODCOMMONtag} -d IMProv COMP/PRODCOMMON/src/python/IMProv
-#cvs co -r ${PRODCOMMONtag} -d FwkJobRep COMP/PRODAGENT/src/python/FwkJobRep
-cd ..
+#cvs co -r ${PRODCOMMONtag} -d ProdCommon COMP/PRODCOMMON/src/python/ProdCommon
+#cvs co -r ${PRODCOMMONtag} -d IMProv COMP/PRODCOMMON/src/python/IMProv
+cvs co -d ProdCommon COMP/PRODCOMMON/src/python/ProdCommon
+cvs co -d IMProv COMP/PRODCOMMON/src/python/IMProv
+cd ../..
 ##################
-
-### FEDE FOR DBS2 PUBLICATION
-### add modify report file
-echo "mv ModifyJobReport.py file from python to ProdCommon/FwkJobRep"
-mv python/ModifyJobReport.py ProdCommon/ProdCommon/FwkJobRep/ModifyJobReport.py
-cd ..
 
 tar zcvf $CRABdir.tgz $CRABdir
 echo ""
