8800009e3a2ad47ee757112dde8f1278d2328aae
farinafa <>
2008-03-17 14:39:00 +0000

    Added the support for the Client-Server interactions and submission to the server

--- python/SubmitterServer.py
+++ python/SubmitterServer.py
@@ -5,280 +5,220 @@ from ApmonIf import ApmonIf
 import time
 
 import commands
-from TaskDB import TaskDB
 import os, errno, time, sys, re
-
-#import subprocess
-#PIPE = subprocess.PIPE
 
 import select
 import fcntl
-
+import zlib
+import traceback
 import xml.dom.minidom
-
+from ServerCommunicator import ServerCommunicator 
+	 
 class SubmitterServer(Actor):
     def __init__(self, cfg_params, parsedRange, nominalRange):
-
-        self.cfg_params = cfg_params
+	self.cfg_params = cfg_params
         self.submitRange = parsedRange
-        self.nominalRange = nominalRange # unparsed version of the range # useful to distinguish 'all'
-        
-        try:  
-            self.server_name = self.cfg_params['CRAB.server_name'] # gsiftp://pcpg01.cern.ch/data/SEDir/
-            if not self.server_name.endswith("/"):
-                self.server_name = self.server_name + "/"
-        except KeyError:
-            msg = 'No server selected ...' 
-            msg = msg + 'Please specify a server in the crab cfg file' 
-            raise CrabException(msg) 
-        return
+	self.nominalRange = nominalRange 
+		
+	try:  
+            #print self.cfg_params['CRAB.server_name'].split(':')
+	    #self.server_name, self.server_port = self.cfg_params['CRAB.server_name'].split(':')
+	    #self.server_port = int(self.server_port)
+            ## BRUTAL SERVER-SE HARDCODED - MATTY
+            ## the API (wget or siteDB) can be callet here
+            self.server_name, self.server_path = self.cfg_params['CRAB.server_name'].split('/',1)
+            self.server_path = os.path.join('/',self.server_path)
+            self.server_port = int("20081")
+	except KeyError:
+	    msg = 'No server selected or port specified.' 
+	    msg = msg + 'Please specify a server in the crab cfg file' 
+	    raise CrabException(msg) 
+
+	return
 
     def run(self):
-        """
-        The main method of the class: submit jobs in range self.nj_list
-        """
+	"""
+	The main method of the class: submit jobs in range self.nj_list
+	"""
+	start = time.time()
+	isFirstSubmission = False
 
-        # partial submission code ## Fabio
-        if os.path.exists(common.work_space.shareDir()+'/submit_directive') == True:
-            common.logger.debug(5, "SubmitterServer::run() called for subsequent submission")
-            self.subseqSubmission()
-            return
-        # standard submission to the server
+        # check proxy and send to the crab-server  
+        self.proxyPath = self.moveProxy()
+ 
+	# subsequent submission   
+	if os.path.exists(common.work_space.shareDir()+'/first_submission') == False:
+	    self.moveISB()
+	    isFirstSubmission = True
+	    os.system('touch %s'%str(common.work_space.shareDir()+'/first_submission'))
+
+	# standard submission to the server
+	common.logger.debug(5, "SubmitterServer::run() called")
+	self.performSubmission(isFirstSubmission)
+	return
+	
+    def moveISB(self):
+
+	## get task info from BL ##
+	taskuuid = common._db.queryTask('name')
+	common.logger.debug(3, "Task name: " + taskuuid)
+	isblist = common._db.queryTask('globalSandbox').split(',')
         
-        common.logger.debug(5, "SubmitterServer::run() called")
+	common.logger.debug(3, "List of ISB files: " +str(isblist) )
+	scriptexe = common._db.queryTask('scriptName')
+	common.logger.debug(3, "Executable: " +str(scriptexe) )
+	common.logger.message("Starting sending the project to the server "+str(self.server_name)+"...")
 
-        totalCreatedJobs= 0
-        start = time.time()
-        flagSubmit = 1
-        common.jobDB.load()
+	## create remote dir ##
+        self.remotedir = os.path.join(self.server_path, taskuuid)
+	try:
+	    cmd = "edg-gridftp-mkdir gsiftp://" +str(self.server_name)+ self.remotedir
+	    common.logger.debug(3, "Creating project directory on gsiftp://" +str(self.server_name)+self.remotedir)
+	    common.logger.debug(5, " with:\n    " + cmd)
+	    status, out = commands.getstatusoutput (cmd)
+	    if int(status) != 0:
+		common.logger.debug(1, str(out))
+		msg = "ERROR : Unable to ship the project to the server \n"
+		msg +="Project "+str(taskuuid)+" not Submitted \n"
+		raise CrabException(msg)
+	except Exception, ex:
+	    common.logger.debug(1, str(ex))
+	    msg = "ERROR : Unable to ship the project to the server \n"
+	    msg +="Project "+str(taskuuid)+" not Submitted \n"
+	    raise CrabException(msg)
 
-        list_nJ = []
+	## copy ISB ##
+	for filetocopy in isblist:
+	    try:
+		cmd = 'lcg-cp -v --vo cms file://' + os.path.abspath(filetocopy) + \
+					' gsiftp://' + str(self.server_name) + \
+					  os.path.join(self.remotedir, os.path.basename(filetocopy))
+		common.logger.debug(1, "Sending "+filetocopy+" to "+str(self.server_name))
+		common.logger.debug(5, " with:\n    " + cmd)
+		status, out = commands.getstatusoutput(cmd)
+		if int(status) != 0:
+		    common.logger.debug(1, str(out))
+		    msg = "ERROR : Unable to ship the project to the server \n"
+		    msg +="Project "+str(taskuuid)+" not Submitted \n"
+		    raise CrabException(msg)
+	    except Exception, ex:
+		common.logger.debug(1, str(ex))
+		msg = "ERROR : Unable to ship the project to the server \n"
+		msg +="Project "+str(taskuuid)+" not Submitted \n"
+		raise CrabException(msg)
+	try:
+	    cmd = 'lcg-cp -v --vo cms file://' + scriptexe + \
+				    ' gsiftp://' + str(self.server_name) + \
+				      os.path.join(self.remotedir, os.path.basename(scriptexe))
+	    common.logger.debug(3, "Sending "+scriptexe+" to "+str(self.server_name))
+	    common.logger.debug(5, " with:\n    " + cmd)
+	    status, out = commands.getstatusoutput(cmd)
+	    if int(status) != 0:
+		common.logger.debug(1, str(out))
+		msg = "ERROR : Unable to ship the project to the server \n"
+		msg +="Project "+str(taskuuid)+" not Submitted \n"
+		raise CrabException(msg)
+	except Exception, ex:
+	    common.logger.debug(1, str(ex))
+	    msg = "ERROR : Unable to ship the project to the server \n"
+	    msg +="Project "+str(taskuuid)+" not Submitted \n"
+	    raise CrabException(msg)
 
-        if self.nominalRange == 'all':
-           list_nJ = self.submitRange
+	## if here then project submitted ##
+	msg = 'Project '+str(taskuuid)+' files successfully submitted to the supporting storage element.\n'
+	common.logger.message(msg)
+	return
+
+    def moveProxy(self):
+	WorkDirName = os.path.basename(os.path.split(common.work_space.topDir())[0])
+	proxySubject = None
+
+	## get subject ##
+	# TODO here we should get the proxy from the task
+	x509 = None # common._db.queryTask('proxy')
+	#
+	if 'X509_USER_PROXY' in os.environ:
+	    x509 = os.environ['X509_USER_PROXY']
+	else:
+	    status, x509 = commands.getstatusoutput('ls /tmp/x509up_u`id -u`')
+	    x509 = x509.strip()
+
+	## register proxy ##
+	common.scheduler.checkProxy()
+	try:
+	    flag = " --myproxy"
+	    common.logger.message("Registering a valid proxy to the server\n")
+	    cmd = 'asap-user-register --server '+str(self.server_name) + flag
+	    attempt = 3
+	    while attempt:
+		common.logger.debug(3, " executing:\n    " + cmd)
+		status, outp = commands.getstatusoutput(cmd)
+		common.logger.debug(3, outp)
+		if status == 0:
+		    common.logger.message("Proxy successfully delegated to the server.")
+		    break
+		else:
+		    attempt = attempt - 1
+		if (attempt == 0):
+		    raise CrabException("ASAP ERROR: Unable to ship a valid proxy to the server "+str(self.server_name)+"\n")
+	except:
+	    msg = "ASAP ERROR: Unable to ship a valid proxy to the server \n"
+	    msg +="Project "+str(WorkDirName)+" not Submitted \n"
+	    raise CrabException(msg)
+	return x509
+
+    def performSubmission(self, firstSubmission=True):
+        # create the communication session with the server frontend
+        csCommunicator = ServerCommunicator(self.server_name, self.server_port, self.cfg_params, self.proxyPath)
+ 
+        # TODO to be fixed
+        # taskname is equal to taskuuid
+        taskname = common._db.queryTask('name')
+        taskuuid = ''+str(taskname)
+        taskXML = ''
+        subOutcome = 0
+
+        # transfer remote dir to server
+        self.cfg_params['CRAB.se_remote_dir'] = self.remotedir
+
+        if firstSubmission==True:
+            # first time submit
+            try:
+                task = common.bossSession.loadTaskByName(common.work_space.taskName())
+
+                # set the paths refered to SE remotedir
+                surlpreamble = self.server_name # TODO: parametric 'gsiftp://' + self.server_name
+                remoteSBlist = [surlpreamble + os.path.join(self.remotedir, os.path.basename(f)) \
+                        for f in common._db.queryTask('globalSandbox').split(',') ]
+                task['globalSandbox'] = ', '.join(remoteSBlist)
+                task['scriptName'] = self.server_name + os.path.join( self.remotedir, \
+                        os.path.basename(common._db.queryTask('scriptName')) )
+                task['cfgName'] = self.server_name + os.path.join( self.remotedir, \
+                        os.path.basename(common._db.queryTask('cfgName')) )
+
+                for j in task.jobs:
+                    j['executable'] = self.server_name + os.path.join( self.remotedir, os.path.basename(j['executable']) )
+                #
+
+                taskXML += common.bossSession.serialize(task)
+                common.logger.debug(5, taskXML)
+            except Exception, e:
+                msg = "BossLite ERROR: Unable to serialize task object\n"
+                msg +="Project "+str(taskname)+" not Submitted \n"
+                msg += str(e)
+                msg += traceback.format_exc()
+                raise CrabException(msg)
+            subOutcome = csCommunicator.submitNewTask(taskuuid, taskname, taskXML, self.submitRange)
         else:
-           for x in self.submitRange:
-               ### FEDE removed -1 ####
-               list_nJ.append(int(x))
-               ########################
-        for nj in list_nJ:
-            #### FEDE added -1 #####
-            if (common.jobDB.status(nj -1)=='C') or (common.jobDB.status(nj -1)=='RC'):
-            #################
-                totalCreatedJobs +=1
-            else:
-                flagSubmit = 0
+            # subsequent submissions and resubmit
+            subOutcome = csCommunicator.subsequentJobSubmit(taskuuid, taskname, self.submitRange)
 
-        if not flagSubmit:
-            if totalCreatedJobs > 0:
-                common.logger.message("Not all jobs are created: before submit create all of them")
-                return
-            else:
-                common.logger.message("Impossible to submit jobs that are already submitted")
-                return
-        elif (totalCreatedJobs==0):
-            common.logger.message("No jobs to be submitted: first create them")
-            return
-
-        # submit pre DashBoard information
-        params = {'jobId':'TaskMeta'}
-               
-        fl = open(common.work_space.shareDir() + '/' + common.apmon.fName, 'r')
-        for i in fl.readlines():
-            val = i.split(':')
-            params[val[0]] = string.strip(val[1])
-        fl.close()
-
-        common.logger.debug(5,'Submission DashBoard Pre-Submission report: '+str(params))
-                        
-        common.apmon.sendToML(params)
-
-        ### Here start the server submission 
-        try:
-            x509=os.environ['X509_USER_PROXY']
-        except Exception, ex:
-            import traceback
-            common.logger.debug( 6, str(ex) )
-            # traceback fix # Fabio
-            t,v,tb = sys.exc_info()
-            tbPrintout = traceback.format_exception(t, v, tb)
-            for l in tbPrintout:
-                 common.logger.debug(6, l)
-            #
-            x509 = runCommand('ls /tmp/x509up_u`id -u`').strip()
-            pass
-
-        pSubj = os.popen3('openssl x509 -in '+str(x509)+' -subject -noout')[1].readlines()[0]
-       
-        userSubj='userSubj'
-        userSubjFile = open(common.work_space.shareDir()+'/'+userSubj,'w')
-        userSubjFile.write(str(pSubj))   
-        userSubjFile.close()  
-
-        ### Create submission range directive file # Fabio
-        submDirectiveFile = open(common.work_space.shareDir()+'/submit_directive','w')
-        if self.nominalRange == 'all':
-             submDirectiveFile.write('all\n')
-        else:
-             submDirectiveFile.write(str(self.submitRange))
-        submDirectiveFile.close()
-        # 
-    
-        WorkDirName =os.path.basename(os.path.split(common.work_space.topDir())[0])
-        common.scheduler.checkProxy()
-
-        try: 
-            flag = " --myproxy"
-            common.logger.message("Registering a valid proxy to the server\n")
-            cmd = 'asap-user-register --server '+str(self.server_name).split("/")[0] + flag
-            attempt = 3
-            while attempt:
-                #### not available for LCG python 2.3 # Fabio
-                # shell, command, tail = ('sh', cmd, '\n')
-                # a = Popen(shell, stdin=PIPE, stdout=PIPE)
-                # send_all(a, command + tail)
-                # print recv_some(a)
-                # send_all(a, 'exit' + tail)
-                # ret = a.wait()
-                ####
-                status, outp = commands.getstatusoutput(cmd)
-                print outp 
-                if status == 0: 
-                    print "Asap register ok"
-                    break
-                else: 
-                    attempt = attempt - 1
-                if (attempt == 0): 
-                    raise CrabException("ASAP ERROR: Unable to ship a valid proxy to the server "+str(server_name).split("/")[0]+"\n")
-        except:  
-            msg = "ASAP ERROR: Unable to ship a valid proxy to the server \n"
-            msg +="Project "+str(WorkDirName)+" not Submitted \n"      
+        if subOutcome != 0:
+            msg = "ClientServer ERROR: %d raised during the communication.\n"%subOutcome
             raise CrabException(msg)
 
-        projectUniqName = 'crab_'+str(WorkDirName)+'_'+common.taskDB.dict('TasKUUID')
-        common.taskDB.load()
-        common.taskDB.setDict('projectName',projectUniqName)
-        common.taskDB.save()
+        del csCommunicator  
+        return     
 
-        ### create a tar ball
-        TarDirName = os.path.dirname(os.path.split(common.work_space.topDir())[0])
-        cmd = ""
-        if TarDirName:
-            cmd = 'tar -zcvf '+str(WorkDirName)+'.tgz -C '+str(TarDirName)+' '+str(WorkDirName)
-        else:
-            cmd = 'tar -zcvf '+str(WorkDirName)+'.tgz '+str(WorkDirName)
-        common.logger.debug( 5, cmd )
-        cmd_out = runCommand(cmd)
-    
-        try: 
-            ### submit poject to the server   
-            #projectUniqName = 'crab_'+str(WorkDirName)+'_'+common.taskDB.dict('TasKUUID')     
-            common.logger.message("Sending the project to the server...\n")
-            cmd = 'lcg-cp --vo cms file://'+os.getcwd()+'/'+str(WorkDirName)+'.tgz gsiftp://' + str(self.server_name) + str(projectUniqName)+'.tgz'
-            shipProject = os.system(cmd +' >& /dev/null')
-            common.logger.debug( 5, 'rm -f '+str(WorkDirName)+'.tgz' )
-            cmd = 'rm -f '+str(WorkDirName)+'.tgz'
-            cmd_out = runCommand(cmd)
-            if (shipProject>0):
-                raise CrabException("ERROR : Unable to ship the project to the server \n "+str(self.server_name).split("/")[0]+"\n")
-            else:
-                msg='Project '+str(WorkDirName)+' succesfuly submitted to the server \n'      
-                common.logger.message(msg)
-                if self.nominalRange == 'all':
-                    list_nJ = self.submitRange
-                else:
-                    for x in self.submitRange:
-                        ### FEDE removed -1 ###
-                        list_nJ.append(int(x))
-                        #######################
-                #   
-                for nj in list_nJ:
-                    ### FEDE added -1 ####
-                    common.jobDB.setStatus(nj -1, 'S')
-                    #####################
-                    common.jobDB.save()
-        except Exception, ex:  
-            print str(ex)
-            cmd = 'rm -f '+str(WorkDirName)+'.tgz'
-            cmd_out = runCommand(cmd)
-            msg = "ERROR : Unable to ship the project to the server \n"
-            msg +="Project "+str(WorkDirName)+" not Submitted \n"      
-            raise CrabException(msg)
-        return
 
-###################################
-    #Partial submission code # Fabio
-###################################
-    def subseqSubmission(self):
-        # prepare standard xml messages data
-        common.jobDB.load()
-        server_name = self.cfg_params['CRAB.server_name'] # gsiftp://pcpg01.cern.ch/data/SEDir/
-        if not server_name.endswith("/"):
-            server_name = server_name + "/"
-
-        WorkDirName =os.path.basename(os.path.split(common.work_space.topDir())[0])
-        projectUniqName = 'crab_'+str(WorkDirName)+'_'+common.taskDB.dict('TasKUUID')
-        
-        pSubj = os.popen3('openssl x509 -in  /tmp/x509up_u`id -u` -subject -noout')[1].readlines()[0]
-
-        # cross check the specified range w.r.t. the previously submitted jobs
-        # build the difference set between the directives and finally materialize
-        # the hystory of the submissions
-        prev_subms = 'None'
-        delta_subm = []
-        new_subms = ''
-
-        file = open(common.work_space.shareDir()+'/submit_directive','r')
-        prev_subms = str(file.readlines()[0]).split('\n')[0]
-        file.close()
-        
-        if prev_subms == 'all':
-            common.logger.message("Impossible to submit jobs that are already submitted")
-            return
-
-        prev_subms = eval(prev_subms)
-        delta_subm = [i for i in self.submitRange if i not in prev_subms]
-        new_subms = prev_subms + delta_subm
-        if self.nominalRange == 'all': 
-            new_subms = 'all'
-
-        if len(delta_subm) == 0:
-            common.logger.message("Impossible to submit jobs that are already submitted")
-            return
-            
-        file = open(common.work_space.shareDir()+'/submit_directive','w')
-        file.write(str(new_subms))
-        file.close()
-        
-        # submit the xml message with delta_subm range
-        try: 
-            self.cfile = xml.dom.minidom.Document()
-            root = self.cfile.createElement("TaskCommand")
-            node = self.cfile.createElement("TaskAttributes")
-            node.setAttribute("Task", projectUniqName)
-            node.setAttribute("Subject", string.strip(pSubj))
-            node.setAttribute("Command", "submit_range")
-            node.setAttribute("Range", str(delta_subm))
-            root.appendChild(node)
-            self.cfile.appendChild(root)
-            file = open(WorkDirName + '/share/command.xml', 'w')
-            # minidom fix # Fabio 
-                    #xml.dom.ext.PrettyPrint(self.cfile, file)
-            file.write(self.cfile.toprettyxml())
-            #
-            file.close()
-            
-            cmd = 'lcg-cp --vo cms file://'+os.getcwd()+'/'+str(WorkDirName)+'/share/command.xml gsiftp://' + str(server_name) + str(projectUniqName)+'.xml'
-            retcode = os.system(cmd)
-            if retcode: 
-                raise CrabException("Failed to ship submission command to server")
-            else: 
-                common.logger.message("Submission command succesfully shipped to server")
-                for nj in delta_subm:
-                    common.jobDB.setStatus((nj-1), 'S')
-                    common.jobDB.save()
-        except RuntimeError,e:
-            msg +="Project "+str(WorkDirName)+" not submitted: \n"      
-            raise CrabException(msg + e.__str__())
-        return
 
