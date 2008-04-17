5757dde5c17664d9c918db7e7260a68b6d07aad8
ewv <>
2008-04-17 14:20:55 +0000

    First version for Glidein

--- python/SchedulerGlidein.py
+++ python/SchedulerGlidein.py
@@ -6,9 +6,9 @@ __version__ = "$Revision: 1.96 $"
 
 # All of the content moved to SchedulerCondorCommon.
 
-class SchedulerCondor_g(SchedulerCondorCommon):
+class SchedulerGlidein(SchedulerCondorCommon):
   def __init__(self):
-    SchedulerCondorCommon.__init__(self,"CONDOR_G")
+    SchedulerCondorCommon.__init__(self,"GLIDEIN")
     return
 
   def sched_parameter(self,i,task):
@@ -18,9 +18,13 @@ class SchedulerCondor_g(SchedulerCondorCommon):
     jobParams = SchedulerCondorCommon.sched_parameter(self,i,task)
 
     seDest = self.blackWhiteListParser.cleanForBlackWhiteList(eval(task.jobs[i-1]['dlsDestination']))
-    ceDest = self.getCEfromSE(seDest)
+    #ceDest = self.getCEfromSE(seDest)
 
-    jobParams += "globusscheduler = "+ceDest+":2119/jobmanager-condor; "
+    # FIXME: Translate seDest into glidein name using SiteDB
+
+    jobParams += '+DESIRED_Sites = "UCSDT2"; '
+    jobParams += '+DESIRED_Archs = "INTEL,X86_64"; '
+    jobParams += "Requirements = stringListMember(GLIDEIN_Site,DESIRED_Sites) &&  stringListMember(Arch,DESIRED_Archs); "
 
     common._db.updateTask_({'jobType':jobParams})
     return jobParams # Not sure I even need to return anything
