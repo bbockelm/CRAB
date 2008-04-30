1c3eeb2d0bbede69cc17ca074cf533a29135c5ef
farinafa <>
2008-04-30 12:36:57 +0000

    One more fix on a misleading api call

--- python/GetOutputServer.py
+++ python/GetOutputServer.py
@@ -102,6 +102,6 @@ class GetOutputServer( GetOutput, StatusServer ):
             csCommunicator = ServerCommunicator(self.server_name, self.server_port, self.cfg_params)
             csCommunicator.outputRetrieved(self.taskuuid, retrievedFilesJodId)
 
-            common._db.updateRunJob_(retrievedFilesJodId, {'statusScheduler':'Cleared', 'status':'E'})
+            common._db.updateRunJob_(retrievedFilesJodId, [{'statusScheduler':'Cleared', 'status':'E'}] * len(retrievedFilesJodId) )
         return
 
