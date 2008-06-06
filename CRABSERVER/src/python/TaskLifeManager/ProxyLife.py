aaf5e3a579c329eb0c4d1f22018fc3fda9758aaa
mcinquil <>
2008-06-06 22:15:45 +0000

    Sending one e-mail per proxy expired, getting just active task, bug fix, cleaned code, added comments

--- CRABSERVER/src/python/TaskLifeManager/ProxyLife.py
+++ CRABSERVER/src/python/TaskLifeManager/ProxyLife.py
@@ -1,3 +1,6 @@
+import os
+import copy
+
 # logging
 import logging
 from logging.handlers import RotatingFileHandler
@@ -9,11 +12,9 @@ from MessageService.MessageService import MessageService
 from TaskTracking.UtilSubject import UtilSubject
 from TaskTracking.TaskStateAPI import findTaskPA, getStatusUUIDEmail
 
-import os
 from ProdAgentDB.Config import defaultConfig as dbConfig
 from ProdCommon.Database import Session
 from ProdCommon.Database.MysqlInstance import MysqlInstance
-import copy
 
 class ProxyLife:
 
@@ -23,9 +24,16 @@ class ProxyLife:
             min = 3600*6
         self.minimumleft = min
         self.bossCfgDB = dBlite
-        self.ms = MessageService()
+
         # register
+        self.ms = MessageService()
         self.ms.registerAs("TaskLifeManager")
+
+        ## preserv proxyes notified
+        self.__allproxies = []
+
+    ###############################################
+    ######       SYSTEM  INTERACTIONS        ######
 
     def executeCommand(self, command):
         import commands
@@ -39,6 +47,25 @@ class ProxyLife:
             return output
         return -1
 
+    def cleanProxy(self, proxy):
+        self.executeCommand( "rm -f " + str(proxy) )
+
+    def notified(self, proxy):
+        if proxy in self.__allproxies:
+            return True
+        return False
+
+    def denotify(self, proxy):
+        if proxy in self.__allproxies:
+            self.__allproxies.remove(proxy)
+
+    def notify(self, proxy):
+        if not self.notified(proxy):
+            self.__allproxies.append(proxy)
+   
+    ###############################################
+    ######          DB INTERACTIONS          ######
+
     def getListProxies(self):
         proxyList = []
         dbCfg = copy.deepcopy(dbConfig)
@@ -47,15 +74,12 @@ class ProxyLife:
         Session.set_database(dbCfg)
         Session.connect(self)
         Session.start_transaction(self)
-
         sqlStr="select distinct(proxy) from js_taskInstance;"
         Session.execute(sqlStr)
-
         for tupla in Session.fetchall(self):
             proxyList.append(tupla[0]) 
-
-        Session.commit(self)
         Session.close(self)
+
         return proxyList
 
     def getTaskList(self, proxy):
@@ -66,30 +90,23 @@ class ProxyLife:
         Session.set_database(dbCfg)
         Session.connect(proxy)
         Session.start_transaction(proxy)
-
-        sqlStr="select taskName, eMail from js_taskInstance where proxy = '"+str(proxy)+"';"
+        ## get active tasks for proxy 'proxy'
+        sqlStr="select taskName, eMail from js_taskInstance " + \
+               "where proxy = '"+str(proxy)+"' and notificationSent < 2;"
         Session.execute(sqlStr)
-
         for tupla in Session.fetchall(proxy):
             if tupla[1] in dictionary.keys():
                 dictionary[tupla[1]].append(tupla[0])
             else:
                 dictionary.setdefault(tupla[1],[tupla[0]])
-
-        Session.commit(proxy)
         Session.close(proxy)
+
         return dictionary
 
     def archiveBliteTask(self, taskname):
         # Blite API import
         from ProdCommon.BossLite.API.BossLiteAPI import  BossLiteAPI
         from ProdCommon.BossLite.Common.Exceptions import TaskError, JobError
-#        selbossCfgDB = {\
-#                      'dbName': 'CrabServerDB', \
-#                      'user': 'root', \
-#                      'passwd': 'root', \
-#                      'socketFileLocation': '/home/crab/work/mysqldata/mysql.sock' \
-#                    }
 
         mySession = BossLiteAPI("MySQL", self.bossCfgDB)
         taskObj = None
@@ -116,14 +133,10 @@ class ProxyLife:
         Session.set_database(dbCfg)
         Session.connect(taskname)
         Session.start_transaction(taskname)
-    
         sqlStr="select id from we_Job where owner = '"+str(taskname)+"';"
         Session.execute(sqlStr)
-    
         for tupla in Session.fetchall(taskname):
             joblist.append(tupla[0])
-
-        Session.commit(taskname)
         Session.close(taskname)
 
         return joblist
@@ -135,13 +148,14 @@ class ProxyLife:
         dbCfg['dbType'] = 'mysql'
         Session.set_database(dbCfg)
         Session.connect(jobspecid)
-
+        ## get info from we_job table
         jobInfo = wfJob.get(jobspecid)
+        Session.close(jobspecid)
+
         if int(jobInfo['retries']) >= int(jobInfo['max_retries']):
            return True
         else:
             return False
-        Session.close(jobspecid)
 
     def archiveServerTask(self, taskname):
         from ProdAgent.WorkflowEntities.JobState import doNotAllowMoreSubmissions
@@ -153,58 +167,88 @@ class ProxyLife:
             logging.error( "Not achiving server job " + str(jobtoclean) )
             logging.error( "   cause: " + str(ex) )
 
-    def cleanProxy(self, proxy):
-        #executeCommand( "rm -f " + str(proxy) )
-        pass
-
     def notifyExpiring(self, mail, tasks, lifetime):
         mexage = "ProxyExpiring"
 
-        payload = str(mail) + "::" + str(lifetime) #str(mail) + "::" + str(tasks) + "::" + str(lifetime)
+        payload = str(mail) + "::" + str(lifetime)
 
         logging.info(" Publishing ['"+ mexage +"']")
         logging.info("   payload = " + payload )
         self.ms.publish( mexage, payload)
         self.ms.commit()
 
+    ###############################################
+    ######     PUBLIC CALLABLE METHODS       ######
 
     def pollProxies(self):
+        """
+        __pollProxies__
+
+        loops on the proxies and makes related actions
+        """
+        ##################
+        ##### STARTS #####
         logging.info( "Start proxy's polling...." )
         if os.path.exists(self.proxiespath):
-            #proxieslist = os.listdir(self.proxiespath)
+
+            ## get the list of proxies
             proxieslist = self.getListProxies()
-            for proxy in proxieslist:
-                proxyfull = proxy #self.proxiespath + "/" + proxy #os.path.join(self.proxiespath,proxy)
+            for proxyfull in proxieslist:
+
+                ## get the remaining proxy life time
                 logging.info("Checking proxy [" + str(proxyfull) + "]")
-                timeleft = 0
+                timeleft = -1
                 try:
-                    ### get the remaining life time
                     timeleft = int(self.checkUserProxy(proxyfull))
                 except ValueError, ex:
                     timeleft = -1
                 except Exception, ex:
                     logging.info(str(ex))
-                tasksbymail = self.getTaskList(proxyfull)
+
+                ###############################################################
+                ######
+                ## problem checking the proxy ##
                 if timeleft < 0:
                     logging.error( "Problem on checking proxy: [" + proxyfull + "]") 
+                ######
+                ## if expired archive the jobs/taks ##
                 elif timeleft == 0:
+                    ## get the dictionary of mail-[tasklist] asscoiated to the proxy
+                    tasksbymail = self.getTaskList(proxyfull)
                     logging.info( "Proxy expired [" + proxyfull + "]: " + str(timeleft) + "sec." )
                     for mail, tasks in tasksbymail.iteritems():
                         for task in tasks:
-                            logging.info( "TaskToClean: " + str(task) )
+                            ## archive the jobs for blite
                             self.archiveBliteTask(task)
+                            ## archive the jobs for the server
                             self.archiveServerTask(task)
                     logging.info( "Cleaning expired proxy: " + str(proxyfull) ) 
+                    ## delete the proxy file
                     self.cleanProxy(proxyfull)
+                ######
+                ## if valid proxy, but not too long notify to renew ##
                 elif timeleft <= self.minimumleft:
+                    ## get the dictionary of mail-[tasklist] asscoiated to the proxy
+                    tasksbymail = self.getTaskList(proxyfull)
                     for mail, tasks in tasksbymail.iteritems():
-                        ## need to notifying expired proxy
-                        logging.info( "Renew your proxy: " + str(mail) )
-                        self.notifyExpiring(mail, tasks, timeleft)
+                        if not self.notified(proxyfull):
+                             ## notify the expired proxy
+                            logging.info( "Renew your proxy: " + str(mail) )
+                            self.notifyExpiring(mail, tasks, timeleft)
+                            self.notify(proxyfull)
+                ######
+                ## long proxy, do nothing ##
                 else:
                     logging.debug(" Valid proxy [" + proxyfull + "]: " + str(timeleft) + "sec.")
+                    ## reabilitate if necessary
+                    self.denotify(proxyfull)
+                ###############################################################
+
                 logging.info("")
         else:
             logging.error( "Could not find proxies path [" + self.proxiespath +"]." )
+
         logging.info( "Proxy's polling ended." )
+        ###### ENDS ######
+        ##################
 
