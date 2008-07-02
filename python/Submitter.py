2fc0cd572ce791ead6c949ba3b768085c92a0f12
spiga <>
2008-07-02 17:23:52 +0000

    fixed bug in Target SE reported to the dashboard

--- python/Submitter.py
+++ python/Submitter.py
@@ -315,8 +315,10 @@ class Submitter(Actor):
                 rb = str(job.runningJob['service'])
         
             dlsDest = job['dlsDestination'] 
-            if len(dlsDest) <= 2 :
-                T_SE=string.join(str(dlsDest),",")
+            if len(dlsDest) == 1 :
+                T_SE=str(dlsDest[0])
+            elif len(dlsDest) == 2 :
+                T_SE=str(dlsDest[0])+','+str(dlsDest[1])
             else : 
                 T_SE=str(len(dlsDest))+'_Selected_SE'
 
