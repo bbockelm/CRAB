238a56c5da06002f37dc8c5b81f3fca017da28f0
ewv <>
2008-07-08 19:34:00 +0000

    Use SiteDB from ProdCommon

--- python/LFNBaseName.py
+++ python/LFNBaseName.py
@@ -7,16 +7,17 @@ from crab_exceptions import *
 from crab_util import runCommand
 import common
 import os, string
+from ProdCommon.SiteDB.SiteDB import SiteDBJSON
 
 
 def LFNBase(ProcessedDataset,merged=True,LocalUser=False):
     """
     """
-    lfnbase = "/store" 
+    lfnbase = "/store"
     if not merged:
-        lfnbase = os.path.join(lfnbase,"tmp")   
+        lfnbase = os.path.join(lfnbase,"tmp")
     lfnbase = os.path.join(lfnbase, "user", getUserName(LocalUser=LocalUser), ProcessedDataset )
-       
+
     return lfnbase
 
 def PFNportion(ProcessedDataset,LocalUser=False):
@@ -47,33 +48,24 @@ def getDN():
     except:
         msg = "Error. Problem with voms-proxy-info -identity command"
         raise CrabException(msg)
-    return userdn.split('\n')[0] 
+    return userdn.split('\n')[0]
 
 def gethnUserName():
     """
     extract user name from SiteDB
     """
-    import urllib
     hnUserName = None
     userdn = getDN()
+    mySiteDB = SiteDBJSON()
+
     try:
-        sitedburl="https://cmsweb.cern.ch/sitedb/sitedb/json/index/dnUserName"
-        params = urllib.urlencode({'dn': userdn })
-        f = urllib.urlopen(sitedburl,params)
-        udata = f.read()
-        try:
-            userinfo= eval(udata)
-        except StandardError, ex:
-            msg = "Error. Problem extracting user name from %s : %s \n SiteDB call output: \n %s"%(sitedburl,ex,udata)
-            raise CrabException(msg)
-        hnUserName = userinfo['user']
+        hnUserName = mySiteDB.dnUserName(dn=userdn)
     except:
-        msg = "Error. Problem extracting user name from %s "%sitedburl
-        msg += "\n Check that you are registered in SiteDB, see https://twiki.cern.ch/twiki/bin/view/CMS/SiteDBForCRAB" 
-        msg += "\n SiteDB call output: \n %s"%udata
+        msg = "Error. Problem extracting user name from SiteDB"
+        msg += "\n Check that you are registered in SiteDB, see https://twiki.cern.ch/twiki/bin/view/CMS/SiteDBForCRAB"
         raise CrabException(msg)
     if not hnUserName:
-        msg = "Error. There is no user name associated to DN %s in %s. You need to register in SiteDB with the instructions at https://twiki.cern.ch/twiki/bin/view/CMS/SiteDBForCRAB"%(userdn,sitedburl)
+        msg = "Error. There is no user name associated to DN %s in SiteDB. You need to register in SiteDB with the instructions at https://twiki.cern.ch/twiki/bin/view/CMS/SiteDBForCRAB" % userdn
         print msg
         raise CrabException(msg)
     return hnUserName
@@ -86,7 +78,7 @@ def getUserName(LocalUser=False):
        common.logger.message("==> Using as username the Unix user name")
        UserName=getUnixUserName()
        return UserName
-    
+
     UserName=gethnUserName()
     return UserName
 
@@ -95,16 +87,16 @@ if __name__ == '__main__' :
     """
     from crab_logger import Logger
     from WorkSpace import *
-    continue_dir="/afs/cern.ch/user/a/afanfani/"
+    continue_dir = os.path.expanduser("~")
     cfg_params={'USER.logdir' : continue_dir }
     common.work_space = WorkSpace(continue_dir, cfg_params)
     log = Logger()
     common.logger = log
 
-    print "xx %s xx"%getUserName() 
+    print "xx %s xx"%getUserName()
     baselfn = LFNBase("datasetstring")
-    print baselfn    
+    print baselfn
 
     unmergedlfn = LFNBase("datasetstring",merged=False)
-    print unmergedlfn  
+    print unmergedlfn
     print PFNportion("datasetstring")
