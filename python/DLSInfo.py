e583e5d88e4bdc3a4bd90c32018d7c31e753900a
spiga <>
2008-07-13 19:21:37 +0000

    re-added. this is still needed

---
+++ python/DLSInfo.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+import sys, os, commands,string, re
+import exceptions
+from crab_exceptions import *
+from crab_util import *
+import common
+
+import dlsApi
+import dlsClient
+from dlsDataObjects import DlsLocation, DlsFileBlock, DlsEntry
+                                                                                            
+# ## for python 2.2 add the pyexpat.so to PYTHONPATH
+# pythonV=sys.version.split(' ')[0]
+# if pythonV.find('2.2') >= 0 :
+#     Crabpydir=commands.getoutput('which crab')
+#     Topdir=string.replace(Crabpydir,'/python/crab','')
+#     extradir=Topdir+'/DLSAPI/extra'
+#     if sys.path.count(extradir) <= 0:
+#          if os.path.exists(extradir):
+#             sys.path.insert(0, extradir)
+
+
+class DLSError:
+    def __init__(self, fileblocks):
+        print '\nERROR accessing DLS for fileblock '+fileblocks+'\n'
+        pass
+
+
+class DLSNoReplicas(exceptions.Exception):
+    def __init__(self, FileBlock):
+        self.args ="No replicas exists for fileblock: "+FileBlock+"\n"
+        exceptions.Exception.__init__(self, self.args)
+        pass
+
+    def getClassName(self):
+        """ Return class name. """
+        return "%s" % (self.__class__.__name__)
+
+    def getErrorMessage(self):
+        """ Return exception error. """
+        return "%s" % (self.args)
+
+
+##############################################################################
+# Class to extract info from DLS 
+##############################################################################
+
+class DLSInfo:
+    def __init__(self, type, cfg_params):
+        self.cfg_params = cfg_params
+        if type=="DLS_TYPE_DBS":
+            #
+            # use dbs_url as dls_endpoint if dls_type is dbs
+            #
+            try:
+                endpoint=self.cfg_params['CMSSW.dbs_url']
+            except KeyError:
+                endpoint="http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet"
+        else:
+            msg = "DLS type %s not among the supported DLS ( DLS_TYPE_DLI and DLS_TYPE_MYSQL ) "%type
+            raise CrabException(msg)
+
+        common.logger.debug(5,"DLS interface: %s Server %s"%(type,endpoint))       
+        try:
+            self.api = dlsClient.getDlsApi(dls_type=type,dls_endpoint=endpoint)
+        except dlsApi.DlsApiError, inst:
+            msg = "Error when binding the DLS interface: %s  Server %s"%(str(inst),endpoint)
+            #print msg
+            raise CrabException(msg)
+ 
+# ####################################
+    def getReplicas(self,fileblocks):
+        """
+        query DLS to get replicas
+        """
+        ##
+        try:
+            entryList=self.api.getLocations([fileblocks])
+        except dlsApi.DlsApiError, inst:
+            #msg = "Error in the DLS query: %s." % str(inst)
+            #print msg
+            raise DLSNoReplicas(fileblocks)
+
+        ListSites=[] 
+        for entry in entryList:
+            for loc in entry.locations:
+                ListSites.append(str(loc.host))
+        if len(ListSites)<=0:
+            raise DLSNoReplicas(fileblocks)
+
+        return ListSites         
