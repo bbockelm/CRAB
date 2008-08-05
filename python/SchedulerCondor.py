9ab567e7ba410f65d4d78d7a86c0fa376f4a2240
ewv <>
2008-08-05 18:48:52 +0000

    Mostly cosmetic changes from pylint

--- python/SchedulerCondor.py
+++ python/SchedulerCondor.py
@@ -1,123 +1,135 @@
-__revision__ = "$Id: SchedulerCondor.py,v 1.8 2008/06/11 10:03:07 spiga Exp $"
-__version__ = "$Revision: 1.8 $"
+"""
+Implements the vanilla (local) Condor scheduler
+"""
 
-from Scheduler import Scheduler
-from SchedulerLocal import SchedulerLocal
-from crab_exceptions import *
-from crab_util import *
-from crab_logger import Logger
+__revision__ = "$Id: SchedulerCondor.py,v 1.9 2008/06/17 21:54:14 ewv Exp $"
+__version__ = "$Revision: 1.9 $"
+
+from SchedulerLocal  import SchedulerLocal
+from crab_exceptions import CrabException
+#from crab_logger import Logger
+
 import common
-
 import os
 
-#  Naming convention:
-#  methods starting with 'ws' are responsible to provide
-#  corresponding part of the job script ('ws' stands for 'write script').
+# Naming convention:  Methods starting with 'ws' provide the corresponding part of the job script
+# ('ws' stands for 'write script').
 
 class SchedulerCondor(SchedulerLocal) :
-
-  def __init__(self):
-    Scheduler.__init__(self,"CONDOR")
-    return
-
-
-  def configure(self, cfg_params):
-    SchedulerLocal.configure(self, cfg_params)
-    self.environment_unique_identifier ='${HOSTNAME}_${CONDOR_ID}_' + common._db.queryTask('name')
-
-    try:
-      tmp =  cfg_params['CMSSW.datasetpath']
-      if string.lower(tmp)=='none':
-        self.datasetPath = None
-        self.selectNoInput = 1
-      else:
-        self.datasetPath = tmp
-        self.selectNoInput = 0
-    except KeyError:
-      msg = "Error: datasetpath not defined "
-      raise CrabException(msg)
-
-    return
-
-
-  def sched_parameter(self,i,task):
     """
-    Return scheduler-specific parameters
-    """
-    index = int(common._db.nJobs()) - 1
-    sched_param= ''
-
-    for i in range(index):
-      pass
-
-    return sched_param
-
-
-  def realSchedParams(self,cfg_params):
-    """
-    Return dictionary with specific parameters, to use
-    with real scheduler
+    Class to implement the vanilla (local) Condor scheduler
     """
 
-    tmpDir = os.path.join(common.work_space.shareDir(),'.condor_temp')
-    params = {'tmpDir':tmpDir}
-    return  params
+    def __init__(self):
+        SchedulerLocal.__init__(self,"CONDOR")
+        self.datasetPath   = None
+        self.selectNoInput = None
+        self.environment_unique_identifier = None
+        return
 
 
-  def listMatch(self, seList, full, onlyOSG=True):
-    """
-    Check the compatibility of available resources
-    """
+    def configure(self, cfg_params):
+        """
+        Configure the scheduler with the config settings from the user
+        """
 
-    # May have problems with onlyOSG being false, probably due to lengths of lists and command line.
-    # Either re-write osg_bdii.py with a proper ldap library or break the queries apart
+        SchedulerLocal.configure(self, cfg_params)
+        self.environment_unique_identifier ='${HOSTNAME}_${CONDOR_ID}_' + common._db.queryTask('name')
 
-    if self.selectNoInput:
-      return [True]
-    else:
-      return SchedulerLocal.listMatch(self, seList, full)
+        try:
+            tmp =  cfg_params['CMSSW.datasetpath']
+            if tmp.lower() == 'none':
+                self.datasetPath = None
+                self.selectNoInput = 1
+            else:
+                self.datasetPath = tmp
+                self.selectNoInput = 0
+        except KeyError:
+            msg = "Error: datasetpath not defined "
+            raise CrabException(msg)
 
-  def decodeLogInfo(self, file):
-    """
-    Parse logging info file and return main info
-    """
-    import CondorGLoggingInfo
-    loggingInfo = CondorGLoggingInfo.CondorGLoggingInfo()
-    reason = loggingInfo.decodeReason(file)
-    return reason
+        return
 
 
-  def wsExitFunc(self):
-    """
-    """
-    txt = '\n'
-    txt += '#\n'
-    txt += '# EXECUTE THIS FUNCTION BEFORE EXIT \n'
-    txt += '#\n\n'
+    def sched_parameter(self, i, task):
+        """
+        Return scheduler-specific parameters
+        """
 
-    txt += 'func_exit() { \n'
-    txt += self.wsExitFunc_common()
+        index = int(common._db.nJobs()) - 1
+        schedParam = ''
 
-    txt += '    tar zcvf ${out_files}.tgz  ${final_list}\n'
-    txt += '    cp  ${out_files}.tgz $ORIG_WD/\n'
-    txt += '    cp  crab_fjr_$NJob.xml $ORIG_WD/\n'
+        for i in range(index):
+            pass
 
-    txt += '    exit $job_exit_code\n'
-    txt += '}\n'
+        return schedParam
 
-    return txt
 
-  def wsInitialEnvironment(self):
-    """
-    Returns part of a job script which does scheduler-specific work.
-    """
+    def realSchedParams(self, cfg_params):
+        """
+        Return dictionary with specific parameters, to use with real scheduler
+        """
 
-    txt  = '\n# Written by SchedulerCondor::wsInitialEnvironment\n'
-    txt += 'echo "Beginning environment"\n'
-    txt += 'printenv | sort\n'
+        tmpDir = os.path.join(common.work_space.shareDir(),'.condor_temp')
+        params = {'tmpDir':tmpDir}
+        return params
 
-    txt += 'middleware='+self.name()+' \n'
-    txt += """
+
+    def listMatch(self, seList, full):
+        """
+        Check the compatibility of available resources
+        """
+
+        if self.selectNoInput:
+            return [True]
+        else:
+            return SchedulerLocal.listMatch(self, seList, full)
+
+
+    def decodeLogInfo(self, fileName):
+        """
+        Parse logging info file and return main info
+        """
+
+        import CondorGLoggingInfo
+        loggingInfo = CondorGLoggingInfo.CondorGLoggingInfo()
+        reason = loggingInfo.decodeReason(fileName)
+        return reason
+
+
+    def wsExitFunc(self):
+        """
+        Returns the part of the job script which runs prior to exit
+        """
+
+        txt = '\n'
+        txt += '#\n'
+        txt += '# EXECUTE THIS FUNCTION BEFORE EXIT \n'
+        txt += '#\n\n'
+
+        txt += 'func_exit() { \n'
+        txt += self.wsExitFunc_common()
+
+        txt += '    tar zcvf ${out_files}.tgz  ${final_list}\n'
+        txt += '    cp  ${out_files}.tgz $ORIG_WD/\n'
+        txt += '    cp  crab_fjr_$NJob.xml $ORIG_WD/\n'
+
+        txt += '    exit $job_exit_code\n'
+        txt += '}\n'
+
+        return txt
+
+    def wsInitialEnvironment(self):
+        """
+        Returns part of a job script which does scheduler-specific work.
+        """
+
+        txt  = '\n# Written by SchedulerCondor::wsInitialEnvironment\n'
+        txt += 'echo "Beginning environment"\n'
+        txt += 'printenv | sort\n'
+
+        txt += 'middleware='+self.name()+' \n'
+        txt += """
 if [ $_CONDOR_SCRATCH_DIR ] && [ -d $_CONDOR_SCRATCH_DIR ]; then
     ORIG_WD=`pwd`
     echo "Change from $ORIG_WD to Condor scratch directory: $_CONDOR_SCRATCH_DIR"
@@ -129,4 +141,4 @@ if [ $_CONDOR_SCRATCH_DIR ] && [ -d $_CONDOR_SCRATCH_DIR ]; then
 fi
 """
 
-    return txt
+        return txt
