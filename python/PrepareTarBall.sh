566565e1fe067102f485199f7ae296ab4b85dbfd
spiga <>
2008-09-04 08:55:29 +0000

    PRODCOMMON_0_12_0_testCS4

--- python/PrepareTarBall.sh
+++ python/PrepareTarBall.sh
@@ -14,7 +14,7 @@ echo "CRABDIR = $CRABdir"
 CRABtag=$tag
 DBSAPItag="DBS_1_1_6"
 DLSAPItag="DLS_1_0_0"
-PRODCOMMONtag="PRODCOMMON_0_12_0_testCS3"
+PRODCOMMONtag="PRODCOMMON_0_12_0_testCS4"
 
 CVSrepo=":pserver:anonymous@cmscvs.cern.ch:/cvs_server/repositories"
 export CVSROOT=${CVSrepo}"/CMSSW"
