52817a6faaf1f8bc7bae14fe68f645b69314aeee
ewv <>
2008-09-05 21:59:43 +0000

    cmsenv instead of eval scram runtime, better message for user's case, and clean up indentation

--- python/Scram.py
+++ python/Scram.py
@@ -15,7 +15,7 @@ class Scram:
         scramArea = ''
 
         if os.environ.has_key("SCRAM_ARCH"):
-           self.scramArch = os.environ["SCRAM_ARCH"]
+            self.scramArch = os.environ["SCRAM_ARCH"]
         if os.environ.has_key("SCRAMRT_LOCALRT"):
             # try scram v1
             self.scramArea = os.environ["SCRAMRT_LOCALRT"]
@@ -49,7 +49,7 @@ class Scram:
                 verFile.close()
             pass
         else:
-            msg = 'Did you do eval `scram(v1) runtime ...` from your working area ?\n'
+            msg = 'Did you do cmsenv from your working area ?\n'
             raise CrabException(msg)
         common.logger.debug(5, "Scram::Scram() version is "+str(self.scramVersion))
         common.logger.debug(6, "Scram::Scram() area is "+self.scramArea)
@@ -74,7 +74,7 @@ class Scram:
         ver = ''
         envFileName=self.scramArea+"/.SCRAM/Environment"
         if os.environ.has_key("CMSSW_VERSION"):
-           ver= os.environ["CMSSW_VERSION"]
+            ver= os.environ["CMSSW_VERSION"]
         ##SL Else take  sw version from scramArea/.SCRAM/Environment
         if os.path.exists(envFileName):
             reVer=re.compile( r'SCRAM_PROJECTVERSION=(\S*)' )
@@ -87,36 +87,36 @@ class Scram:
                 pass
             envFile.close()
         else:
-            msg = 'Cannot find sw version:\n'
+            msg  = 'Cannot find sw version:\n'
+            msg += 'Did you do cmsenv from your working area or is your are corrupt?\n'
             raise CrabException(msg)
         return string.strip(ver)
-        
+
     def getReleaseTop_(self):
-       """ get release top """
+        """ get release top """
 
-       result = ''
-       archenvFile = "%s/.SCRAM/%s/Environment"%(self.scramArea,self.scramArch)
-       if (os.path.exists(archenvFile)):
-         envFileName = archenvFile
-       else:
-         envFileName = self.scramArea+"/.SCRAM/Environment"
+        result = ''
+        archenvFile = "%s/.SCRAM/%s/Environment"%(self.scramArea,self.scramArch)
+        if (os.path.exists(archenvFile)):
+            envFileName = archenvFile
+        else:
+            envFileName = self.scramArea+"/.SCRAM/Environment"
 
-       try:
-           envFile = open(envFileName, 'r')
-           for line in envFile:
-               line = string.strip(line)
-               (k, v) = string.split(line, '=')
-               if k == 'RELEASETOP':
-                   result=v
-                   break
-               pass
-           pass
-       except IOError:
-           msg = 'Cannot open scram environment file '+envFileName
-           raise CrabException(msg)
-           pass
-       pass
-       return string.strip(result)
+        try:
+            envFile = open(envFileName, 'r')
+            for line in envFile:
+                line = string.strip(line)
+                (k, v) = string.split(line, '=')
+                if k == 'RELEASETOP':
+                    result=v
+                    break
+                pass
+            pass
+        except IOError:
+            msg = 'Cannot open scram environment file '+envFileName
+            raise CrabException(msg)
+
+        return string.strip(result)
 
     def findFile_(self, filename):
         """
