441541d35bc4ff682164c8232c46fe1f4c4dc9d4
spiga <>
2008-10-11 15:56:29 +0000

    added user jobs display replicating crab -status through the web. Minor chages for protection..

--- CRABSERVER/src/python/Plugins/HTTPFrontend/UserMonitoring.py
+++ CRABSERVER/src/python/Plugins/HTTPFrontend/UserMonitoring.py
@@ -12,12 +12,12 @@ class TaskLogMonitor:
         html = """<html><body><h2>CrabServer Tasks: internal server logging</h2>\n """
 
         html += "<table>\n"
-        html += "<i> </i><br/><br/>"
+        html += "<i>Filling the field with the string resulting from 'crab -printId' allow to check both staus and logging: </i><br/><br/>"
         html += '<form action=\"%s"\ method="get">' % (self.visualize)
         html += 'Task unique name '
-        html += '<input type="text" name="taskname" size=50>'
-        html += '<input type="submit" value="Show logging info"/>'
-        html += '</select>'
+        html += ' <input type="text" name="taskname" size=50> '
+        html += ' <select name="logtype" style="width:80px"><option>Status</option><option>Logging</option></select>'
+        html += '<input type="submit" value="Show"/>'
         html += '</form>'
         html += "</table>\n"
 
@@ -34,6 +34,7 @@ class TaskLogVisualizer:
         return
 
     def getDbox(self):
+
         from ProdAgentCore.Configuration import loadProdAgentConfiguration
         dbox = None
         try:
@@ -45,25 +46,27 @@ class TaskLogVisualizer:
             raise Exception("Problem loading server configuration info " + str(exc))
         return dbox
 
-    def index(self, taskname):
+    def index(self, taskname, logtype):
+        self.taskname = taskname 
         dbox = self.getDbox()
-        filepath = os.path.join(dbox, (taskname + "_spec"), "internalog.xml") 
-        c = XmlFramework()
 
+        if taskname == 'ListAllTaskDir':
+            return  self.listAllDir()
+
+        if logtype == 'Status': filename = "xmlReportFile.xml"         
+        elif logtype == 'Logging': filename =  "internalog.xml" 
+       
+        filepath = os.path.join(dbox, (self.taskname + "_spec"),filename)
+        
+        
+        errhtml = "<html><body><h2>Internal server logging for task: " + str(self.taskname) + "</h2>\n "
         if not os.path.exists( filepath ):
-            raise Exception("Logging info file not existing")
-
-        c.fromFile( filepath )
-
-        html = "<html><body><h2>Internal server logging for task: " + str(taskname) + "</h2>\n "
-
-        html += '<table cellspacing="10">\n'
-
-        for key, value in c.getEventValues().iteritems():
-            html += '<tr><td colspan="2">Event: <b>'+str(value['ev'])+'</b></td></tr>\n'
-            for key2, value2 in value.iteritems():
-                if len(value2) > 0 and key2 != "ev": 
-                    html += '<tr> <td align="right">'+str(key2)+': </td><td>'+str(value2)+'</td></tr>\n'
+            errhtml += "<tr> %s info for task :   ' %s '</tr></br>\n"%(logtype,self.taskname)
+            errhtml += "<tr>  not existing in this CrabServer. Please check the Task ID. </tr>\n"
+            return errhtml
+           # raise Exception("Logging info file %s not existing"% filepath)
+  
+        html = self.showHtml( logtype ,filepath ) 
             
         html += "</table>\n"
         html += """</body></html>"""
@@ -71,6 +74,53 @@ class TaskLogVisualizer:
 
     index.exposed = True
 
+    def listAllDir(self):
+        dbox = self.getDbox()
+        html = "<html><body><h2>List Of All Task Dir</h2>\n "
+        for f in os.listdir( dbox ):
+            html += "<tr> Task Dir Name :   ' %s '</tr></br>\n"%(f)
+        return html
+
+    def showHtml(self, logtype, filepath):
+         
+        if logtype == 'Status': html = self.showStatus(filepath)
+        elif logtype == 'Logging': html = self.showLogging(filepath)
+
+        return html
+
+    def showLogging( self, filepath):
+
+        c = XmlFramework()
+        c.fromFile( filepath )
+
+        html = "<html><body><h2>Internal server logging for task: " + str(self.taskname) + "</h2>\n "
+        html += '<table cellspacing="10">\n'
+
+        for key, value in c.getEventValues().iteritems():
+            html += '<tr><td colspan="2">Event: <b>'+str(value['ev'])+'</b></td></tr>\n'
+            for key2, value2 in value.iteritems():
+                if len(value2) > 0 and key2 != "ev": 
+                    html += '<tr> <td align="right">'+str(key2)+': </td><td>'+str(value2)+'</td></tr>\n'
+        return  html
+    
+    def showStatus( self, filepath ):
+
+        c = XmlFramework("TaskTracking")
+        c.fromFile( filepath )
+
+        html = "<html><body><h2>Staus of task : " + str(self.taskname) + "</h2>\n "
+        html += '<table cellspacing="10">\n'
+
+        #st = ['Job','Status','Destination','Job_exit_code','Exe_exit_code','Submission Number']
+        st = ['Status','Destination','Job_exit_code','Exe_exit_code']
+        for s in st:        
+            html += '<td align="left"> <td><b>%s</b></td>\n'%s
+        for i in c.getJobValues().values():
+            html += '<tr></tr>\n'
+            for key2, value2 in i.items():
+                if len(value2) > 0 and key2 not in  ['id','resubmit',"sched_id","cleared"]:
+                    html += '<td align="left"> <td>%s</td>\n'%str(value2)
+        return  html
 
 if __name__=="__main__":
     tlv = TaskLogVisualizer()
