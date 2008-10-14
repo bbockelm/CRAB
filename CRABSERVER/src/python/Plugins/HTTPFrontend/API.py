dca1628cea001bbd87371e8fa97b1761f1b4eac3
spiga <>
2008-10-14 22:57:23 +0000

    new api for dataset inofs display

--- CRABSERVER/src/python/Plugins/HTTPFrontend/API.py
+++ CRABSERVER/src/python/Plugins/HTTPFrontend/API.py
@@ -89,9 +89,18 @@ def getTasks(from_time, ended=''):
     taskCheck = queryMethod(queryString)
     return taskCheck
 
-def getUserTasks(username):
-    queryString = "SELECT taskName, status  FROM js_taskInstance " +\
-                  "WHERE user_name = '%s' ORDER BY land_time DESC;"%(username)
+def getUserTasks(username='', from_time='', ended=''):
+    user = ''
+    if username != 'All':
+        user = " and user_name = '%s'" %(username)
+    timer = from_time
+    if from_time != '':
+        timer = " and land_time > DATE_SUB(Now(),INTERVAL %s SECOND)" %(str(from_time))
+    notif = ended
+    if ended == True: notif = " and notificationSent > 1"
+    elif ended == False: notif =" and notificationSent < 2"
+    queryString = "SELECT taskName, status, endedLevel FROM js_taskInstance " +\
+                  "WHERE 1 %s %s %s ORDER BY land_time DESC;"%(user, notif, timer)
     taskCheck = queryMethod(queryString)
     return taskCheck
 
@@ -145,6 +154,44 @@ def getTimeStatusJob( time ) :
     return taskCheck
 
 
+def getKeyNum_task(key,destination='all',from_time='all'):
+    dateCondition =  " and land_time  > DATE_SUB(Now(),INTERVAL "+str(from_time)+"  SECOND) "
+    dest_condition = composeDestinationCondition(destination);
+    queryString = "select count("+key+"),"+key+" from bl_task  bl_task join js_taskInstance on\
+                    bl_task.name=js_taskInstance.taskName  where 1 "+dateCondition+" "+dest_condition+" group by "+key+" "
+    taskCheck = queryMethod(queryString)
+    
+    return taskCheck
+
+
+def countTasks(key,from_time='all'):
+    datasetCondition =  " where dataset='%s' "%key 
+    dateCondition =  " and land_time  > DATE_SUB(Now(),INTERVAL "+str(from_time)+"  SECOND) "
+    queryString = "select count(bl_task.id) from bl_task bl_task join js_taskInstance on \
+                   bl_task.name=js_taskInstance.taskName "+datasetCondition+" "+dateCondition+";"
+    taskCheck = queryMethod(queryString)[0][0]
+    
+    return taskCheck
+
+def countJobs(key,from_time='all'):
+    datasetCondition =  " where dataset='%s' "%key 
+    dateCondition =  " and submission_time  > DATE_SUB(Now(),INTERVAL "+str(from_time)+"  SECOND) "
+    queryString = "select count(bl_runningjob.id)  from bl_runningjob left join  bl_task on (bl_runningjob.task_id=bl_task.id) "\
+                   +datasetCondition+" "+dateCondition+";"
+    taskCheck = queryMethod(queryString)
+    
+    return taskCheck[0][0]
+
+def countUsers(key,from_time='all'):
+    datasetCondition =  " where dataset='%s' "%key 
+    dateCondition =  " and land_time  > DATE_SUB(Now(),INTERVAL "+str(from_time)+"  SECOND) "
+    queryString = "select count(distinct(user_name)) from js_taskInstance js_taskInstance join bl_task on \
+                   bl_task.name=js_taskInstance.taskName "+datasetCondition+" "+dateCondition+";"
+    taskCheck = queryMethod(queryString)[0][0]
+    
+    return taskCheck
+
+
 def getKeyNum(key,destination='all',from_time='all'):
     dateCondition =  " and submission_time  > DATE_SUB(Now(),INTERVAL "+str(from_time)+"  SECOND) "
     dest_condition = composeDestinationCondition(destination);
@@ -153,6 +200,47 @@ def getKeyNum(key,destination='all',from_time='all'):
     
     return taskCheck
 
+def getTaskNameList(dataset,from_time):
+
+    datasetCondition =  " where dataset='%s' "%dataset            
+    dateCondition =  " and land_time  > DATE_SUB(Now(),INTERVAL "+str(from_time)+"  SECOND) "
+    queryString = "select taskName,user_name from js_taskInstance join bl_task on (js_taskInstance.taskName=bl_task.name) "+datasetCondition+" "+dateCondition+";"
+    taskCheck = queryMethod(queryString)
+    return taskCheck
+
+def getUserNameList(dataset,from_time):
+
+    datasetCondition =  " where dataset='%s' "%dataset            
+    dateCondition =  " and land_time  > DATE_SUB(Now(),INTERVAL "+str(from_time)+"  SECOND) "
+    queryString = "select count(user_name),user_name from js_taskInstance join bl_task on (js_taskInstance.taskName=bl_task.name) "\
+                   +datasetCondition+" "+dateCondition+"group by user_name;"
+    taskCheck = queryMethod(queryString)
+    return taskCheck
+
+
+def getJobExit(dataset,from_time):
+    datasetCondition =  " where dataset='%s' "%dataset            
+    dateCondition =  " and submission_time  > DATE_SUB(Now(),INTERVAL "+str(from_time)+"  SECOND) "
+    queryString = "select application_return_code, wrapper_return_code  from bl_runningjob left join  bl_task on (bl_runningjob.task_id=bl_task.id) " \
+                   +datasetCondition+" "+dateCondition+";"
+    taskCheck = queryMethod(queryString)
+    return taskCheck
+
+def getWrapExit(dataset,from_time):
+    datasetCondition =  " and dataset='%s' "%dataset            
+    dateCondition =  " and submission_time  > DATE_SUB(Now(),INTERVAL "+str(from_time)+"  SECOND) "
+    queryString = "select count(wrapper_return_code),wrapper_return_code  from bl_runningjob left join  bl_task on (bl_runningjob.task_id=bl_task.id) where \
+                   application_return_code = 0 " +datasetCondition+" "+dateCondition+" group by wrapper_return_code;"
+    taskCheck = queryMethod(queryString)
+    return taskCheck
+
+def getApplExit(dataset,from_time):
+    datasetCondition =  " where dataset='%s' "%dataset            
+    dateCondition =  " and submission_time  > DATE_SUB(Now(),INTERVAL "+str(from_time)+"  SECOND) "
+    queryString = "select count(application_return_code),application_return_code  from bl_runningjob left join  bl_task on (bl_runningjob.task_id=bl_task.id) "\
+                   +datasetCondition+" "+dateCondition+" group by wrapper_return_code;"
+    taskCheck = queryMethod(queryString)
+    return taskCheck
 
 # Statistics users
 def getUserName(from_time):
