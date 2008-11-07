553e6ce4071f94ea9299fefbffb4de602dd75594
ewv <>
2008-11-07 20:28:19 +0000

    Add glidein, same as all other non-gLite schedulers

--- CRABSERVER/src/python/TaskRegister/RegisterWorker.py
+++ CRABSERVER/src/python/TaskRegister/RegisterWorker.py
@@ -6,8 +6,8 @@ Implements thread logic used to perform Crab task reconstruction on server-side.
 
 """
 
-__revision__ = "$Id: RegisterWorker.py,v 1.11 2008/10/27 18:32:49 spiga Exp $"
-__version__ = "$Revision: 1.11 $"
+__revision__ = "$Id: RegisterWorker.py,v 1.12 2008/10/28 10:47:42 mcinquil Exp $"
+__version__ = "$Revision: 1.12 $"
 
 import string
 import sys, os
@@ -37,17 +37,17 @@ class RegisterWorker(Thread):
         self.configs = threadAttributes
 
         self.taskName = self.configs['taskname']
-        self.wdir = self.configs['wdir']       
-         
+        self.wdir = self.configs['wdir']
+
         # derived attributes
         self.blDBsession = BossLiteAPI('MySQL', dbConfig, pool=self.configs['blSessionPool'])
         self.seEl = SElement(self.configs['SEurl'], self.configs['SEproto'], self.configs['SEport'])
         self.local_queue = self.configs['messageQueue']
-        
+
         self.cfg_params = {}
         self.proxy = "anonymous"
         self.cmdRng = "[]"
-        
+
         # run the worker
         try:
             self.start()
@@ -57,7 +57,7 @@ class RegisterWorker(Thread):
         ## CW DB init
         self.cwdb = CrabWorkerAPI( self.blDBsession.bossLiteDB )
         return
-        
+
     def run(self):
         self.log.info("RegisterWorker %s initialized"%self.myName)
         self.local_queue.put( (self.myName, "TaskRegister:TaskArrival", self.taskName) )
@@ -73,7 +73,7 @@ class RegisterWorker(Thread):
             return
 
         # declare and customize the task object on the server
-        reconstructedTask = self.declareAndLocalizeTask() 
+        reconstructedTask = self.declareAndLocalizeTask()
         if reconstructedTask is None:
             self.local_queue.put((self.myName, "RegisterWorkerComponent:RegisterWorkerFailed", self.taskName))
             return
@@ -87,23 +87,23 @@ class RegisterWorker(Thread):
         # check if the ISB are where they should
         if self.inputFileCheck(reconstructedTask) == False:
              self.local_queue.put((self.myName, "RegisterWorkerComponent:RegisterWorkerFailed", self.taskName))
-             return            
+             return
 
         if self.type == 'fullySpecified': # default by client side to backward compatibility
            payload = self.taskName +"::"+ str(self.configs['retries']) +"::"+ self.cmdRng
            self.local_queue.put( (self.myName, "TaskRegisterComponent:NewTaskRegistered", payload) )
         else:
-           id = 'taskid' 
+           id = 'taskid'
            wmbs = WMBSInterface(_input_stuff_)
            result = wmbs.run()
-           
+
            # interact with WMBS ... + messaggio "ritorna qui tra n-ore"
            # prepare a payload for the interaction message
-           # self.local_queue.put( (self.myName, "TaskRegisterComponent:msgXYZ", payload) )  
+           # self.local_queue.put( (self.myName, "TaskRegisterComponent:msgXYZ", payload) )
 
         self.log.info("RegisterWorker %s finished"%self.myName)
         return
-        
+
     def sendResult(self, status, reason, logMsg):
         self.log.info(logMsg)
         msg = self.myName + "::" + self.taskName + "::"
@@ -112,9 +112,9 @@ class RegisterWorker(Thread):
         return
 
 ####################################
-    # RegisterWorker methods 
+    # RegisterWorker methods
 ####################################
-    
+
     def parseCommandXML(self):
         status = 0
         self.log.info('Worker %s parsing creation command'%self.myName)
@@ -122,14 +122,14 @@ class RegisterWorker(Thread):
         try:
             doc = minidom.parse(cmdSpecFile)
             cmdXML = doc.getElementsByTagName("TaskCommand")[0]
-            
+
             self.cfg_params = eval( cmdXML.getAttribute("CfgParamDict"), {}, {} )
             self.cmdRng =  str( cmdXML.getAttribute('Range') )
             self.proxySubject = str( cmdXML.getAttribute('Subject') )
 
             self.schedName = str( cmdXML.getAttribute('Scheduler') ).upper()
 
-            self.flavour = str( cmdXML.getAttribute('Flavour') ) 
+            self.flavour = str( cmdXML.getAttribute('Flavour') )
             self.type = str( cmdXML.getAttribute('Type') )
         except Exception, e:
             status = 6
@@ -147,12 +147,12 @@ class RegisterWorker(Thread):
             try:
                 tmpTask = self.blDBsession.loadTaskByName(self.taskName)
             except Exception, ee:
-                tmpTask = None 
+                tmpTask = None
 
             if tmpTask is not None:
                 self.log.info("Task %s already registered in BossLite"%self.taskName)
                 return tmpTask
- 
+
             taskObj = self.blDBsession.declare(taskSpecFile, self.proxy)
             taskObj['user_proxy'] = self.proxy
         except Exception, e:
@@ -161,27 +161,28 @@ class RegisterWorker(Thread):
             self.sendResult(status, reason, reason)
             self.log.info( traceback.format_exc() )
             return None
-          
+
         if taskObj is not None:
             taskObj = self.alterPath(taskObj)
-         
+
         # all done
-        return taskObj 
+        return taskObj
 
     def alterPath(self, taskObj):
         """
-        here the Input and Output SB locations 
+        here the Input and Output SB locations
         are defined and related fields are filled.
         These infos are finally picked-up for JDL creation
         for this reason the related management is scheduler
-        dependent. 
-        Allowed Schedulers Name:  
-        1) GLITE     
-        2) GLITECOLL 
-        3) CONDOR_G  
-        4) ARC       
-        5) LSF       
-        6) CAF       
+        dependent.
+        Allowed Schedulers Name:
+        1) GLITE
+        2) GLITECOLL
+        3) CONDOR_G
+        4) ARC
+        5) LSF
+        6) CAF
+        7) GLIDEIN
         """
         self.log.info('Worker %s altering paths'%self.myName)
 
@@ -195,12 +196,9 @@ class RegisterWorker(Thread):
                 turlFileCandidate = remoteSBlist[0]
                 self.preamble = SBinterface(self.seEl).getTurl( turlFileCandidate, self.proxy )
                 self.preamble = self.preamble.split(remoteSBlist[0])[0]
-            elif self.schedName in ['LSF','CAF']:
+            elif self.schedName in ['LSF', 'CAF', 'CONDOR_G', 'ARC', 'GLIDEIN']:
                 self.log.info('Worker %s  NO TURL needed (Scheduler Name %s)  '%(self.myName, self.schedName))
                 self.preamble = ''
-            elif self.schedName in ['CONDOR_G','ARC'] : 
-                self.preamble = ''
-                self.log.info('Worker %s  NO TURL needed (Scheduler Name %s)  '%(self.myName, self.schedName))
             else:
                 self.log.info('Worker %s  Scheduler %s  Not Known  '%(self.myName, self.schedName))
                 return None
@@ -210,22 +208,22 @@ class RegisterWorker(Thread):
             taskObj['startDirectory'] = self.preamble
             taskObj['outputDirectory'] = self.preamble + self.cfg_params['CRAB.se_remote_dir']
             taskObj['cfgName'] = self.preamble + os.path.join ( self.cfg_params['CRAB.se_remote_dir'], os.path.basename(taskObj['cfgName']) )
- 
+
             self.log.debug("Worker %s. Reference Preamble: %s"%(self.myName, taskObj['outputDirectory']) )
- 
+
             for j in taskObj.jobs:
                 j['executable'] = os.path.basename(j['executable'])
                 j['outputFiles'] = [ os.path.basename(of) for of in j['outputFiles']  ]
- 
+
                 jid = taskObj.jobs.index(j)
                 if 'crab_fjr_%d.xml'%(jid+1) not in j['outputFiles']:
-                    j['outputFiles'].append('crab_fjr_%d.xml'%(jid+1)) 
+                    j['outputFiles'].append('crab_fjr_%d.xml'%(jid+1))
                     #'file://' + destDir +'_spec/crab_fjr_%d.xml'%(jid+1) )
                 if '.BrokerInfo' not in j['outputFiles']:
                     j['outputFiles'].append('.BrokerInfo')
- 
+
             # save changes
-            try: 
+            try:
                 self.blDBsession.updateDB(taskObj)
             except Exception, e:
                 status = 6
@@ -238,8 +236,8 @@ class RegisterWorker(Thread):
 
 
     def registerTask(self, taskArg):
- 
-        taskName = taskArg['name']  
+
+        taskName = taskArg['name']
 
         range = eval(self.cmdRng)
 
@@ -247,8 +245,8 @@ class RegisterWorker(Thread):
             jobName = job['name']
             cacheArea = os.path.join( self.wdir, taskName + '_spec', jobName )
             we_status = 'create'
-            if job['jobId'] in range : we_status = 'Submitting' 
-            jobDetails = { 
+            if job['jobId'] in range : we_status = 'Submitting'
+            jobDetails = {
                           'id':jobName, 'job_type':'Processing', 'cache':cacheArea, \
                           'owner':taskName, 'status': we_status, \
                           'max_retries': self.configs['retries'], 'max_racers':1 \
@@ -257,10 +255,10 @@ class RegisterWorker(Thread):
             try:
                 jobAlreadyRegistered = self.cwdb.existsWEJob(jobName)
             except Exception, e:
-                ##TODO: need to differnciate when more then 1 entry per job (limit case) 
+                ##TODO: need to differnciate when more then 1 entry per job (limit case)
                 logMsg = 'Error while checking job registration: assuming %s as not registered'%jobName
                 logMsg +=  traceback.format_exc()
-                self.log.info (logMsg) 
+                self.log.info (logMsg)
                 jobAlreadyRegistered = False
 
             if jobAlreadyRegistered == True:
@@ -272,15 +270,15 @@ class RegisterWorker(Thread):
             except Exception, e:
                 logMsg = 'Error while registering job for JT: %s'%jobName
                 logMsg +=  traceback.format_exc()
-                self.log.info (logMsg) 
+                self.log.info (logMsg)
                 return 1
             self.log.debug('Registration for %s performed'%jobName)
         return 0
 
     def inputFileCheck(self, task):
-        self.log.info('Worker %s checking input sanbox'%self.myName) 
+        self.log.info('Worker %s checking input sanbox'%self.myName)
         sbList = task['globalSandbox'].split(',')
-        if len(sbList)==0: return True 
+        if len(sbList)==0: return True
 
         try:
             for f in sbList:
@@ -294,15 +292,15 @@ class RegisterWorker(Thread):
                     if fileFound == True: break
                     checkCount -= 1
                     self.log.info("Worker %s. Checking file %s"%(self.myName, remoteFile))
-                    time.sleep(10) 
+                    time.sleep(10)
                     pass
-                
+
                 if fileFound == False:
                     status = 20
                     reason = "Worker %s. Missing file %s"%(self.myName, remoteFile)
                     self.sendResult(status, reason, reason)
                     return False
-                     
+
         except Exception, e:
             status = 20
             reason = "Worker %s. Error checking if file %s exists"%(self.myName, remoteFile)
@@ -310,7 +308,7 @@ class RegisterWorker(Thread):
             self.log.info( traceback.format_exc() )
             return False
         return True
- 
+
     def associateProxyToTask(self):
         """
         Check whether there are macroscopic conditions that prevent the task to be submitted.
@@ -325,15 +323,15 @@ class RegisterWorker(Thread):
         try:
             assocFile = self.getProxyFile()
             #assocFile = self.getProxyFileMyProxy()
-            if assocFile is not None: 
+            if assocFile is not None:
                 self.proxy = str(assocFile)
                 self.log.info("Project -> Task association: %s -> %s"%(self.taskName, assocFile) )
                 return 0
         except Exception, e:
-            reason = "Warning: error while linking the proxy file for task %s."%self.taskName 
+            reason = "Warning: error while linking the proxy file for task %s."%self.taskName
             self.log.info(reason)
             self.log.info( traceback.format_exc() )
-            
+
         status = 20
         reason = "Unable to locate a proper proxy for the task %s"%(self.taskName)
         self.sendResult(status, reason, reason)
@@ -359,7 +357,7 @@ class RegisterWorker(Thread):
                 continue
 
             ps = ps.split('\n')
-            if len(ps)>0: 
+            if len(ps)>0:
                 ps = str(ps[0]).strip()
                 if proxySubject in ps or ps in proxySubject:
                     return pf
@@ -369,8 +367,8 @@ class RegisterWorker(Thread):
         from CrabServer.myproxyDelegation import myProxyDelegationServerside as myproxyService
         import sha # to compose secure proxy name
         #
-        ##TODO check if these are set in the main component and/or transfered from client-side 
-        srvKeyPath = self.configs.get('X509_KEY', '~/.globus/hostkey.pem') 
+        ##TODO check if these are set in the main component and/or transfered from client-side
+        srvKeyPath = self.configs.get('X509_KEY', '~/.globus/hostkey.pem')
         srvCertPath = self.configs.get('X509_CERT', '~/.globus/hostcert.pem')
         pf = os.path.join(self.configs['ProxiesDir'], sha.new(self.proxySubject).hexdigest() ) # proxy filename
         #
