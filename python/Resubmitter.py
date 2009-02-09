59ff3927da1d4a94db9a5f6d048d6770cb18c5f4
mcinquil <>
2009-02-09 14:51:38 +0000

    Addin Not Submitted jobs at resubmittable jobs

--- python/Resubmitter.py
+++ python/Resubmitter.py
@@ -24,7 +24,7 @@ class Resubmitter(Submitter):
         for job in task.jobs:
             st = job.runningJob['status']
             nj = job['jobId']
-            if st in ['K','A','SE','E','UE','DA']:
+            if st in ['K','A','SE','E','UE','DA','NS']:
                 nj_list.append(int(nj))
             elif st == 'C':
                 common.logger.message('Job #'+`int(nj)`+' has status '+str(job.runningJob['statusScheduler'])+' not yet submitted!!!')
