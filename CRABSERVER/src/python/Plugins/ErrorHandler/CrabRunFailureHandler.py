80039a5a09237a55ad89b63f61de9dcdf671d3b5
slacapra <>
2009-02-17 14:10:02 +0000

    fix url

--- CRABSERVER/src/python/Plugins/ErrorHandler/CrabRunFailureHandler.py
+++ CRABSERVER/src/python/Plugins/ErrorHandler/CrabRunFailureHandler.py
@@ -58,7 +58,7 @@ class CrabRunFailureHandler(HandlerInterface):
         self.localEMFileName = "ErrorHandlerMatrix.txt"
 
         ### TEMPORARY
-        self.url = "https://cmsweb.cern.ch/crabconf/files/"
+        self.url = "https://cmsweb.cern.ch/crabconf/"
         self.initializeActions()
         self.cacheLocation = '' # where the cached ErrorMatrix will be put
 
