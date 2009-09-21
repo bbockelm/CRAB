6e10c21361b61ba597ef8f975bbfeca5b015ef4f
riahi <>
2009-09-21 23:38:06 +0000

    initial commit

---
+++ CRABSERVER/src/python/CrabJobCreator/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+_CrabJobCreator_
+   CrabJobCreator pools wmbs DB to check subscription ready for splitting. 
+   Extends task in bossliteDB creating its jobs and argument.xml file. 
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 0 2009/09/22 00:46:10 riahi Exp $"
+__version__ = "$Revision: 0 $"
+__author__ = "hassen.riahi@cern.ch"
+
