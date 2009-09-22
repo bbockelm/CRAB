fdcfdb2518c8eb3c85c63b62a4680f3e244b3890
spiga <>
2009-09-22 22:17:59 +0000

    update PComonn

--- python/PrepareTarBall.sh
+++ python/PrepareTarBall.sh
@@ -13,8 +13,8 @@ CRABdir=$tag
 echo "CRABDIR = $CRABdir"
 CRABtag=$tag
 DBSAPItag="DBS_2_0_7_patch_1"
-DLSAPItag="DLS_1_1_1_IN2P3_XT"
-PRODCOMMONtag="PRODCOMMON_0_12_16_CRAB_1_PATCH_2"
+DLSAPItag="DLS_1_1_2"
+PRODCOMMONtag="PRODCOMMON_0_12_16_CRAB_1_PATCH_3"
 WMCOREtag="WMCORE_0_1_1_pre11"
 
 CVSrepo=":pserver:anonymous@cmscvs.cern.ch:/cvs_server/repositories"
