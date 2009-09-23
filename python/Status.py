6ffebb2a749f83bdd2858fb72370b06ce6d1f2b6
mcinquil <>
2009-09-23 20:36:37 +0000

    Fixes on status/action for status listing per job and for report

--- python/Status.py
+++ python/Status.py
@@ -10,6 +10,7 @@ class Status(Actor):
         self.cfg_params = args[0]
         self.verbose = (str(self.cfg_params.get("CRAB.status",'')) in ('verbose','v'))
         self.xml = self.cfg_params.get("USER.xml_report",'')
+        ## needed to check server name and difference from SA
         self.server_name = ''
  
         return
@@ -75,17 +76,23 @@ class Status(Actor):
 
             if exe_exit_code == 'None' :  exe_exit_code = ''
             if job_exit_code == 'None' :  job_exit_code = ''
+
+            ## status remapping
             if job.runningJob['state'] == 'SubRequested' : jobStatus = 'Submitting'
             if job.runningJob['state'] == 'Terminated': jobStatus = 'Done'
+
             #This is needed for StandAlone
-            if job.runningJob['status'] in ['SD','DA'] : 
-                listId.append(id)
-                listRunField.append(run_jobToSaveTerm)
-                listJobField.append(jobToSave)
-            elif job.runningJob['status'] in ['A'] : 
-                listId.append(id)
-                listRunField.append(run_jobToSaveAbort)
-                listJobField.append(jobToSave)
+            if len(self.server_name) == 0:
+                if job.runningJob['status'] in ['SD','DA'] : 
+                    listId.append(id)
+                    listRunField.append(run_jobToSaveTerm)
+                    listJobField.append(jobToSave)
+                    jobState = 'Terminated'
+                elif job.runningJob['status'] in ['A'] : 
+                    listId.append(id)
+                    listRunField.append(run_jobToSaveAbort)
+                    listJobField.append(jobToSave)
+                    jobState = 'Aborted'
 
             if (self.verbose) :printline+="%-6s %-18s %-14s %-36s %-13s %-16s %-4s" % (id,jobStatus,jobState,dest,exe_exit_code,job_exit_code,ended)
             else: printline+="%-6s %-18s %-36s %-13s %-16s " % (id,jobStatus,dest,exe_exit_code,job_exit_code)
@@ -147,40 +154,79 @@ class Status(Actor):
             if c != 'None':
                 self.reportCodes(c)
             else:
+                terminatedListId = []
                 for st in possible_status:
                     list_ID = common._db.queryAttrRunJob({'statusScheduler':st},'jobId')
                     if len(list_ID)>0:
                         if st == 'killed':
-                            msg+=  ">>>>>>>>> %i Jobs %s \n" % (len(list_ID), str(st))
+                            msg+=  " >>>>>>>>> %i Jobs %s \n" % (len(list_ID), str(st))
                             msg+=  "\tYou can resubmit them specifying JOB numbers: crab -resubmit <List of jobs>\n"
                             msg+=  "\tList of jobs: %s \n" % readableList(self,list_ID)
+
                         elif st == 'Aborted':
-                            msg+=  ">>>>>>>>> %i Jobs %s\n " % (len(list_ID), str(st))
+                            msg+=  " >>>>>>>>> %i Jobs %s\n" % (len(list_ID), str(st))
                             msg+=  "\tYou can resubmit them specifying JOB numbers: crab -resubmit <List of jobs>\n"
                             msg+=  "\tList of jobs: %s \n" % readableList(self,list_ID)
-                        elif st == 'Done' or st == 'Done (Failed)' :
-                            msg+=  ">>>>>>>>> %i Jobs %s\n " % (len(list_ID), str(st))
-                            terminatedListId=[]
+
+                        elif st == 'Done' or st == 'Done (Failed)' or st == 'Cleared':
+                            cleanedListId=[]
+                            clearedListId=[]
+                            #terminatedListId=[]
                             notTerminatedListId=[]
                             for id in list_ID:
                                 job = task.jobs[id-1]
-                                if job.runningJob['state']=='Terminated': terminatedListId.append(id)
-                                else: notTerminatedListId.append(id)
+                                if job.runningJob['state'] == 'Terminated':
+                                    terminatedListId.append(id)
+                                elif job.runningJob['state'] == 'Cleaned':
+                                    cleanedListId.append(id)      
+                                elif job.runningJob['state'] == 'Cleared':
+                                    clearedListId.append(id)
+                                else:
+                                    notTerminatedListId.append(id)
                             if len(notTerminatedListId)>0:
-                                msg+=  "\tJobs still in final phase: cannot retrieve them, yet\n"
+                                msg+=  " >>>>>>>>> %i Jobs %s\n" % (len(notTerminatedListId), str(st))
+                                msg+=  "\tJobs still in final phase: cannot retrieve them, yet (few minutes of delay can occur when using the server) \n"
                                 msg+=  "\tList of jobs: %s \n" % readableList(self,notTerminatedListId)
-                            if len(terminatedListId)>0:
-                                msg+=  "\tJobs terminated: retrieve them with: crab -getoutput <List of jobs>\n"
-                                msg+=  "\tList of jobs: %s \n" % readableList(self,terminatedListId)
+                            #    if st in ['Cleared']:
+                            #        st = 'Done'
+                            #    msg+=  " >>>>>>>>> %i Jobs %s\n" % (len(list_ID), str(st))
+                            #    msg+=  "\tJobs terminated: retrieve them with: crab -getoutput <List of jobs>\n"
+                            #    msg+=  "\tList of jobs: %s \n" % readableList(self,terminatedListId)
+                            if len(cleanedListId)>0:
+                                msg+=  " >>>>>>>>> %i Jobs Cleaned\n" % (len(cleanedListId))
+                                msg+=  "\tJobs Cleaned by the server (because too old or the proxy was expired) \n"
+                                msg+=  "\tList of jobs: %s \n" % readableList(self,cleanedListId)
+                            if len(clearedListId)>0:
+                                msg+=  " >>>>>>>>> %i Jobs Cleared\n" % (len(clearedListId))
+                                msg+=  "\tList of jobs: %s \n" % (readableList(self,clearedListId))
+
                         elif st in ['NotSubmitted','CannotSubmit']:
-                            msg+=  ">>>>>>>>> %i Jobs %s\n " % (len(list_ID), str(st))
+                            msg+=  " >>>>>>>>> %i Jobs %s\n" % (len(list_ID), str(st))
                             msg+=  "\tCheck if they match resources with: crab -match <List of jobs>\n"
                             msg+=  "\tIf not, check data location (eg your data is just on T1's)\n"
                             msg+=  "\tand software version installation (eg the version you are using has been deprecared and is beeing removed\n"
                             msg+=  "\tList of jobs: %s \n" % readableList(self,list_ID)
-                        else   :
-                            msg+=  ">>>>>>>>> %i Jobs %s \n " % (len(list_ID), str(st))
+
+                        elif st in ['Created']:
+                            submittingList = []
+                            justCreatedList = []
+                            for id in list_ID:
+                                job = task.jobs[id-1]
+                                if job.runningJob['state'] == 'SubRequested': submittingList.append(id)
+                                else: justCreatedList.append(id)
+                            if len(submittingList) > 0:
+                                msg+=  " >>>>>>>>> %i Jobs Submitting \n" % (len(list_ID))
+                            if len(justCreatedList) > 0:
+                                msg+=  " >>>>>>>>> %i Jobs %s \n" % (len(list_ID), str(st))
+
+                        else:
+                            msg+=  " >>>>>>>>> %i Jobs %s \n" % (len(list_ID), str(st))
                             msg+=  "\tList of jobs %s: %s \n" % (str(st),readableList(self,list_ID))
+                if len(terminatedListId)>0:
+                    msg+=  " >>>>>>>>> %i Jobs Done\n" % (len(terminatedListId))
+                    msg+=  "\tJobs terminated: retrieve them with: crab -getoutput <List of jobs>\n"
+                    msg+=  "\tList of jobs: %s \n" % readableList(self,terminatedListId)
+	
         common.logger.info(msg)
         return
 
@@ -190,7 +236,7 @@ class Status(Actor):
         list_ID = common._db.queryAttrRunJob({'wrapperReturnCode':code},'jobId')
         if len(list_ID)>0:
             msg = 'ExitCodes Summary\n'
-            msg +=  ">>>>>>>>> %i Jobs with Wrapper Exit Code : %s \n " % (len(list_ID), str(code))
+            msg +=  " >>>>>>>>> %i Jobs with Wrapper Exit Code : %s \n" % (len(list_ID), str(code))
             msg +=  "\t List of jobs: %s \n" % readableList(self,list_ID)
             if (code!=0):
                 msg +=  "\tSee https://twiki.cern.ch/twiki/bin/view/CMS/JobExitCodes for Exit Code meaning\n"
