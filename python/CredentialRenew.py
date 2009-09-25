83e95940b45b9d7064316c271a35c74f7869fd9e
farinafa <>
2009-09-25 08:12:26 +0000

    RenewCredential now uses the delegation with myproxy in a clean way, without calling SubmitterServer methods

--- python/CredentialRenew.py
+++ python/CredentialRenew.py
@@ -20,19 +20,12 @@ class CredentialRenew(Actor):
     def run(self):
         """
         """
-        common.logger.debug("CredentialRenew::run() called")
-        ## TEMPORARY FIXME  
-        if self.credentialType == 'Proxy':
-            subServer = SubmitterServer(self.cfg_params, None, "all")
-            subServer.moveProxy()
-        else: 
-            self.renewer()    
-        common.logger.info("Credential successfully delegated to the server.\n")
-        return
 
-    def renewer(self):
-        """
-        """
+        common.logger.debug("CredentialRenew::run() called")
+
+        # FIXME With MyProxy delegation this part is completely overlapped with the method manageDelegation
+        # in SubmitServer. We should to maintain just one version of the method in a common part  
+
         myproxyserver = self.cfg_params.get('GRID.proxy_server', 'myproxy.cern.ch')
         configAPI = {'credential' : self.credentialType, \
                      'myProxySvr' : myproxyserver,\
@@ -43,18 +36,35 @@ class CredentialRenew(Actor):
                      'logger' : common.logger() \
                      }
         try:
-            CredAPI =  CredentialAPI( configAPI )            
-        except Exception, err : 
+            CredAPI =  CredentialAPI( configAPI )
+        except Exception, err :
             common.logger.debug( "Configuring Credential API: " +str(traceback.format_exc()))
             raise CrabException("ERROR: Unable to configure Credential Client API  %s\n"%str(err))
-        if not CredAPI.checkCredential(Time=100) :
-           common.logger.info("Please renew your %s :\n"%self.credentialType)
-           try:
-               CredAPI.ManualRenewCredential()
-           except Exception, ex:
-               raise CrabException(str(ex))
-        try:
-            dict = CredAPI.registerCredential() 
-        except Exception, err:
-            common.logger.debug( "Registering Credentials : " +str(traceback.format_exc()))
-            raise CrabException("ERROR: Unable to register %s delegating server: %s\n"%(self.credentialType,self.server_name ))
+
+        if self.credentialType == 'Proxy':
+             # Proxy delegation through MyProxy, 4 days lifetime minimum
+             if not CredAPI.checkMyProxy(Time=4, checkRetrieverRenewer=True) :
+                common.logger.info("Please renew MyProxy delegated proxy:\n")
+                try:
+                    CredAPI.credObj.serverDN = self.server_dn
+                    CredAPI.ManualRenewMyProxy()
+                except Exception, ex:
+                    common.logger.debug("Delegating Credentials to MyProxy : " +str(traceback.format_exc()))
+                    raise CrabException(str(ex))
+        else:
+            self.renewer()
+            if not CredAPI.checkCredential(Time=100) :
+                common.logger.info("Please renew your %s :\n"%self.credentialType)
+                try:
+                    CredAPI.ManualRenewCredential()
+                except Exception, ex:
+                    raise CrabException(str(ex))
+            try:
+                dict = CredAPI.registerCredential() 
+            except Exception, err:
+                common.logger.debug( "Registering Credentials : " +str(traceback.format_exc()))
+                raise CrabException("ERROR: Unable to register %s delegating server: %s\n"%\
+                    (self.credentialType,self.server_name ))
+
+        common.logger.info("Credential successfully delegated to the server.\n")
+        return
