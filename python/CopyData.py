e7e7c3f8cd61e5013433f3d216e57661b593df9f
ewv <>
2009-09-29 14:03:26 +0000

    Add srm-lcg for glidein, branch

--- python/CopyData.py
+++ python/CopyData.py
@@ -1,3 +1,6 @@
+__revision__ = "$Id: CopyData.py,v 1.21 2009/09/29 14:02:26 ewv Exp $"
+__version__  = "$Revision: 1.21 $"
+
 from Actor import *
 from crab_util import *
 from crab_exceptions import *
@@ -10,44 +13,45 @@ class CopyData(Actor):
         constructor
         """
         common.logger.debug("CopyData __init__() : \n")
-        
+
         if (cfg_params.get('USER.copy_data',0) == '0') :
             msg  = 'Cannot copy output if it has not \n'
             msg += '\tbeen stored to SE via USER.copy_data=1'
             raise CrabException(msg)
-            
+
         self.cfg_params = cfg_params
         self.nj_list = nj_list
-        
+
         ### default is the copy local
         self.copy_local = 1
         self.dest_se = cfg_params.get("CRAB.dest_se", 'local')
         self.dest_endpoint = cfg_params.get("CRAB.dest_endpoint", 'local')
-        
+
         if ((self.dest_se != 'local') and (self.dest_endpoint != 'local')):
             msg  = 'You can specify only a parameter with CopyData option \n'
             msg += '1) The dest_se in the case of official CMS SE (i.e -dest_se=T2_IT_Legnaro)\n'
             msg += '2) The complete endpoint for not official SE (i.e -dest_endpoint=srm://<se_name>:<port>/xxx/yyy/)\n'
-            msg += '3) if you do not specify parameters, the output will be copied in your UI under crab_working_dir/res \n' 
+            msg += '3) if you do not specify parameters, the output will be copied in your UI under crab_working_dir/res \n'
             raise CrabException(msg)
-            
+
         if ((self.dest_se != 'local') or (self.dest_endpoint != 'local')):
-           self.copy_local = 0
+            self.copy_local = 0
 
         # update local DB
         if StatusObj:# this is to avoid a really strange segv
             StatusObj.query(display=False)
-      
-        protocolDict = { 'CAF'      : 'rfio' , \
-                         'LSF'      : 'rfio' , \
-                         'CONDOR_G' : 'srmv2' , \
-                         'GLITE'    : 'srm-lcg' , \
-                         'CONDOR'   : 'srmv2',  \
-                         'SGE'      : 'srmv2', \
-                         'ARC'      : 'srmv2' \
-                       }  
-        self.protocol = protocolDict[common.scheduler.name().upper()]  
-       
+
+        protocolDict = { 'CAF'      : 'rfio',
+                         'LSF'      : 'rfio',
+                         'CONDOR_G' : 'srmv2',
+                         'GLITE'    : 'srm-lcg',
+                         'GLIDEIN'  : 'srm-lcg',
+                         'CONDOR'   : 'srmv2',
+                         'SGE'      : 'srmv2',
+                         'ARC'      : 'srmv2',
+                       }
+        self.protocol = protocolDict[common.scheduler.name().upper()]
+
         common.logger.debug("Protocol = %s; Destination SE = %s; Destination Endpoint = %s."%(self.protocol,self.dest_se,self.dest_endpoint))
 
     def run(self):
@@ -56,19 +60,19 @@ class CopyData(Actor):
         """
 
         results = self.copy()
-        self.parseResults( results )          
-        return 
+        self.parseResults( results )
+        return
 
     def copy(self):
         """
         1) local copy: it is the default. The output will be copied under crab_working_dir/res dir
-        2) copy to a remote SE specifying -dest_se (official CMS remote SE) 
-           or -dest_endpoint (not official, needed the complete endpoint) 
+        2) copy to a remote SE specifying -dest_se (official CMS remote SE)
+           or -dest_endpoint (not official, needed the complete endpoint)
         """
 
         to_copy = {}
         results = {}
-        
+
         lfn, to_copy = self.checkAvailableList()
 
         if (self.copy_local == 1):
@@ -86,16 +90,16 @@ class CopyData(Actor):
                 self.endpoint = self.endpoint + tmp
             else:
                 self.endpoint = self.dest_endpoint
-                                 
+
             common.logger.info("Copy file to remote SE.\n\tEndpoint: %s"%self.endpoint)
             dest = {"destination": self.endpoint}
-        
+
         for key in to_copy.keys():
             cmscpConfig = {
                         "source": key,
                         "inputFileList": to_copy[key],
                         "protocol": self.protocol
-                      }  
+                      }
             #if (self.protocol == "srmv2"):
             #    cmscpConfig.setdefault("option", " -retry_timeout=480000 -retry_num=3 ")
             #elif (self.protocol == "srm-lcg"):
@@ -106,20 +110,20 @@ class CopyData(Actor):
             common.logger.log(10-1,"Source = %s"%key)
             common.logger.log(10-1,"Files = %s"%to_copy[key])
             common.logger.log(10-1,"CmscpConfig = %s"%str(cmscpConfig))
-            
+
             results.update(self.performCopy(cmscpConfig))
         return results
-        
+
     def checkAvailableList(self):
         '''
-        check if asked list of jobs 
-        already produce output to move 
-        returns a dictionary {with endpoint, fileList} 
+        check if asked list of jobs
+        already produce output to move
+        returns a dictionary {with endpoint, fileList}
         '''
-      
+
         common.logger.debug("CopyData in checkAvailableList() ")
         # loop over jobs
-        
+
         task=common._db.getTask(self.nj_list)
         allMatch={}
         to_copy={}
@@ -127,7 +131,7 @@ class CopyData(Actor):
         for job in task.jobs:
             InfileList = ''
             if ( job.runningJob['state'] == 'Cleared' and job.runningJob[ 'wrapperReturnCode'] == 0):
-                id_job = job['jobId'] 
+                id_job = job['jobId']
                 common.logger.log(10-1,"job_id = %s"%str(id_job))
                 endpoint = job.runningJob['storage']
                 output_files = job.runningJob['lfn']
@@ -136,27 +140,27 @@ class CopyData(Actor):
                     InfileList += '%s,'%os.path.basename(file)
                     lfn = os.path.dirname(file)
                 common.logger.log(10-1,"InfileList = %s"%str(InfileList))
-                if to_copy.has_key(endpoint):     
+                if to_copy.has_key(endpoint):
                     to_copy[endpoint] = to_copy[endpoint] + InfileList
                 else:
                     to_copy[endpoint] = InfileList
-                     
+
             elif ( job.runningJob['state'] == 'Cleared' and job.runningJob['wrapperReturnCode'] != 0):
                 common.logger.info("Not possible copy outputs of Job # %s : Wrapper Exit Code is %s" \
                                       %(str(job['jobId']),str(job.runningJob['wrapperReturnCode'])))
-            else: 
+            else:
                 common.logger.info("Not possible copy outputs of Job # %s : Status is %s" \
                                        %(str(job['jobId']),str(job.runningJob['statusScheduler'])))
             pass
 
         if (len(to_copy) == 0) :
             raise CrabException("No files to copy")
-        
+
         for key in to_copy.keys():
             to_copy[key] = to_copy[key][:-1]
-        common.logger.debug(" to_copy = %s"%str(to_copy)) 
+        common.logger.debug(" to_copy = %s"%str(to_copy))
 
-        return lfn, to_copy 
+        return lfn, to_copy
 
     def performCopy(self, dict):
         """
@@ -176,15 +180,15 @@ class CopyData(Actor):
         return results
 
     def parseResults(self,results):
-        ''' 
-        take the results dictionary and
-        print the results 
         '''
-        for file, dict in results.items() : 
+        take the results dictionary and
+        print the results
+        '''
+        for file, dict in results.items() :
             if file:
-                txt = 'success' 
+                txt = 'success'
                 if dict['erCode'] != '0': txt = 'failed'
                 msg = 'Copy %s for file: %s '%(txt,file)
                 if txt == 'failed': msg += '\n\tCopy failed because : %s'%dict['reason']
                 common.logger.info( msg )
-        return 
+        return
