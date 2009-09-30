55a7aaeaa0691306e31ff27fbef8cc3dde96a199
riahi <>
2009-09-30 00:25:17 +0000

    Add CrabServer parameters

--- CRABSERVER/src/python/CrabJobCreator/DefaultConfig.py
+++ CRABSERVER/src/python/CrabJobCreator/DefaultConfig.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #pylint: disable-msg=E1101,E1103,C0103,R0902
 """
-Defines default config values for CrabJobCreator specific
+Defines default config values for CranWmbs specific
 parameters.
 """
 __all__ = []
-__revision__ = "$Id: DefaultConfig.py,v 0 2009/09/22 00:46:10 riahi Exp $"
-__version__ = "$Revision: 0 $"
+__revision__ = "$Id: DefaultConfig.py,v 0.2 2009/09/30 01:26:34 hriahi Exp $"
+__version__ = "$Revision: 0.2 $"
 
 import os
 from WMCore.Agent.Configuration import Configuration
@@ -21,9 +21,15 @@ config.CrabJobCreator.componentDir = \
 # The maximum number of threads to process each message type
 config.CrabJobCreator.maxThreads = 10
 
-# CrabServer parameters
+# CrabServer parameter
 config.CrabJobCreator.wdir = '/data/Storage/logs' 
 config.CrabJobCreator.maxRetries = 3
+config.CrabJobCreator.credentialType = "Proxy"
+config.CrabJobCreator.ProxiesDir = "/tmp/del_proxies/"
+config.CrabJobCreator.StorageName = "crab.pg.infn.it"
+config.CrabJobCreator.storagePort = "2811"
+config.CrabJobCreator.Protocol = "gridftp"
+config.CrabJobCreator.storagePath = "/data/Storage/"
 
 # The poll interval at which to look for new filesets
 config.CrabJobCreator.pollInterval = 60
