1315c0059312bd54ff418f41cc7d152dfd0fbc2a
riahi <>
2009-10-04 14:35:38 +0000

    initial commit.

---
+++ CRABSERVER/src/python/CrabJobCreator/Database/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+Database tables and queries for CrabJobCreator component
+"""
+
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2009/10/04 21:57:12 hriahi Exp $"
+__version__ = "$Revision: 1.1 $"
+__author__ = "hassen.riahi@cern.ch"
