a25d9658366d63392ae3ce8315f2733007830bf7
spiga <>
2009-11-06 16:08:17 +0000

    minor fixes

--- python/Scheduler.py
+++ python/Scheduler.py
@@ -197,7 +197,8 @@ class Scheduler :
                 if i in metaList :
                     msg  = 'Warning: You are asking to stage out on a remote directory \n'
                     msg += '\twhich already contains files with same name.\n'
-                    msg += '\tPlease change directory or remove the actual content.\n'
+                    msg += '\tPlease change directory or remove the actual content following this HowTo:\n'
+                    msg += '\thttps://twiki.cern.ch/twiki/bin/view/CMS/CheckUserRemoteDir\n'
                     raise CrabException(msg)
         else:
             msg = 'Remote directory is empty or not existis\n'
@@ -209,7 +210,7 @@ class Scheduler :
         """
         protocol = self.protocolDict[common.scheduler.name().upper()]
         try:
-            Storage = SElement( FullPath(endpoint), protocol )
+            Storage = SElement( FullPath(string.strip(endpoint)), protocol )
         except ProtocolUnknown, ex:
             remoteList = -1
             msg  = 'Warning : %s '% str(ex)
