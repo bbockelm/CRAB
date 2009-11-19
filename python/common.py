9cc753de3a656843790ce8e85d7331850004777f
spiga <>
2009-11-19 15:55:29 +0000

    271 and update author list

--- python/common.py
+++ python/common.py
@@ -12,20 +12,18 @@
 ###########################################################################
 
 prog_name = 'crab'
-prog_version = (2, 7, 0)
+prog_version = (2, 7, 1)
 prog_version_str=`prog_version[0]`+'.'+`prog_version[1]`+'.'+`prog_version[2]`
 prog_authors = [
     ['Stefano Lacaprara', 'Stefano.Lacaprara@pd.infn.it', 'INFN/LNL'],
-    ['Daniele Spiga', 'Daniele.Spiga@pg.infn.it', 'INFN/Perugia'],
-    ['Alvise Dorigo', 'Alvise.Dorigo@pd.infn.it', 'INFN/Padova'],
+    ['Daniele Spiga', 'Daniele.Spiga@pg.infn.it', 'CERN'],
     ['Mattia Cinquilli', 'Mattia.Cinquilli@cern.ch', 'INFN/Perugia'],
-    ['Marco Corvo', 'Marco.Corvo@cern.ch', 'CERN/CNAF'],
     ['Alessandra Fanfani', 'Alessandra.Fanfani@bo.infn.it', 'INFN/Bologna'],
-    ['Federica Fanzago', 'Federica.Fanzago@cern.ch' , 'CERN/CNAF'],
+    ['Federica Fanzago', 'Federica.Fanzago@cern.ch' , 'INFN/Padova'],
     ['Fabio Farina', 'fabio.farina@cern.ch', 'INFN/Milano Bicocca'],
-    ['Carlos Kavka', 'Carlos.Kavka@ts.infn.it', 'INFN/Trieste'],
-    ['Matteo Merlo'  , 'merloma@gmail.com', 'INFN/Milano Bicocca'],
     ['Eric Vaandering', 'ewv@fnal.gov', 'FNAL'],
+    ['Filippo Spiga', 'filippo.spiga@disco.unimib.it', 'INFN/Milano Bicocca'],
+    ['Hassen Riahi', 'hassen.riahi@pg.infn.it', 'INFN/Perugia']
     ]
 
 ###########################################################################
