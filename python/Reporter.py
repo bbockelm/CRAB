137462ecbf62b8318a2aeee1d2fc02fcc5bfb4ba
ewv <>
2009-12-03 22:15:54 +0000

    Write out lumi summary file, on branch until 2.8

--- python/Reporter.py
+++ python/Reporter.py
@@ -1,6 +1,13 @@
 import os, common, string
 from Actor import *
 from crab_util import *
+from ProdCommon.FwkJobRep.ReportParser import readJobReport
+
+try: # FUTURE: Python 2.6, prior to 2.6 requires simplejson
+    import json
+except:
+    import simplejson as json
+
 
 class Reporter(Actor):
     """ A class to report a short summary of the info of a task, including what
@@ -30,11 +37,10 @@ class Reporter(Actor):
             #print endpoint, lfn, SE, SE_PATH, user
 
             msg+=  "SE: %s %s  srmPath: %s\n"%(self.cfg_params['USER.storage_element'],SE,endpoint)
-            
+
         else:
             msg+=  "Local output: %s"%task['outputDirectory']
         #print task
-        from ProdCommon.FwkJobRep.ReportParser import readJobReport
         possible_status = [ 'Created',
                             'Undefined',
                             'Submitting',
@@ -57,19 +63,21 @@ class Reporter(Actor):
         eventsRequired=0
         filesRead=0
         filesRequired=0
+        runsAndLumis = {}
         for job in task.getJobs():
             if (job.runningJob['applicationReturnCode']>0 or job.runningJob['wrapperReturnCode']>0): continue
             # get FJR filename
             fjr=task['outputDirectory']+job['outputFiles'][-1]
             jobReport = readJobReport(fjr)
-            if len(jobReport)>0:
-                inputFiles=jobReport[0].inputFiles
+            if len(jobReport) > 0:
+                inputFiles = jobReport[0].inputFiles
                 for inputFile in inputFiles:
-                    runs=inputFile.runs
-                    #print [inputFile[it] for it in ['LFN','EventsRead']]
-                    # print "FileIn :",inputFile['LFN'],": Events",inputFile['EventsRead']
-                    # for run in runs.keys():
-                    #     print "Run",run,": lumi sections",runs[run]
+                    # Accumulate the list of lum sections run over
+                    for run in inputFile.runs.keys():
+                        if not runsAndLumis.has_key(run):
+                            runsAndLumis[run] = set()
+                        for lumi in inputFile.runs[run]:
+                            runsAndLumis[run].add(lumi)
                     filesRead+=1
                     eventsRead+=int(inputFile['EventsRead'])
                 #print jobReport[0].inputFiles,'\n'
@@ -77,9 +85,33 @@ class Reporter(Actor):
                 pass
                 #print 'no FJR avaialble for job #%s'%job['jobId']
             #print "--------------------------"
+
+        # Sort, compact, and write the list of successful lumis
+        compactList = {}
+
+        for run in runsAndLumis.keys():
+            lastLumi = -1000
+            compactList[run] = []
+
+            lumiList = list(runsAndLumis[run])
+            lumiList.sort()
+            for lumi in lumiList:
+                if lumi != lastLumi + 1: # Break in sequence from last lumi
+                    compactList[run].append([lumi,lumi])
+                else:
+                    compactList[run][len(compactList[run])-1][1] = lumi
+                lastLumi = lumi
+
+        lumiFilename = task['outputDirectory'] + 'lumiSummary.json'
+        lumiSummary = open(lumiFilename, 'w')
+        json.dump(compactList, lumiSummary)
+        lumiSummary.write('\n')
+        lumiSummary.close()
+
         msg+=  "Total Events read: %s required: %s\n"%(eventsRead,eventsRequired)
         msg+=  "Total Files read: %s required: %s\n"%(filesRead,filesRequired)
         msg+=  "Total Jobs : %s \n"%len(task.getJobs())
+        msg+=  "Luminosity section summary file: %s\n" % lumiFilename
         list_ID={}
         task = common.scheduler.queryEverything(task['id'])
         for st in possible_status:
@@ -88,7 +120,7 @@ class Reporter(Actor):
                 msg+=  "   # Jobs: %s:%s\n"%(str(st),len(list_ID))
             pass
         msg+=  "\n----------------------------\n"
-        common.logger.info(msg)   
-        return      
+        common.logger.info(msg)
+        return
 
-                   
+
