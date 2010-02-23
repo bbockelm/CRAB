3841066951672896c14a58f13349b1f9c16ed0c6
spiga <>
2010-02-23 15:00:23 +0000

    update Tag

--- python/PrepareTarBall.sh
+++ python/PrepareTarBall.sh
@@ -14,7 +14,7 @@ echo "CRABDIR = $CRABdir"
 CRABtag=$tag
 DBSAPItag="DBS_2_0_9_patch_4"
 DLSAPItag="DLS_1_1_2"
-PRODCOMMONtag="PRODCOMMON_0_12_18_CRAB_2"
+PRODCOMMONtag="PRODCOMMON_0_12_18_CRAB_3"
 WMCOREtag="WMCORE_0_1_1_pre21"
 
 CVSrepo=":pserver:anonymous@cmscvs.cern.ch:/cvs_server/repositories"
