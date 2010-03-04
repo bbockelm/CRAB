2974fa4429e4574f12aa5b3263e7cf3784fcc169
spiga <>
2010-03-04 17:02:38 +0000

    handle correclty file descriptor and stderr

--- python/crab_util.py
+++ python/crab_util.py
@@ -231,8 +231,12 @@ def runCommand(command, printout=0, timeout=None,errorCode=False):
 
     # playing with fd
     fd = p.stdout.fileno()
+    fde = p.stderr.fileno()
+
     flags = fcntl.fcntl(fd, fcntl.F_GETFL)
     fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+    flags = fcntl.fcntl(fde, fcntl.F_GETFL)
+    fcntl.fcntl(fde, fcntl.F_SETFL, flags | os.O_NONBLOCK)
 
     # return values
     timedOut = False
@@ -240,14 +244,18 @@ def runCommand(command, printout=0, timeout=None,errorCode=False):
 
     while 1:
         (r, w, e) = select.select([fd], [], [], timeout)
-
-        if fd not in r :
+        read = '' 
+        readerr = '' 
+        if fd in r :
+            read = p.stdout.read()
+        if fde in r:
+            readerr = p.stderr.read()
+        if fd not in r and fde not in r:
             timedOut = True
             break
-
-        read = p.stdout.read()
-        if read != '' :
-            outc.append( read )
+        if read != '' or readerr != '' :
+            if read != '': outc.append( read )
+            if readerr != '':errc.append( readerr )
         else :
             break
 
