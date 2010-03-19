94bceaa3e8201458c28edae68904c80143b36e9e
fanzago <>
2010-03-19 13:44:16 +0000

    changed cmscp_args

--- python/SchedulerLocal.py
+++ python/SchedulerLocal.py
@@ -134,7 +134,9 @@ class SchedulerLocal(Scheduler) :
             txt += 'export TIME_STAGEOUT_INI=`date +%s` \n'
             txt += 'copy_exit_status=0\n'
             cmscp_args = ' --destination $endpoint --inputFileList $file_list'
-            cmscp_args +=' --middleware $middleware --lfn $LFNBaseName %s %s '%(self.loc_stage_out,self.debugWrap)
+            ### FEDE FOR MULTIOUT 
+            #cmscp_args +=' --middleware $middleware --lfn $LFNBaseName %s %s '%(self.loc_stage_out,self.debugWrap)
+            cmscp_args +=' --middleware $middleware --se_name $SE --for_lfn $LFNBaseName %s %s '%(self.loc_stage_out,self.debugWrap)
             txt += 'echo "python cmscp.py %s "\n'%cmscp_args
             txt += 'python cmscp.py %s \n'%cmscp_args
             if self.debug_wrapper==1:
