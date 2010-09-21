7c79d8e50d15844f10c719173dd8cbbc33ffe96f
fanzago <>
2010-09-21 09:49:37 +0000

    PRODCOMMON tag includes fix for ModifyJobReport and ProtocolLocal

--- python/PrepareTarBall.sh
+++ python/PrepareTarBall.sh
@@ -14,7 +14,7 @@ echo "CRABDIR = $CRABdir"
 CRABtag=$tag
 DBSAPItag="DBS_2_0_9_patch_7"
 DLSAPItag="DLS_1_1_2"
-PRODCOMMONtag="PRODCOMMON_0_12_18_CRAB_16"
+PRODCOMMONtag="PRODCOMMON_0_12_18_CRAB_18"
 WMCOREtag="WMCORE_0_1_1_pre26"
 
 CVSrepo=":pserver:anonymous@cmscvs.cern.ch:/cvs_server/repositories"
