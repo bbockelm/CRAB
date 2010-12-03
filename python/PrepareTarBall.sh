a5ddfe589c4a4bc22b04d1a5e7ae85208c753233
spiga <>
2010-12-03 11:00:51 +0000

    uses DLS_1_1_3

--- python/PrepareTarBall.sh
+++ python/PrepareTarBall.sh
@@ -13,7 +13,7 @@ CRABdir=$tag
 echo "CRABDIR = $CRABdir"
 CRABtag=$tag
 DBSAPItag="DBS_2_0_9_patch_7"
-DLSAPItag="DLS_1_1_2"
+DLSAPItag="DLS_1_1_3"
 PRODCOMMONtag="PRODCOMMON_0_12_18_CRAB_21"
 WMCOREtag="WMCORE_0_1_1_pre26"
 
