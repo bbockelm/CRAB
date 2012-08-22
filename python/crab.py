77aed8c87ee6c2a4ddc9875df0819ea068a150f4
belforte <>
2012-08-22 22:08:15 +0000

    add submit_host option for rglidein

--- python/crab.py
+++ python/crab.py
@@ -372,6 +372,9 @@ class Crab:
             elif ( opt == '-scheduler' ):
                 pass
 
+            elif ( opt == '-submit_host' ):
+                pass
+
             ### for CopyData option
             elif ( opt == '-dest_se' ):
                 pass
