b28cac6861ce821447601d764cd132260c2c48e0
belforte <>
2012-10-08 15:58:54 +0000

    use PRODCOMMON_0_12_18_CRAB_44 for CRAB_2_8_3

--- python/PrepareTarBall.sh
+++ python/PrepareTarBall.sh
@@ -14,7 +14,7 @@ echo "CRABDIR = $CRABdir"
 CRABtag=$tag
 DBSAPItag="DBS_2_0_9_patch_9"
 DLSAPItag="DLS_1_1_3"
-PRODCOMMONtag="PRODCOMMON_0_12_18_CRAB_43"
+PRODCOMMONtag="PRODCOMMON_0_12_18_CRAB_44"
 WMCOREtag="WMCORE_0_1_1_pre26"
 
 CVSrepo=":pserver:anonymous@cmscvs.cern.ch:/cvs_server/repositories"
