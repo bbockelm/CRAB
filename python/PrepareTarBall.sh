a0d10592ddfcc4a6e7fcdd596f355f614ae530df
belforte <>
2013-01-07 15:02:44 +0000

    stick to WMCORE_CRAB2_1 i.e. same old one, see https://savannah.cern.ch/bugs/index.php?99588

--- python/PrepareTarBall.sh
+++ python/PrepareTarBall.sh
@@ -15,7 +15,7 @@ CRABtag=$tag
 DBSAPItag="DBS_2_0_9_patch_9"
 DLSAPItag="DLS_1_1_3"
 PRODCOMMONtag="PRODCOMMON_0_12_18_CRAB_48"
-WMCOREtag="WMCORE_CRAB2_2"
+WMCOREtag="WMCORE_CRAB2_1"
 
 CVSrepo=":pserver:anonymous@cmscvs.cern.ch:/cvs_server/repositories"
 #export CVSROOT=${CVSrepo}"/CMSSW"
